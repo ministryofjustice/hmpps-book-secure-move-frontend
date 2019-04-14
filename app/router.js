0251bc26289b409032d156a261e224ce9fe5e253
Dom Smith <dom@teneightfive.com>
2019-04-14 15:41:17 +0100

    Create initial Express server
    This adds a basic setup for running an Express server on node.
    
    Includes a few new dependencies centered around Express.

---
+++ app/router.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', {
+    title: 'PECS Frontend',
+  })
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,51 @@
+// Core dependencies
+const path = require('path')
+
+// NPM dependencies
+const bodyParser = require('body-parser')
+const createError = require('http-errors')
+const cookieParser = require('cookie-parser')
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const router = require('./app/router')
+
+const app = express()
+
+// view engine setup
+app.set('view engine', 'njk')
+nunjucks.configure([
+  `./app/views`,
+], {
+  autoescape: true,
+  express: app,
+})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }))
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routing
+app.use(router)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+module.exports = app
