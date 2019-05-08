38af249a82fed1ce3101b934c2b4fc1f866bcb2e
Dom Smith <dom@teneightfive.com>
2019-05-08 13:09:45 +0100

    Add test support for components
    This change adds some helpers for testing component templates using
    cheerio and the component setup.
    
    Tests will be run using the existing mocha + chai setup.

---
+++ test/unit/component-helpers.js
@@ -0,0 +1,117 @@
+const fs = require('fs')
+const path = require('path')
+const util = require('util')
+
+const cheerio = require('cheerio')
+const nunjucks = require('nunjucks')
+const yaml = require('js-yaml')
+
+const sass = require('node-sass')
+const sassRender = util.promisify(sass.render)
+
+const configPaths = require('../../config/paths')
+
+nunjucks.configure(configPaths.components, {
+  trimBlocks: true,
+  lstripBlocks: true,
+})
+
+/**
+ * Return a macro name for a component
+ * @param {string} componentName
+ * @returns {string} returns naming convention based macro name
+ */
+function _componentNameToMacroName (componentName) {
+  let macroName = componentName
+    .toLowerCase()
+    .split('-')
+    // capitalize each 'word'
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join('')
+
+  return `app${macroName}`
+}
+
+/**
+ * Render a component's macro for testing
+ * @param {string} componentName
+ * @param {string} params parameters that are used in the component macro
+ * @param {any} children any child components or text, pass the children to the macro
+ * @returns {function} returns cheerio (jQuery) instance of the macro for easy DOM querying
+ */
+function render (componentName, params, children = false) {
+  if (typeof params === 'undefined') {
+    throw new Error('Parameters passed to `render` should be an object but are undefined')
+  }
+
+  const macroName = _componentNameToMacroName(componentName)
+  const macroParams = JSON.stringify(params, null, 2)
+
+  let macroString = `{%- from "${componentName}/macro.njk" import ${macroName} -%}`
+
+  // If we're nesting child components or text, pass the children to the macro
+  // using the 'caller' Nunjucks feature
+  if (children) {
+    macroString += `{%- call ${macroName}(${macroParams}) -%}${children}{%- endcall -%}`
+  } else {
+    macroString += `{{- ${macroName}(${macroParams}) -}}`
+  }
+
+  const output = nunjucks.renderString(macroString)
+  return cheerio.load(output)
+}
+
+/**
+ * Get examples from a component's metadata file
+ * @param {string} componentName
+ * @returns {object} returns object that includes all examples at once
+ */
+function getExamples (componentName) {
+  const file = fs.readFileSync(
+    path.join(configPaths.components, componentName, `${componentName}.yaml`),
+    'utf8'
+  )
+
+  const docs = yaml.safeLoad(file)
+
+  const examples = {}
+
+  for (let example of docs.examples) {
+    examples[example.name] = example.data
+  }
+
+  return examples
+}
+
+/**
+ * Render Sass
+ *
+ * @param {object} options Options to pass to sass.render
+ * @return {promise} Result of calling sass.render
+ */
+function renderSass (options) {
+  return sassRender({
+    includePaths: [ configPaths.src ],
+    ...options,
+  })
+}
+
+/**
+ * Get the raw HTML representation of a component, and remove any other
+ * child elements that do not match the component.
+ * Relies on B.E.M naming ensuring that child components relating to a component
+ * are namespaced.
+ * @param {function} $ requires an instance of cheerio (jQuery) that includes the
+ * rendered component.
+ * @param {string} className the top level class 'Block' in B.E.M terminology
+ * @returns {string} returns HTML
+ */
+function htmlWithClassName ($, className) {
+  const $component = $(className)
+  const classSelector = className.replace('.', '')
+  // Remove all other elements that do not match this component
+  $component.find(`[class]:not([class^=${classSelector}])`).remove()
+  return $.html($component)
+}
+
+module.exports = { render, getExamples, htmlWithClassName, renderSass }
