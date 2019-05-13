abcea23fda51256f0006980842fb9b2745a46d6d
Dom Smith <dom@teneightfive.com>
2019-05-13 12:30:46 +0100

    Display current day on the dashboard
    This change makes use of some new components and displays the
    current move date as the heading on the dashboard.

--- app/dashboard/controllers.test.js
+++ app/dashboard/controllers.test.js
@@ -2,15 +2,91 @@ const controllers = require('./controllers')
 
 describe('Dashboard app', function () {
   describe('#getController()', function () {
-    it('should render a template', function () {
-      const req = {}
-      const res = {
-        render: sinon.spy(),
-      }
+    beforeEach(() => {
+      const mockDate = new Date('2017-08-10')
+      this.clock = sinon.useFakeTimers(mockDate.getTime())
+    })
 
-      controllers.get(req, res)
+    afterEach(() => {
+      this.clock.restore()
+    })
 
-      expect(res.render.calledOnce).to.be.true
+    context('when query contains no move date', () => {
+      let req, res
+
+      beforeEach(() => {
+        req = {
+          query: {},
+        }
+        res = {
+          render: sinon.spy(),
+        }
+
+        controllers.get(req, res)
+      })
+
+      it('should render a template', function () {
+        expect(res.render.calledOnce).to.be.true
+      })
+
+      describe('template params', () => {
+        it('should contain a page title', function () {
+          expect(res.render.args[0][1]).to.have.property('pageTitle')
+        })
+
+        it('should contain move date as current date', function () {
+          const params = res.render.args[0][1]
+          expect(params).to.have.property('moveDate')
+          expect(params.moveDate).to.equal('2017-08-10')
+        })
+
+        it('should contain pagination with correct links', function () {
+          const params = res.render.args[0][1]
+          expect(params).to.have.property('pagination')
+          expect(params.pagination.nextUrl).to.equal('?move-date=2017-08-11')
+          expect(params.pagination.prevUrl).to.equal('?move-date=2017-08-09')
+        })
+      })
+    })
+
+    context('when query contatins a move date', () => {
+      let req, res
+
+      beforeEach(() => {
+        req = {
+          query: {
+            'move-date': '2018-05-10',
+          },
+        }
+        res = {
+          render: sinon.spy(),
+        }
+
+        controllers.get(req, res)
+      })
+
+      it('should render a template', function () {
+        expect(res.render.calledOnce).to.be.true
+      })
+
+      describe('template params', () => {
+        it('should contain a page title', function () {
+          expect(res.render.args[0][1]).to.have.property('pageTitle')
+        })
+
+        it('should contain move date as current date', function () {
+          const params = res.render.args[0][1]
+          expect(params).to.have.property('moveDate')
+          expect(params.moveDate).to.equal('2018-05-10')
+        })
+
+        it('should contain pagination with correct links', function () {
+          const params = res.render.args[0][1]
+          expect(params).to.have.property('pagination')
+          expect(params.pagination.nextUrl).to.equal('?move-date=2018-05-11')
+          expect(params.pagination.prevUrl).to.equal('?move-date=2018-05-09')
+        })
+      })
     })
   })
 })
