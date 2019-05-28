35d2c2792a28b1c74c71141ba2cb757d8bd98576
Dom Smith <dom@teneightfive.com>
2019-05-28 16:49:37 +0100

    Skeleton a basic profile app
    This creates the index loader, basic router and controller for
    the profile app.

---
+++ app/moves/controllers.test.js
@@ -0,0 +1,15 @@
+const controllers = require('./controllers')
+
+describe('Moves controllers', function () {
+  describe('#get()', function () {
+    it('should render a template', function () {
+      const req = {}
+      const res = {
+        render: sinon.spy(),
+      }
+
+      controllers.get(req, res)
+      expect(res.render.calledOnce).to.be.true
+    })
+  })
+})
---
+++ app/moves/index.js
@@ -0,0 +1,20 @@
+// NPM dependencies
+const express = require('express')
+
+// Local dependencies
+const { get } = require('./controllers')
+
+// Initialisation
+const router = new express.Router()
+const paths = {
+  index: '/moves/:moveId',
+}
+
+// Routing
+router.get(paths.index, get)
+
+// Export
+module.exports = {
+  router,
+  paths,
+}
