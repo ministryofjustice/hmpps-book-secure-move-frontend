0ad7d22e72ff458ad7d0f30362c2fb64188ddb5d
Steve Lorek <steve@stevelorek.com>
2019-05-29 11:47:46 +0100

    Add test for auth controller

---
+++ app/auth/controllers.test.js
@@ -0,0 +1,24 @@
+const controllers = require('./controllers')
+
+describe.only('Authentication app', function () {
+  describe('#get action', function () {
+    let req, res, url
+
+    beforeEach(async () => {
+      url = "/test"
+      req = {
+        query: {},
+        session: {
+          postAuthRedirect: url
+        }
+      }
+      res = { redirect: sinon.spy() }
+
+      await controllers.get(req, res)
+    })
+
+    it('redirects to the intended URL', function () {
+      expect(res.redirect.calledWith(url)).to.be.true
+    })
+  })
+})
