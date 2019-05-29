3bcac03633a8254ceb869dacbc475c64962e297d
Steve Lorek <steve@stevelorek.com>
2019-05-29 17:48:21 +0100

    Remove redundant async functionality

--- app/auth/controllers.js
+++ app/auth/controllers.js
@@ -1,11 +1,7 @@
 module.exports = {
-  get: async (req, res, next) => {
-    try {
-      const redirectUrl = req.session.postAuthRedirect
-      req.session.postAuthRedirect = null
-      res.redirect(redirectUrl)
-    } catch (error) {
-      next(error)
-    }
+  get: (req, res, next) => {
+    const redirectUrl = req.session.postAuthRedirect
+    req.session.postAuthRedirect = null
+    res.redirect(redirectUrl)
   },
 }
