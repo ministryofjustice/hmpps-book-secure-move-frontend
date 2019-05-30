715e35f8591f6f90f07d511a76b770d6fa812a77
Steve Lorek <steve@stevelorek.com>
2019-05-30 11:27:37 +0100

    Remove redundant argument from controller

--- app/auth/controllers.js
+++ app/auth/controllers.js
@@ -1,5 +1,5 @@
 module.exports = {
-  get: (req, res, next) => {
+  get: (req, res) => {
     const redirectUrl = req.session.postAuthRedirect
     req.session.postAuthRedirect = null
     res.redirect(redirectUrl)
