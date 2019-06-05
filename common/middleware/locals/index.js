bf4f19b3d0e4e05d5a1f405f3b2c5db5769db405
Dom Smith <dom@teneightfive.com>
2019-06-05 17:19:41 +0100

    Add helper method to get the value of a res.local property
    In some instances (not good ones) the key uses spaces or strings.
    
    This means they can't be accessed the normal way in the view so
    this helper means the value can still be gathered from the `res.locals`
    object.

--- common/middleware/locals/index.js
+++ common/middleware/locals/index.js
@@ -3,6 +3,7 @@ const getAssetPath = require('./get-asset-path')
 module.exports = function setLocals (req, res, next) {
   res.locals = Object.assign({}, res.locals, {
     getAssetPath,
+    getLocal: key => res.locals[key],
   })
 
   next()
