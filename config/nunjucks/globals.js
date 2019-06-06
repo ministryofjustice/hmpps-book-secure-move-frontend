0962e604ab1f6b9132481b2b2295e526a12391f7
Dom Smith <dom@teneightfive.com>
2019-06-06 23:39:04 +0100

    Add support to form wizard to call macros
    This adds a helper method to nunjucks globals so that macros
    can be called dynamically from the form wizard fields object
    that is passed to the view layer.

--- config/nunjucks/globals.js
+++ config/nunjucks/globals.js
@@ -1,3 +1,17 @@
+const { isFunction } = require('lodash')
+
+const logger = require('../logger')
+
 module.exports = {
   SERVICE_NAME: 'Book a secure move',
+  callAsMacro (name) {
+    const macro = this.ctx[name]
+
+    if (!isFunction(macro)) {
+      logger.warn(`'${name}' macro does not exist`)
+      return () => ''
+    }
+
+    return macro
+  },
 }
