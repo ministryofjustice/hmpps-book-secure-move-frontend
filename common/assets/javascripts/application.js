846b8884ef6788f4126204c11201870c13a5724a
Dom Smith <dom@teneightfive.com>
2019-06-17 22:53:33 +0100

    Add message component
    This creates a new component to display a group of messages.
    
    It includes informative, success, warning and error styled messages.

--- common/assets/javascripts/application.js
+++ common/assets/javascripts/application.js
@@ -1,6 +1,13 @@
 import '../images/app-logotype-crest.png'
 import '../images/app-logotype-crest@2x.png'
 
+import { nodeListForEach } from './utils'
 import { initAll } from 'govuk-frontend'
+import Message from '../../components/message/message'
 
 initAll()
+
+var $messages = document.querySelectorAll('[data-module="app-message"]')
+nodeListForEach($messages, function ($message) {
+  new Message($message).init()
+})
