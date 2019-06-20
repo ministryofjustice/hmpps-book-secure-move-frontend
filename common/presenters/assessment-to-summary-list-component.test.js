311edd5d84f2c06b0a60600d1c8f958381f0692c
Dom Smith <dom@teneightfive.com>
2019-06-20 21:30:40 +0100

    Add new presenters for handling the person assessment
    This adds two new presenters that handle transforming
    parts of the assessment into structures that can be used
    in the view layer or by a specific component.

---
+++ common/presenters/assessment-to-summary-list-component.test.js
@@ -0,0 +1,89 @@
+const assessmentToSummaryListComponent = require('./assessment-to-summary-list-component')
+
+const { data: mockPerson } = require('../../test/fixtures/api-client/person.post.deserialized.json')
+
+describe('Presenters', function () {
+  describe('#assessmentToSummaryListComponent()', function () {
+    context('when provided with a mock person object', function () {
+      let transformedResponse
+
+      beforeEach(function () {
+        transformedResponse = assessmentToSummaryListComponent(mockPerson.assessment_answers)
+      })
+
+      describe('response', function () {
+        it('should contain rows property', function () {
+          expect(transformedResponse).to.have.property('rows')
+          expect(transformedResponse.rows.length).to.equal(8)
+        })
+
+        it('should format rows correctly', function () {
+          expect(transformedResponse.rows).to.deep.equal([
+            {
+              key: { text: 'Violent' },
+              value: { text: 'Karate black belt' },
+            },
+            {
+              key: { text: 'Escape' },
+              value: { text: 'Large poster in cell' },
+            },
+            {
+              key: { text: 'Must be held separately' },
+              value: { text: 'Incitement to riot' },
+            },
+            {
+              key: { text: 'Special diet or allergy' },
+              value: { text: 'Vegan' },
+            },
+            {
+              key: { text: 'Health issue' },
+              value: { text: 'Claustophobic' },
+            },
+            {
+              key: { text: 'Medication' },
+              value: { text: 'Heart medication needed twice daily' },
+            },
+            {
+              key: { text: 'Solicitor or other legal representation' },
+              value: { text: 'Johns & Sons' },
+            },
+            {
+              key: { text: 'Any other information' },
+              value: { text: 'Former prison officer' },
+            },
+          ])
+        })
+      })
+    })
+
+    context('when filter category is specific', function () {
+      let transformedResponse
+
+      beforeEach(function () {
+        transformedResponse = assessmentToSummaryListComponent(mockPerson.assessment_answers, 'court')
+      })
+
+      describe('response', function () {
+        describe('response', function () {
+          it('should only return filtered categories', function () {
+            expect(transformedResponse).to.have.property('rows')
+            expect(transformedResponse.rows.length).to.equal(2)
+          })
+
+          it('should format filtered rows correctly', function () {
+            expect(transformedResponse.rows).to.deep.equal([
+              {
+                key: { text: 'Solicitor or other legal representation' },
+                value: { text: 'Johns & Sons' },
+              },
+              {
+                key: { text: 'Any other information' },
+                value: { text: 'Former prison officer' },
+              },
+            ])
+          })
+        })
+      })
+    })
+  })
+})
