d786b9a295ffad5f40340e38474b5c01a6f5a59a
Dom Smith <dom@teneightfive.com>
2019-06-21 14:42:49 +0100

    Store the answers to an assessment step
    This change adds the ability to store the asessment answers
    on a person object during the form creation session.
    
    Once a step has been successfully saved it will map the values
    from the current step and store them as an object on the assessment
    property and then also flatten this to the format expected by the
    Person service.

--- app/moves/controllers/assessment.js
+++ app/moves/controllers/assessment.js
@@ -1,3 +1,5 @@
+const { flatten, values } = require('lodash')
+
 const FormController = require('./form')
 const referenceDataService = require('../../../common/services/reference-data')
 
@@ -32,6 +34,31 @@ class AssessmentController extends FormController {
     } catch (error) {
       next(error)
     }
+  }
+
+  saveValues (req, res, next) {
+    const person = req.sessionModel.get('person') || {}
+    const assessment = req.sessionModel.get('assessment') || {}
+    const { fields } = req.form.options
+
+    Object.keys(fields).forEach((field) => {
+      const answers = req.form.values[field]
+
+      assessment[field] = answers
+        .filter(Boolean) // filter out any empty values
+        .map((questionId) => {
+          return {
+            assessment_question_id: questionId,
+          }
+        })
+    })
+
+    req.form.values.assessment = assessment
+    req.form.values.person = Object.assign({}, person, {
+      assessment_answers: flatten(values(assessment)),
+    })
+
+    super.saveValues(req, res, next)
   }
 }
 
--- app/moves/controllers/assessment.test.js
+++ app/moves/controllers/assessment.test.js
@@ -77,13 +77,11 @@ describe('Moves controllers', function () {
         })
 
         it('should call parent configure method', function () {
-          expect(FormController.prototype.configure).to.be.calledOnce
-          expect(FormController.prototype.configure).to.be.calledWith(req, {}, nextSpy)
+          expect(FormController.prototype.configure).to.be.calledOnceWithExactly(req, {}, nextSpy)
         })
 
         it('should not throw an error', function () {
-          expect(nextSpy).to.be.calledOnce
-          expect(nextSpy).to.be.calledWith()
+          expect(nextSpy).to.be.calledOnceWithExactly()
         })
       })
 
@@ -122,6 +120,235 @@ describe('Moves controllers', function () {
 
         it('should not call parent configure method', function () {
           expect(FormController.prototype.configure).not.to.be.called
+        })
+      })
+    })
+
+    describe('#saveValues()', function () {
+      let nextSpy
+
+      beforeEach(function () {
+        sinon.spy(FormController.prototype, 'saveValues')
+        nextSpy = sinon.spy()
+      })
+
+      context('with no previous session values', function () {
+        let req
+
+        beforeEach(function () {
+          req = {
+            form: {
+              options: {
+                fields: {
+                  risk: {},
+                },
+              },
+              values: {
+                risk: ['a1f6a3b5-a448-4a78-8cf7-6659a71661c2'],
+              },
+            },
+            sessionModel: {
+              get: sinon.stub().returns(),
+              set: sinon.spy(),
+              unset: sinon.spy(),
+            },
+          }
+
+          controller.saveValues(req, {}, nextSpy)
+        })
+
+        it('should save values on assessment property', function () {
+          expect(req.form.values.assessment).to.deep.equal({
+            risk: [{
+              assessment_question_id: 'a1f6a3b5-a448-4a78-8cf7-6659a71661c2',
+            }],
+          })
+        })
+
+        it('should flatten values on assessment_answers property', function () {
+          expect(req.form.values.person.assessment_answers).to.deep.equal([
+            {
+              assessment_question_id: 'a1f6a3b5-a448-4a78-8cf7-6659a71661c2',
+            },
+          ])
+        })
+
+        it('should call parent configure method', function () {
+          expect(FormController.prototype.saveValues).to.be.calledOnceWithExactly(req, {}, nextSpy)
+        })
+
+        it('should not throw an error', function () {
+          expect(nextSpy).to.be.calledOnceWithExactly()
+        })
+      })
+
+      context('with previous session values', function () {
+        let req, sessionGetStub
+
+        beforeEach(function () {
+          sessionGetStub = sinon.stub()
+          req = {
+            form: {
+              options: {
+                fields: {
+                  risk: {},
+                },
+              },
+              values: {
+                risk: ['a1f6a3b5-a448-4a78-8cf7-6659a71661c2'],
+              },
+            },
+            sessionModel: {
+              get: sessionGetStub,
+              set: sinon.spy(),
+              unset: sinon.spy(),
+            },
+          }
+
+          sessionGetStub.withArgs('person').returns({
+            first_names: 'Steve',
+            last_name: 'Smith',
+          })
+          sessionGetStub.withArgs('assessment').returns({
+            risk: [{
+              assessment_question_id: '534b05af-8b55-4a37-9f36-d36a60f04aa8',
+            }],
+            health: [{
+              assessment_question_id: '7360ea7b-f4c2-4a09-88fd-5e3b57de1a47',
+            }],
+          })
+
+          controller.saveValues(req, {}, nextSpy)
+        })
+
+        it('should overwrite values for current field', function () {
+          expect(req.form.values.assessment.risk).to.deep.equal([{
+            assessment_question_id: 'a1f6a3b5-a448-4a78-8cf7-6659a71661c2',
+          }])
+        })
+
+        it('should not mutate other assessment fields', function () {
+          expect(req.form.values.assessment.health).to.deep.equal([{
+            assessment_question_id: '7360ea7b-f4c2-4a09-88fd-5e3b57de1a47',
+          }])
+        })
+
+        it('should flatten values on assessment_answers property', function () {
+          expect(req.form.values.person.assessment_answers).to.deep.equal([
+            {
+              assessment_question_id: 'a1f6a3b5-a448-4a78-8cf7-6659a71661c2',
+            },
+            {
+              assessment_question_id: '7360ea7b-f4c2-4a09-88fd-5e3b57de1a47',
+            },
+          ])
+        })
+
+        it('should call parent configure method', function () {
+          expect(FormController.prototype.saveValues).to.be.calledOnceWithExactly(req, {}, nextSpy)
+        })
+
+        it('should not throw an error', function () {
+          expect(nextSpy).to.be.calledOnceWithExactly()
+        })
+      })
+
+      context('with empty values in form', function () {
+        let req
+
+        beforeEach(function () {
+          req = {
+            form: {
+              options: {
+                fields: {
+                  risk: {},
+                },
+              },
+              values: {
+                risk: ['', 'a1f6a3b5-a448-4a78-8cf7-6659a71661c2', '', false],
+              },
+            },
+            sessionModel: {
+              get: sinon.stub().returns(),
+              set: sinon.spy(),
+              unset: sinon.spy(),
+            },
+          }
+
+          controller.saveValues(req, {}, nextSpy)
+        })
+
+        it('should remove empty values', function () {
+          expect(req.form.values.assessment).to.deep.equal({
+            risk: [{
+              assessment_question_id: 'a1f6a3b5-a448-4a78-8cf7-6659a71661c2',
+            }],
+          })
+        })
+
+        it('should flatten values on assessment_answers property', function () {
+          expect(req.form.values.person.assessment_answers).to.deep.equal([
+            {
+              assessment_question_id: 'a1f6a3b5-a448-4a78-8cf7-6659a71661c2',
+            },
+          ])
+        })
+      })
+
+      context('with multiple values in form', function () {
+        let req
+
+        beforeEach(function () {
+          req = {
+            form: {
+              options: {
+                fields: {
+                  risk: {},
+                },
+              },
+              values: {
+                risk: [
+                  '7360ea7b-f4c2-4a09-88fd-5e3b57de1a47',
+                  'a1f6a3b5-a448-4a78-8cf7-6659a71661c2',
+                  '534b05af-8b55-4a37-9f36-d36a60f04aa8',
+                ],
+              },
+            },
+            sessionModel: {
+              get: sinon.stub().returns(),
+              set: sinon.spy(),
+              unset: sinon.spy(),
+            },
+          }
+
+          controller.saveValues(req, {}, nextSpy)
+        })
+
+        it('should include all values', function () {
+          expect(req.form.values.assessment).to.deep.equal({
+            risk: [{
+              assessment_question_id: '7360ea7b-f4c2-4a09-88fd-5e3b57de1a47',
+            }, {
+              assessment_question_id: 'a1f6a3b5-a448-4a78-8cf7-6659a71661c2',
+            }, {
+              assessment_question_id: '534b05af-8b55-4a37-9f36-d36a60f04aa8',
+            }],
+          })
+        })
+
+        it('should includes all flatten values on assessment_answers property', function () {
+          expect(req.form.values.person.assessment_answers.length).to.equal(3)
+          expect(req.form.values.person.assessment_answers).to.deep.equal([
+            {
+              assessment_question_id: '7360ea7b-f4c2-4a09-88fd-5e3b57de1a47',
+            },
+            {
+              assessment_question_id: 'a1f6a3b5-a448-4a78-8cf7-6659a71661c2',
+            },
+            {
+              assessment_question_id: '534b05af-8b55-4a37-9f36-d36a60f04aa8',
+            },
+          ])
         })
       })
     })
