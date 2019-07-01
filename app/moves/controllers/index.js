1d125fe446a8f775a45b4c8142cd3644368e8d65
Dom Smith <dom@teneightfive.com>
2019-07-01 13:56:13 +0100

    Add controller to download moves in different formats
    This adds a simple controller that expects an extension paramter
    and will then download the moves in the desired format if it is
    supported.

--- app/moves/controllers/index.js
+++ app/moves/controllers/index.js
@@ -1,5 +1,6 @@
 const cancelMove = require('./cancel-move')
 const detail = require('./detail')
+const download = require('./download')
 const list = require('./list')
 const Form = require('./form')
 const PersonalDetails = require('./personal-details')
@@ -10,6 +11,7 @@ const Save = require('./save')
 module.exports = {
   cancelMove,
   detail,
+  download,
   list,
   Form,
   PersonalDetails,
