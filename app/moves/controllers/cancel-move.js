a03d49e2bab5038390ecf22195e348bfd929b8a2
Dom Smith <dom@teneightfive.com>
2019-07-01 17:03:03 +0100

    Add controller handlers to deal with cancelling moves
    This adds a get and post controller to deal with changing the
    status of a move to cancelled.
    
    The get handler displays the confirmation page for cancelling a
    move that will later be expanded to capture some more details
    about the cancellation.

---
+++ app/moves/controllers/cancel-move.js
@@ -0,0 +1,32 @@
+const personService = require('../../../common/services/person')
+const moveService = require('../../../common/services/move')
+
+module.exports = {
+  post: async (req, res, next) => {
+    try {
+      const { move } = res.locals
+
+      await moveService.cancel(move.id)
+
+      req.flash('success', {
+        title: req.t('messages:cancel_move.success.title'),
+        content: req.t('messages:cancel_move.success.content', {
+          name: personService.getFullname(move.person),
+          location: move.to_location.title,
+        }),
+      })
+
+      res.redirect('/moves')
+    } catch (error) {
+      next(error)
+    }
+  },
+  get: (req, res) => {
+    const { move } = res.locals
+    const locals = {
+      fullname: personService.getFullname(move.person),
+    }
+
+    res.render('moves/views/cancel', locals)
+  },
+}
