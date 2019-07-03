5161e56b30d8253dbe686b7a6825e37c0cd50389
Dom Smith <dom@teneightfive.com>
2019-07-03 15:52:29 +0100

    Use current location from session for move creation
    This change uses the current location stored in the session
    to populate the from_location ID during the move creation journey.

--- common/middleware/locals/index.js
+++ common/middleware/locals/index.js
@@ -4,6 +4,7 @@ module.exports = function setLocals (req, res, next) {
   const locals = {
     TODAY: new Date(),
     TOMORROW: startOfTomorrow(),
+    CURRENT_LOCATION: req.session.currentLocation,
     getLocal: key => res.locals[key],
     getMessages: () => req.flash(),
   }
