e27657fcfcb0752fdda36f6293fcea842c5096ea
Dom Smith <dom@teneightfive.com>
2019-07-23 18:14:03 +0100

    Create new helper to hide disabled reference data
    This adds a new filter helper that can be used to hide any
    disabled reference data from a given point in time.

---
+++ common/helpers/reference-data.test.js
@@ -0,0 +1,166 @@
+const { addMonths, subMonths } = require('date-fns')
+
+const { filterDisabled } = require('./reference-data')
+
+const nextMonth = addMonths(new Date(), 1)
+const lastMonth = subMonths(new Date(), 1)
+
+describe('Reference data helpers', function () {
+  describe('#filterDisabled', function () {
+    context('when an option was disabled in the past', function () {
+      beforeEach(function () {
+        this.options = [
+          {
+            id: '1234',
+            name: 'Freds',
+            disabled_on: lastMonth,
+          },
+        ]
+      })
+
+      context('when the option is not the current field value', function () {
+        beforeEach(function () {
+          this.filteredOptions = this.options.filter(
+            filterDisabled({
+              currentValue: '3',
+              createdOn: lastMonth,
+            })
+          )
+        })
+
+        it('should not include the option', function () {
+          expect(this.filteredOptions).to.have.length(0)
+        })
+      })
+
+      context('when the option is the current field value', function () {
+        beforeEach(function () {
+          this.filteredOptions = this.options.filter(
+            filterDisabled({
+              currentValue: '1234',
+              createdOn: lastMonth,
+            })
+          )
+        })
+
+        it('should include the option', function () {
+          expect(this.filteredOptions).to.have.length(1)
+        })
+      })
+
+      context('when there is no current value', function () {
+        beforeEach(function () {
+          this.filteredOptions = this.options.filter(filterDisabled())
+        })
+
+        it('should not include the option', function () {
+          expect(this.filteredOptions).to.have.length(0)
+        })
+      })
+    })
+
+    context('when an option is disabled in the future', function () {
+      beforeEach(function () {
+        this.options = [
+          {
+            id: '1234',
+            name: 'Freds',
+            disabled_on: nextMonth,
+          },
+        ]
+      })
+
+      context('when the option is not the current field value', function () {
+        beforeEach(function () {
+          this.filteredOptions = this.options.filter(
+            filterDisabled({
+              currentValue: '3',
+              createdOn: lastMonth,
+            })
+          )
+        })
+
+        it('should include the option', function () {
+          expect(this.filteredOptions).to.have.length(1)
+        })
+      })
+
+      context('when the option is the current field value', function () {
+        beforeEach(function () {
+          this.filteredOptions = this.options.filter(
+            filterDisabled({
+              currentValue: '1234',
+              createdOn: lastMonth,
+            })
+          )
+        })
+
+        it('should include the option', function () {
+          expect(this.filteredOptions).to.have.length(1)
+        })
+      })
+
+      context('when there is no current value', function () {
+        beforeEach(function () {
+          this.filteredOptions = this.options.filter(filterDisabled())
+        })
+
+        it('should include the option', function () {
+          expect(this.filteredOptions).to.have.length(1)
+        })
+      })
+    })
+
+    context('when an option is not disabled', function () {
+      beforeEach(function () {
+        this.options = [
+          {
+            id: '1234',
+            name: 'Freds',
+            disabled_on: null,
+          },
+        ]
+      })
+
+      context('when the option is not the current field value', function () {
+        beforeEach(function () {
+          this.filteredOptions = this.options.filter(
+            filterDisabled({
+              currentValue: '3',
+              createdOn: lastMonth,
+            })
+          )
+        })
+
+        it('should include the option', function () {
+          expect(this.filteredOptions).to.have.length(1)
+        })
+      })
+
+      context('when the option is the current field value', function () {
+        beforeEach(function () {
+          this.filteredOptions = this.options.filter(
+            filterDisabled({
+              currentValue: '1234',
+              createdOn: lastMonth,
+            })
+          )
+        })
+
+        it('should include the option', function () {
+          expect(this.filteredOptions).to.have.length(1)
+        })
+      })
+
+      context('when there is no current value', function () {
+        beforeEach(function () {
+          this.filteredOptions = this.options.filter(filterDisabled())
+        })
+
+        it('should include the option', function () {
+          expect(this.filteredOptions).to.have.length(1)
+        })
+      })
+    })
+  })
+})
