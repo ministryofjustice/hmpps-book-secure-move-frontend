88ebbb92ace2066e7a8e98f21e1230378add451b
Dom Smith <dom@teneightfive.com>
2019-08-05 14:03:27 +0100

    Redirect user back to that day after save
    This change redirects the user back to the date they created the move
    for on the dashboard.
    
    This makes more sense than always redirecting the user back to the
    default day (today) where they won't see the move they just created.

--- app/move/controllers/new.save.test.js
+++ app/move/controllers/new.save.test.js
@@ -167,7 +167,9 @@ describe('Move controllers', function() {
 
       it('should redirect correctly', function() {
         expect(res.redirect).to.have.been.calledOnce
-        expect(res.redirect).to.have.been.calledWith('/moves')
+        expect(res.redirect).to.have.been.calledWith(
+          `/moves?move-date=${moveMock.date}`
+        )
       })
     })
   })
