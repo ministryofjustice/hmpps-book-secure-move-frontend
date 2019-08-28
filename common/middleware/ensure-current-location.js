8ea263301a06f77675e0d9c9e26c0066ce19da8c
Dom Smith <dom@teneightfive.com>
2019-08-28 11:44:17 +0100

    Fix moves permissions with no locations case
    If a user has permission to view all moves, regardless of location,
    they should be able to view moves even if their user does not currently
    have any available locations.
    
    This is currently the case for suppliers. Without this change a
    supplier user is only greeted with the no locations message and cannot
    view any other parts of the app.

--- common/middleware/ensure-current-location.js
+++ common/middleware/ensure-current-location.js
@@ -1,9 +1,14 @@
 function ensureCurrentLocation({ locationsMountpath, whitelist = [] } = {}) {
   return (req, res, next) => {
+    const { permissions = [], locations = [] } = req.session.user || {}
+    const canViewAllMovesWithoutLocations =
+      permissions.includes('moves:view:all') && !locations.length
+
     if (
       req.session.currentLocation ||
       whitelist.includes(req.url) ||
-      req.url.includes(locationsMountpath)
+      req.url.includes(locationsMountpath) ||
+      canViewAllMovesWithoutLocations
     ) {
       return next()
     }
