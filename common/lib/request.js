fe546ede193ea792b91299cdc7b7094846b19a1f
Ben Chidgey <ben@feedmypixel.com>
2019-08-29 11:54:21 +0100

    Tidy up query string method
    Little bit of a tidyup/refactor around the query string method

--- common/lib/request.js
+++ common/lib/request.js
@@ -1,8 +1,9 @@
 const queryString = require('query-string')
 
+function getQueryString(target, source) {
+  return `?${queryString.stringify({ ...target, ...source })}`
+}
+
 module.exports = {
-  getQueryString(target, source) {
-    const merged = Object.assign({}, target, source)
-    return `?${queryString.stringify(merged)}`
-  },
+  getQueryString,
 }
