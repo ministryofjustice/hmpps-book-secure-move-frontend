fcc5b6654fef741ac17ab315ededceb9197ba509
Dom Smith <dom@teneightfive.com>
2019-08-30 16:11:56 +0100

    Refactor how API client library is called
    This changes the module to a function call and also changes it to
    a singleton so that we're not creating multiple instances each time
    the API client library is loaded.
    
    This change also adds some tests to ensure we are setting up the
    devour API client library as expected.

--- common/lib/api-client/index.js
+++ common/lib/api-client/index.js
@@ -1,3 +1,30 @@
-const jsonApi = require('./json-api')
+const JsonApi = require('devour-client')
 
-module.exports = jsonApi
+const { API, IS_DEV } = require('../../../config')
+const { errors, requestTimeout } = require('./middleware')
+const { devourAuthMiddleware } = require('./middleware/auth')
+const models = require('./models')
+
+let instance
+
+module.exports = function() {
+  if (instance) {
+    return instance
+  }
+
+  instance = new JsonApi({
+    apiUrl: API.BASE_URL,
+    logger: IS_DEV,
+  })
+
+  instance.replaceMiddleware('errors', errors)
+  instance.insertMiddlewareBefore('axios-request', requestTimeout(API.TIMEOUT))
+  instance.insertMiddlewareBefore('axios-request', devourAuthMiddleware)
+
+  // define models
+  Object.entries(models).forEach(([modelName, model]) => {
+    instance.define(modelName, model.attributes, model.options)
+  })
+
+  return instance
+}
