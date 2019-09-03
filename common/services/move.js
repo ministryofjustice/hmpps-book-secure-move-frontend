ce7c6c31923b3109c437a1a74ab65428823fd47c
Dom Smith <dom@teneightfive.com>
2019-09-03 14:26:04 +0100

    Refactor move service unit tests
    This refactors the way the move service unit tests work.
    
    Currently they rely on mocking the HTTP requests from the API
    client which has lead to some vague tests.
    
    This refactor stubs the API client library so that the tests only
    cover this layer.
    
    It also means that the need to stub the auth middleware no longer
    needs to take place.

--- common/services/move.js
+++ common/services/move.js
@@ -3,92 +3,91 @@ const { mapValues, pickBy } = require('lodash')
 const apiClient = require('../lib/api-client')()
 const personService = require('../services/person')
 
-function format(data) {
-  const relationships = ['to_location', 'from_location']
+const moveService = {
+  format(data) {
+    const relationships = ['to_location', 'from_location']
 
-  return mapValues(pickBy(data), (value, key) => {
-    if (relationships.includes(key) && typeof value === 'string') {
-      return { id: value }
-    }
-    return value
-  })
-}
-
-function getMoves({ filter, combinedData = [], page = 1 } = {}) {
-  return apiClient
-    .findAll('move', {
-      ...filter,
-      page,
-      per_page: 100,
-    })
-    .then(response => {
-      const { data, links } = response
-      const moves = [...combinedData, ...data]
-
-      if (!links.next) {
-        return moves.map(move => ({
-          ...move,
-          person: personService.transform(move.person),
-        }))
+    return mapValues(pickBy(data), (value, key) => {
+      if (relationships.includes(key) && typeof value === 'string') {
+        return { id: value }
       }
+      return value
+    })
+  },
 
-      return getMoves({
-        filter,
-        combinedData: moves,
-        page: page + 1,
+  getMoves({ filter, combinedData = [], page = 1 } = {}) {
+    return apiClient
+      .findAll('move', {
+        ...filter,
+        page,
+        per_page: 100,
       })
+      .then(response => {
+        const { data, links } = response
+        const moves = [...combinedData, ...data]
+
+        if (!links.next) {
+          return moves.map(move => ({
+            ...move,
+            person: personService.transform(move.person),
+          }))
+        }
+
+        return moveService.getMoves({
+          filter,
+          combinedData: moves,
+          page: page + 1,
+        })
+      })
+  },
+
+  getRequestedMovesByDateAndLocation(moveDate, locationId) {
+    return moveService.getMoves({
+      filter: {
+        'filter[status]': 'requested',
+        'filter[date_from]': moveDate,
+        'filter[date_to]': moveDate,
+        'filter[from_location_id]': locationId,
+      },
     })
+  },
+
+  getMoveById(id) {
+    if (!id) {
+      return Promise.reject(new Error('No move ID supplied'))
+    }
+
+    return apiClient
+      .find('move', id)
+      .then(response => response.data)
+      .then(move => ({
+        ...move,
+        person: personService.transform(move.person),
+      }))
+  },
+
+  create(data) {
+    return apiClient
+      .create('move', moveService.format(data))
+      .then(response => response.data)
+      .then(move => ({
+        ...move,
+        person: personService.transform(move.person),
+      }))
+  },
+
+  cancel(id) {
+    if (!id) {
+      return Promise.reject(new Error('No move ID supplied'))
+    }
+
+    return apiClient
+      .update('move', {
+        id,
+        status: 'cancelled',
+      })
+      .then(response => response.data)
+  },
 }
 
-function getRequestedMovesByDateAndLocation(moveDate, locationId) {
-  return getMoves({
-    filter: {
-      'filter[status]': 'requested',
-      'filter[date_from]': moveDate,
-      'filter[date_to]': moveDate,
-      'filter[from_location_id]': locationId,
-    },
-  })
-}
-
-function getMoveById(id) {
-  return apiClient
-    .find('move', id)
-    .then(response => response.data)
-    .then(move => ({
-      ...move,
-      person: personService.transform(move.person),
-    }))
-}
-
-function create(data) {
-  return apiClient
-    .create('move', format(data))
-    .then(response => response.data)
-    .then(move => ({
-      ...move,
-      person: personService.transform(move.person),
-    }))
-}
-
-function cancel(id) {
-  if (!id) {
-    return
-  }
-
-  return apiClient
-    .update('move', {
-      id,
-      status: 'cancelled',
-    })
-    .then(response => response.data)
-}
-
-module.exports = {
-  format,
-  getMoves,
-  getRequestedMovesByDateAndLocation,
-  getMoveById,
-  create,
-  cancel,
-}
+module.exports = moveService
