3912b47de858c6461b52fabdbedd1b215b7b79c4
Dom Smith <dom@teneightfive.com>
2019-09-12 17:27:41 +0100

    Group assessment answers by title
    This change updates the way the assessment is grouped for the
    move detail view.
    
    Previously all answers were listed within a category under `items`.
    
    This change groups them by title so that any child items are displayed
    within the panel rather than as separate panels each time.

--- common/presenters/assessment-by-category.js
+++ common/presenters/assessment-by-category.js
@@ -1,11 +1,15 @@
+const { filter, groupBy, sortBy, mapValues } = require('lodash')
+
 const { TAG_CATEGORY_WHITELIST } = require('../../config')
-const { filter, sortBy, mapValues } = require('lodash')
 
 module.exports = function assessmentByCategory(assessment) {
   const mapped = mapValues(TAG_CATEGORY_WHITELIST, (params, category) => {
-    params.category = category
-    params.items = filter(assessment, { category })
-    return params
+    const answers = filter(assessment, { category })
+    return {
+      ...params,
+      key: category,
+      answersByTitle: groupBy(answers, 'title'),
+    }
   })
 
   return sortBy(mapped, 'sortOrder')
