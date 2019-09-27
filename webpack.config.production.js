23e6f878ac17fea5ac0cc361501dab484ce3e90e
Ben Chidgey <ben@feedmypixel.com>
2019-09-27 14:31:10 +0100

    Updates to webpack config
    - Add imagemin-webpack-plugin
    - Set hashed filenames for images and fonts in production
    - Place MiniCssExtractPlugin loader plugin information in relevant
      webpack environment files

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,13 +1,44 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ImageminPlugin = require('imagemin-webpack-plugin').default
 
 module.exports = {
   output: {
     filename: 'javascripts/[name].[contenthash:8].js',
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[contenthash:8].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|ico)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[contenthash:8].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'stylesheets/[name].[contenthash:8].css',
+    }),
+    new ImageminPlugin({
+      test: 'images/**',
     }),
   ],
 }
