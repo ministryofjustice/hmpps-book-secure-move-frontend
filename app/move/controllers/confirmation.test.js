9bef6da6c136f77d4205c88ac3021280b8b97a55
Ben Chidgey <ben@feedmypixel.com>
2019-11-12 14:56:37 +0000

    Send analytics timing hit to google
    This work sends the jounrey duration for the create move journey to
    google analytics

--- app/move/controllers/confirmation.test.js
+++ app/move/controllers/confirmation.test.js
@@ -1,3 +1,5 @@
+const proxyquire = require('proxyquire')
+const sendJourneyTimeStub = sinon.stub().resolves('GA hit sent')
 const controller = require('./confirmation')
 
 const mockMove = {
@@ -6,13 +8,14 @@ const mockMove = {
     title: 'Axminster Crown Court',
   },
 }
+const mockTimestampKey = 'createMoveJourneyTime'
 
 describe('Move controllers', function() {
   describe('#confirmation()', function() {
     describe('with move_type "court_appearance"', function() {
-      let req, res
+      let req, res, nextSpy
 
-      beforeEach(function() {
+      beforeEach(async function() {
         req = {}
         res = {
           render: sinon.spy(),
@@ -20,8 +23,9 @@ describe('Move controllers', function() {
             move: mockMove,
           },
         }
+        nextSpy = sinon.spy()
 
-        controller(req, res)
+        await controller(req, res, nextSpy)
       })
 
       it('should render confirmation template', function() {
@@ -39,9 +43,9 @@ describe('Move controllers', function() {
     })
 
     describe('with move_type "prison_recall"', function() {
-      let req, res
+      let req, res, nextSpy
 
-      beforeEach(function() {
+      beforeEach(async function() {
         req = {
           t: sinon.stub().returnsArg(0),
         }
@@ -54,8 +58,9 @@ describe('Move controllers', function() {
             },
           },
         }
+        nextSpy = sinon.spy()
 
-        controller(req, res)
+        await controller(req, res, nextSpy)
       })
 
       it('should render confirmation template', function() {
@@ -71,6 +76,89 @@ describe('Move controllers', function() {
         expect(req.t.firstCall).to.have.been.calledWithExactly(
           'fields::move_type.items.prison_recall.label'
         )
+      })
+    })
+
+    describe('with Create Move Journey Timestamp', function() {
+      const controller = proxyquire('./confirmation', {
+        '../../../common/lib/analytics': {
+          sendJourneyTime: sendJourneyTimeStub,
+        },
+      })
+      let req, res, nextSpy
+
+      beforeEach(async function() {
+        req = {}
+        res = {
+          render: sinon.spy(),
+          locals: {
+            move: mockMove,
+          },
+        }
+        nextSpy = sinon.spy()
+
+        await controller(req, res, nextSpy)
+      })
+
+      it('should call GA sendJourneyTime method', function() {
+        expect(sendJourneyTimeStub).to.be.calledOnce
+        expect(sendJourneyTimeStub).to.be.calledWithExactly(
+          req,
+          mockTimestampKey,
+          {
+            utv: 'Create a move',
+          }
+        )
+      })
+
+      it('should render confirmation template', function() {
+        const template = res.render.args[0][0]
+
+        expect(res.render.calledOnce).to.be.true
+        expect(template).to.equal('move/views/confirmation')
+      })
+
+      it('should contain a location param', function() {
+        const params = res.render.args[0][1]
+        expect(params).to.have.property('location')
+        expect(params.location).to.equal(mockMove.to_location.title)
+      })
+    })
+
+    describe('when sendJourneyTime fails', function() {
+      const errorMock = new Error('Problem')
+      const controller = proxyquire('./confirmation', {
+        '../../../common/lib/analytics': {
+          sendJourneyTime: sinon.stub().throws(errorMock),
+          '../../config': {
+            ANALYTICS: {
+              GA_ID: '11111',
+            },
+          },
+        },
+      })
+
+      let req, res, nextSpy
+
+      beforeEach(async function() {
+        req = {}
+        res = {
+          render: sinon.spy(),
+          locals: {
+            move: mockMove,
+          },
+        }
+        nextSpy = sinon.spy()
+
+        await controller(req, res, nextSpy)
+      })
+
+      it('should call next with the error', function() {
+        expect(nextSpy).to.be.calledWith(errorMock)
+      })
+
+      it('should call next once', function() {
+        expect(nextSpy).to.be.calledOnce
       })
     })
   })
