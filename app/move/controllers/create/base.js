36464c4b356046adcd91f250a90e38eebe1a64a4
Ben Chidgey <ben@feedmypixel.com>
2019-12-09 16:54:27 +0000

    delete move if it exists when journey canceled
    Because the move has to be created before documents are uploaded to it,
    because documents require a moveId to be uploaded to. A user could
    cancel the move in the Create a move journey after it has been created
    and before documents are uploaded.
    This work will delete a move if it exists and it has been canceled.

--- app/move/controllers/create/base.js
+++ app/move/controllers/create/base.js
@@ -1,4 +1,5 @@
 const { get, set } = require('lodash')
+const moveService = require('../../../../common/services/move')
 const FormWizardController = require('../../../../common/controllers/form-wizard')
 const presenters = require('../../../../common/presenters')
 
@@ -18,6 +19,44 @@ class CreateBaseController extends FormWizardController {
   setCancelUrl(req, res, next) {
     res.locals.cancelUrl = res.locals.MOVES_URL
     next()
+  }
+
+  async post(req, res, next) {
+    /*
+    TODO - upcoming functionality will remove this work & the cancel button
+
+    At present the move is created the step before document uploads. Because
+    we need a move id to upload documents to. This means that if a user cancels
+    on the document uploads page we need to delete the move that has just been
+    created.
+    This is not ideal because the user can exit the page without clicking the
+    "cancel" button. Meaning a move has been created but the user hasn't
+    scheduled it.
+
+    There is work to be played around a "draft" status, so when a move is
+    created, to enable document uploads, it has a status of "draft".
+    Then when a user finishes the journey by clicking "schedule move" the
+    status is set to "requested". We could then not display "drafts" and remove
+    stale "drafts" of a certain age. And also remove this cancel functionality
+    and the cancel button in the view.
+    */
+    const { cancel_move: cancelMove } = req.body
+
+    if (cancelMove && cancelMove === 'cancel') {
+      const { id } = req.sessionModel.get('move') || {}
+
+      try {
+        if (id) {
+          await moveService.destroy(id)
+        }
+      } catch (error) {
+        return next(error)
+      }
+
+      return res.redirect(res.locals.MOVES_URL)
+    }
+
+    super.post(req, res, next)
   }
 
   setJourneyTimer(req, res, next) {
