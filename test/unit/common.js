0e105ad38380fb0e21ecb53259b8780bcd477c40
Ben Chidgey <ben@feedmypixel.com>
2019-12-09 17:55:44 +0000

    Add global helpers to test
    Add requireUncached and mockFs to the global namespace used in tests

--- test/unit/common.js
+++ test/unit/common.js
@@ -1,6 +1,9 @@
+const { requireUncached } = require('./helpers')
+
 const chai = require('chai')
 const sinon = require('sinon')
 const nock = require('nock')
+const mockFs = require('mock-fs')
 
 chai.use(require('sinon-chai'))
 chai.use(require('chai-as-promised'))
@@ -9,3 +12,7 @@ chai.use(require('chai-as-promised'))
 global.expect = chai.expect
 global.sinon = sinon
 global.nock = nock
+global.mockFs = mockFs
+
+// global helpers
+global.requireUncached = requireUncached
