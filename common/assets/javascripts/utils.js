8282a4e1a83ccc6fea59b7899506a3d67996822f
Ben Chidgey <ben@feedmypixel.com>
2019-12-09 17:31:58 +0000

    Add helpers to client side utils
    A number of helpers are needed for the multi file upload component. This
    work adds them in.
    This work also aligns the way of exporting items from this helper file.

--- common/assets/javascripts/utils.js
+++ common/assets/javascripts/utils.js
@@ -3,11 +3,33 @@
  * This seems to fail in IE8, requires more investigation.
  * See: https://github.com/imagitama/nodelist-foreach-polyfill
  */
-export function nodeListForEach(nodes, callback) {
+function nodeListForEach(nodes, callback) {
   if (window.NodeList.prototype.forEach) {
     return nodes.forEach(callback)
   }
   for (var i = 0; i < nodes.length; i++) {
     callback.call(window, nodes[i], i, nodes)
   }
+}
+
+function dragAndDropAvailable() {
+  const div = document.createElement('div')
+  return typeof div.ondrop !== 'undefined'
+}
+
+function formDataAvailable() {
+  return typeof FormData === 'function'
+}
+
+function fileApiAvailable() {
+  const input = document.createElement('input')
+  input.type = 'file'
+  return typeof input.files !== 'undefined'
+}
+
+module.exports = {
+  nodeListForEach,
+  dragAndDropAvailable,
+  formDataAvailable,
+  fileApiAvailable,
 }
