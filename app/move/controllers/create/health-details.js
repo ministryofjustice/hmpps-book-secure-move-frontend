49090cb4dea3eec29ac11282e9d9db0f894df6a2
barbaracassani <barbara@kitsch-en.net>
2020-02-03 18:39:16 +0000

    save details

--- app/move/controllers/create/health-details.js
+++ app/move/controllers/create/health-details.js
@@ -18,12 +18,19 @@ class HealthDetails extends AssessmentController {
   saveValues(req, res, next) {
     if (get(req, 'form.values.health__special_vehicle') === 'yes') {
       const health = get(req, 'form.values.health')
-      const valueOfOriginalQuestion = find(
-        get(req, 'form.options.fields.health.items'),
-        ['key', 'special_vehicle']
-      ).value
-      Array.isArray(health) && health.push(valueOfOriginalQuestion)
-      set(req, 'form.values.health', health)
+      if (Array.isArray(health)) {
+        const valueOfOriginalQuestion = find(
+          get(req, 'form.options.fields.health.items'),
+          ['key', 'special_vehicle']
+        ).value
+        health.push(valueOfOriginalQuestion)
+        set(req, 'form.values.health', health)
+        set(
+          req,
+          'form.values.health__special_vehicle',
+          get(req, 'form.values.type_health__special_vehicle')
+        )
+      }
     }
     return super.saveValues(req, res, next)
   }
