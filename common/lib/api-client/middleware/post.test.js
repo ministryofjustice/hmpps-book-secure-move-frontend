21d73521f17b9a1f6a5b8d57d1d7b8cfddb09574
Dom Smith <dom@teneightfive.com>
2020-02-08 00:06:28 +0000

    Use new API middleware for POST handling
    This middleware replaces the built-in middleware for transforming
    POST requests via devour.
    
    We need to replace is so that if a service calls the devour methods
    using FormData it doesn't try to transform it. Instead it should
    send that as part of the request.

---
+++ common/lib/api-client/middleware/post.test.js
@@ -0,0 +1,99 @@
+const FormData = require('form-data')
+
+const postMiddleware = require('./post')
+
+const originalHeaders = {
+  original: 'header',
+  'content-type': 'application/vnd.api+json',
+}
+
+describe('API Client', function() {
+  describe('POST middleware', function() {
+    describe('#req()', function() {
+      let jsonApi, payload, originalMiddleware, response
+
+      beforeEach(function() {
+        originalMiddleware = {
+          name: 'POST',
+          req: sinon.stub().returnsArg(0),
+        }
+        jsonApi = {
+          _originalMiddleware: [originalMiddleware],
+        }
+        payload = {
+          jsonApi,
+          req: {
+            headers: {
+              ...originalHeaders,
+            },
+            data: {},
+          },
+        }
+      })
+
+      context('when request method is POST', function() {
+        beforeEach(function() {
+          payload.req.method = 'POST'
+        })
+
+        context('when data is instance of FormData', function() {
+          beforeEach(function() {
+            payload.req.data = new FormData()
+            response = postMiddleware.req(payload)
+          })
+
+          it('should update headers', function() {
+            expect(response.req.headers).to.deep.equal({
+              ...payload.req.headers,
+              ...payload.req.data.getHeaders(),
+            })
+          })
+
+          it('should return payload', function() {
+            expect(response).to.deep.equal(payload)
+          })
+
+          it('should not call original middleware', function() {
+            expect(originalMiddleware.req).not.to.be.called
+          })
+        })
+
+        context('when data is not instance of FormData', function() {
+          beforeEach(function() {
+            response = postMiddleware.req(payload)
+          })
+
+          it('should not update headers', function() {
+            expect(response.req.headers).to.deep.equal(originalHeaders)
+          })
+
+          it('should return payload', function() {
+            expect(response).to.deep.equal(payload)
+          })
+
+          it('should call original middleware', function() {
+            expect(originalMiddleware.req).to.calledOnceWithExactly(payload)
+          })
+        })
+      })
+
+      context('when request is not POST', function() {
+        beforeEach(function() {
+          response = postMiddleware.req(payload)
+        })
+
+        it('should not update headers', function() {
+          expect(response.req.headers).to.deep.equal(originalHeaders)
+        })
+
+        it('should return original payload', function() {
+          expect(response).to.deep.equal(payload)
+        })
+
+        it('should call original middleware', function() {
+          expect(originalMiddleware.req).to.calledOnceWithExactly(payload)
+        })
+      })
+    })
+  })
+})
