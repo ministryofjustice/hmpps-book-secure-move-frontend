00bfee6e60a2e79381a3df57ecb6e5ff3aaecb52
Dom Smith <dom@teneightfive.com>
2020-03-03 17:17:35 +0000

    refactor: Update assessment mode to only call field helper
    This refactor moves the logic of setting the fields to the field
    helpers instead of the controller.

--- app/move/controllers/create/assessment.test.js
+++ app/move/controllers/create/assessment.test.js
@@ -1,5 +1,4 @@
 const referenceDataService = require('../../../../common/services/reference-data')
-const createFields = require('../../../move/fields/create')
 const fieldHelpers = require('../../../../common/helpers/field')
 
 const BaseController = require('./base')
@@ -15,101 +14,11 @@ describe('Move controllers', function() {
       beforeEach(function() {
         sinon.stub(BaseController.prototype, 'configure')
         sinon.stub(referenceDataService, 'getAssessmentQuestions')
-        sinon.stub(createFields, 'assessmentCategory')
-        sinon.stub(fieldHelpers, 'mapDependentFields').returnsArg(0)
+        sinon.stub(fieldHelpers, 'populateAssessmentFields')
         nextSpy = sinon.spy()
       })
 
-      context('with explicit fields', function() {
-        const mockQuestionsResponse = [
-          {
-            id: 'e6faaf20-3072-4a65-91f7-93d52b16260f',
-            type: 'assessment_questions',
-            key: 'special_diet_or_allergy',
-            category: 'health',
-            title: 'Special diet or allergy',
-            disabled_at: null,
-          },
-          {
-            id: '1a73d31a-8dd4-47b6-90a0-15ce4e332539',
-            type: 'assessment_questions',
-            key: 'special_vehicle',
-            category: 'health',
-            title: 'Special vehicle',
-            disabled_at: null,
-          },
-        ]
-
-        beforeEach(async function() {
-          referenceDataService.getAssessmentQuestions.resolves(
-            mockQuestionsResponse
-          )
-          req = {
-            form: {
-              options: {
-                assessmentCategory: 'health',
-                fields: {
-                  special_diet_or_allergy: {
-                    skip: true,
-                    rows: 3,
-                    component: 'govukTextarea',
-                    classes: 'govuk-input--width-20',
-                    label: {
-                      text: 'fields::assessment_comment.optional',
-                      classes: 'govuk-label--s',
-                    },
-                  },
-                  special_vehicle: {
-                    skip: true,
-                    rows: 3,
-                    component: 'govukTextarea',
-                    classes: 'govuk-input--width-20',
-                    label: {
-                      text: 'fields::assessment_comment.required',
-                      classes: 'govuk-label--s',
-                    },
-                    validate: 'required',
-                    explicit: true,
-                  },
-                },
-              },
-            },
-          }
-          await controller.configure(req, {}, nextSpy)
-        })
-
-        it('produces the correct result', function() {
-          expect(req.form.options.fields).to.deep.equal(req.form.options.fields)
-        })
-
-        it('invokes getAssessmentQuestions', function() {
-          expect(
-            referenceDataService.getAssessmentQuestions
-          ).to.be.calledOnceWithExactly('health')
-        })
-
-        it('invokes assessmentCategory', function() {
-          expect(createFields.assessmentCategory).to.be.calledOnceWithExactly(
-            'health'
-          )
-        })
-
-        it('invokes mapDependentFields with correct arguments', function() {
-          expect(fieldHelpers.mapDependentFields).to.be.calledOnceWithExactly(
-            req.form.options.fields,
-            mockQuestionsResponse,
-            req.form.options.assessmentCategory
-          )
-        })
-
-        it('should call parent configure method', function() {
-          expect(
-            BaseController.prototype.configure
-          ).to.be.calledOnceWithExactly(req, {}, nextSpy)
-        })
-      })
-
-      context('without explicit fields', function() {
+      context('when getAssessmentQuestions resolves', function() {
         const mockQuestionsResponse = [
           {
             id: 'af8cfc67-757c-4019-9d5e-618017de1617',
@@ -128,8 +37,31 @@ describe('Move controllers', function() {
             disabled_at: null,
           },
         ]
+        const mockPopulateAssessmentFieldsResponse = {
+          foo: 'bar',
+        }
+        const mockFields = {
+          first_names: {
+            name: 'first_names',
+          },
+          violent: {
+            skip: true,
+            rows: 3,
+            component: 'govukTextarea',
+            classes: 'govuk-input--width-20',
+            label: {
+              text: 'fields::assessment_comment.required',
+              classes: 'govuk-label--s',
+            },
+            validate: 'required',
+            explicit: true,
+          },
+        }
 
         beforeEach(async function() {
+          fieldHelpers.populateAssessmentFields.returns(
+            mockPopulateAssessmentFieldsResponse
+          )
           referenceDataService.getAssessmentQuestions.resolves(
             mockQuestionsResponse
           )
@@ -137,37 +69,12 @@ describe('Move controllers', function() {
             form: {
               options: {
                 assessmentCategory: 'risk',
-                fields: {
-                  risk: {
-                    name: 'risk',
-                    items: [],
-                  },
-                  first_names: {
-                    name: 'first_names',
-                  },
-                  violent: {
-                    skip: true,
-                    rows: 3,
-                    component: 'govukTextarea',
-                    classes: 'govuk-input--width-20',
-                    label: {
-                      text: 'fields::assessment_comment.required',
-                      classes: 'govuk-label--s',
-                    },
-                    validate: 'required',
-                  },
-                },
+                fields: mockFields,
               },
             },
           }
 
           await controller.configure(req, {}, nextSpy)
-        })
-
-        it('should not update name field', function() {
-          expect(req.form.options.fields.first_names).to.deep.equal({
-            name: 'first_names',
-          })
         })
 
         it('invokes getAssessmentQuestions', function() {
@@ -176,101 +83,20 @@ describe('Move controllers', function() {
           ).to.be.calledOnceWithExactly('risk')
         })
 
-        it('invokes assessmentCategory', function() {
-          expect(createFields.assessmentCategory).to.be.calledOnceWithExactly(
-            'risk'
-          )
-        })
-
-        it('invokes mapDependentFields with correct arguments', function() {
-          expect(fieldHelpers.mapDependentFields).to.be.calledOnceWithExactly(
-            req.form.options.fields,
-            mockQuestionsResponse,
-            req.form.options.assessmentCategory
-          )
-        })
-
-        it('should call parent configure method', function() {
+        it('should call populateAssessmentFields', function() {
           expect(
-            BaseController.prototype.configure
-          ).to.be.calledOnceWithExactly(req, {}, nextSpy)
+            fieldHelpers.populateAssessmentFields
+          ).to.be.calledOnceWithExactly(mockFields, mockQuestionsResponse)
         })
-      })
 
-      context('without implicit fields', function() {
-        const mockQuestionsResponse = [
-          {
-            id: 'af8cfc67-757c-4019-9d5e-618017de1617',
-            type: 'assessment_questions',
-            key: 'violent',
-            category: 'risk',
-            title: 'Violent',
-            disabled_at: null,
-          },
-          {
-            id: 'f2db9a8f-a5a9-40cf-875b-d1f5f62b2497',
-            type: 'assessment_questions',
-            key: 'escape',
-            category: 'risk',
-            title: 'Escape',
-            disabled_at: null,
-          },
-        ]
-
-        beforeEach(async function() {
-          referenceDataService.getAssessmentQuestions.resolves(
-            mockQuestionsResponse
+        it('should set fields to populateAssessmentFields reponse', function() {
+          expect(req.form.options.fields).to.deep.equal(
+            mockPopulateAssessmentFieldsResponse
           )
-          req = {
-            form: {
-              options: {
-                assessmentCategory: 'risk',
-                fields: {
-                  first_names: {
-                    name: 'first_names',
-                  },
-                  violent: {
-                    skip: true,
-                    rows: 3,
-                    component: 'govukTextarea',
-                    classes: 'govuk-input--width-20',
-                    label: {
-                      text: 'fields::assessment_comment.required',
-                      classes: 'govuk-label--s',
-                    },
-                    validate: 'required',
-                    explicit: true,
-                  },
-                },
-              },
-            },
-          }
-
-          await controller.configure(req, {}, nextSpy)
         })
 
-        it('should not update name field', function() {
-          expect(req.form.options.fields.first_names).to.deep.equal({
-            name: 'first_names',
-          })
-        })
-
-        it('invokes getAssessmentQuestions', function() {
-          expect(
-            referenceDataService.getAssessmentQuestions
-          ).to.be.calledOnceWithExactly('risk')
-        })
-
-        it('not create implicit field', function() {
-          expect(createFields.assessmentCategory).not.to.be.called
-        })
-
-        it('invokes mapDependentFields with correct arguments', function() {
-          expect(fieldHelpers.mapDependentFields).to.be.calledOnceWithExactly(
-            req.form.options.fields,
-            mockQuestionsResponse,
-            req.form.options.assessmentCategory
-          )
+        it('should set questions on request', function() {
+          expect(req.questions).to.deep.equal(mockQuestionsResponse)
         })
 
         it('should call parent configure method', function() {
@@ -581,6 +407,12 @@ describe('Move controllers', function() {
                 key: 'self',
                 category: 'risk',
               },
+              {
+                id: '29f8177c-2cf8-41e8-b1ad-1f66c3a1fda0',
+                type: 'assessment_questions',
+                key: 'bully',
+                category: 'risk',
+              },
             ],
             form: {
               options: {
@@ -593,9 +425,12 @@ describe('Move controllers', function() {
                   'a1f6a3b5-a448-4a78-8cf7-6659a71661c2',
                   '534b05af-8b55-4a37-9f36-d36a60f04aa8',
                 ],
+                violent_explicit: 'Violent comments',
                 violent: 'Violent comments',
                 escape: 'Escape comments',
                 self: 'Self comments',
+                bully_explicit: '29f8177c-2cf8-41e8-b1ad-1f66c3a1fda0',
+                bully: 'Bully comments',
               },
             },
             sessionModel: {
@@ -623,12 +458,16 @@ describe('Move controllers', function() {
                 comments: 'Self comments',
                 assessment_question_id: '534b05af-8b55-4a37-9f36-d36a60f04aa8',
               },
+              {
+                comments: 'Bully comments',
+                assessment_question_id: '29f8177c-2cf8-41e8-b1ad-1f66c3a1fda0',
+              },
             ],
           })
         })
 
         it('should includes all flatten values on assessment_answers property', function() {
-          expect(req.form.values.person.assessment_answers.length).to.equal(3)
+          expect(req.form.values.person.assessment_answers.length).to.equal(4)
           expect(req.form.values.person.assessment_answers).to.deep.equal([
             {
               comments: 'Violent comments',
@@ -641,6 +480,10 @@ describe('Move controllers', function() {
             {
               comments: 'Self comments',
               assessment_question_id: '534b05af-8b55-4a37-9f36-d36a60f04aa8',
+            },
+            {
+              comments: 'Bully comments',
+              assessment_question_id: '29f8177c-2cf8-41e8-b1ad-1f66c3a1fda0',
             },
           ])
         })
