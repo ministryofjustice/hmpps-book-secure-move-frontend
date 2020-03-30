3acf7a977e3b89e9fc2a7680713f6190fed704de
Dom Smith <dom@teneightfive.com>
2020-03-30 15:44:37 +0100

    feat: Use supplier name in confirmation message
    This adds the ability to display the supplier that serves that location
    on the confirmation page.
    
    If there are no suppliers mapped it uses a fallback.

--- app/move/controllers/confirmation.test.js
+++ app/move/controllers/confirmation.test.js
@@ -9,18 +9,22 @@ const mockMove = {
 
 describe('Move controllers', function() {
   describe('#confirmation()', function() {
-    describe('with move_type "court_appearance"', function() {
-      let req, res
+    let req, res
 
+    beforeEach(function() {
+      req = {
+        t: sinon.stub().returnsArg(0),
+      }
+      res = {
+        render: sinon.spy(),
+        locals: {
+          move: mockMove,
+        },
+      }
+    })
+
+    context('by default', function() {
       beforeEach(function() {
-        req = {}
-        res = {
-          render: sinon.spy(),
-          locals: {
-            move: mockMove,
-          },
-        }
-
         controller(req, res)
       })
 
@@ -31,46 +35,114 @@ describe('Move controllers', function() {
         expect(template).to.equal('move/views/confirmation')
       })
 
-      it('should contain a location param', function() {
+      it('should use to location title as location', function() {
         const params = res.render.args[0][1]
         expect(params).to.have.property('location')
         expect(params.location).to.equal(mockMove.to_location.title)
       })
+
+      it('should use supplier fallback as supplier name', function() {
+        const params = res.render.args[0][1]
+        expect(params).to.have.property('supplierName')
+        expect(params.supplierName).to.equal('supplier_fallback')
+      })
+
+      it('should translate supplier fallback key', function() {
+        expect(req.t).to.have.been.calledOnceWithExactly('supplier_fallback')
+      })
     })
 
     describe('with move_type "prison_recall"', function() {
-      let req, res
-
       beforeEach(function() {
-        req = {
-          t: sinon.stub().returnsArg(0),
+        res.locals.move = {
+          ...mockMove,
+          move_type: 'prison_recall',
         }
-        res = {
-          render: sinon.spy(),
-          locals: {
-            move: {
-              ...mockMove,
-              move_type: 'prison_recall',
-            },
+
+        controller(req, res)
+      })
+
+      it('should use translation key as location', function() {
+        const params = res.render.args[0][1]
+        expect(params).to.have.property('location')
+        expect(params.location).to.equal(
+          'fields::move_type.items.prison_recall.label'
+        )
+        expect(req.t.secondCall).to.have.been.calledWithExactly(
+          'fields::move_type.items.prison_recall.label'
+        )
+      })
+    })
+
+    describe('with empty supplier', function() {
+      beforeEach(function() {
+        res.locals.move = {
+          ...mockMove,
+          from_location: {
+            suppliers: [],
           },
         }
 
         controller(req, res)
       })
 
-      it('should render confirmation template', function() {
-        const template = res.render.args[0][0]
-
-        expect(res.render.calledOnce).to.be.true
-        expect(template).to.equal('move/views/confirmation')
+      it('should use supplier fallback as supplier name', function() {
+        const params = res.render.args[0][1]
+        expect(params).to.have.property('supplierName')
+        expect(params.supplierName).to.equal('supplier_fallback')
       })
 
-      it('should pass correct values to location translation', function() {
+      it('should translate supplier fallback key', function() {
+        expect(req.t).to.have.been.calledOnceWithExactly('supplier_fallback')
+      })
+    })
+
+    describe('with one supplier', function() {
+      beforeEach(function() {
+        res.locals.move = {
+          ...mockMove,
+          from_location: {
+            suppliers: [
+              {
+                name: 'Supplier one',
+              },
+            ],
+          },
+        }
+
+        controller(req, res)
+      })
+
+      it('should use first supplier name as supplier param', function() {
         const params = res.render.args[0][1]
-        expect(params).to.have.property('location')
-        expect(req.t.firstCall).to.have.been.calledWithExactly(
-          'fields::move_type.items.prison_recall.label'
-        )
+        expect(params).to.have.property('supplierName')
+        expect(params.supplierName).to.equal('Supplier one')
+      })
+    })
+
+    describe('with multiple suppliers', function() {
+      beforeEach(function() {
+        res.locals.move = {
+          ...mockMove,
+          from_location: {
+            suppliers: [
+              {
+                name: 'Supplier one',
+              },
+              {
+                name: 'Supplier two',
+              },
+            ],
+          },
+        }
+
+        controller(req, res)
+      })
+
+      it('should join supplier names as supplier param', function() {
+        const params = res.render.args[0][1]
+        expect(params).to.have.property('supplierName')
+        expect(params.supplierName).to.equal('Supplier one and Supplier two')
       })
     })
   })
