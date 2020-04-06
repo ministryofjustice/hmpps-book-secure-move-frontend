40611309fa4390322a2cd483af3782181e30f94b
Dom Smith <dom@teneightfive.com>
2020-04-06 12:34:02 +0100

    chore(e2e): Remove unused e2e test helpers
    This cleans up the old methods that are no longer used in the
    end-to-end test helpers.

--- test/e2e/_helpers.js
+++ test/e2e/_helpers.js
@@ -2,57 +2,15 @@ import { find, isArray, isNil } from 'lodash'
 import { format } from 'date-fns'
 import { join } from 'path'
 import { homedir } from 'os'
-import { ClientFunction, Selector, t } from 'testcafe'
+import { ClientFunction, t } from 'testcafe'
 import faker from 'faker'
 import glob from 'glob'
 
 import personService from '../../common/services/person'
 
-/**
- * Get inner text of TestCafe selector
- *
- * @param {Selector} selector - TestCafe selector
- * @returns {Promise<string>} - element inner text
- */
-export const getInnerText = selector => selector.innerText
-
 export const scrollToTop = ClientFunction(() => {
   window.scrollTo(0, 0)
 })
-
-/**
- * Select option from selector
- *
- * @param {Selector} selector
- * @param {string|number} [textOrIndex='random'] - option text or 0-based index or 'random'
- * @param {string} [cssSelector] - CSS selector to pass to ClientFunction. Required when picking a random option
- * @returns {Selector}
- */
-async function selectOption(selector, textOrIndex = 'random', cssSelector) {
-  let option
-  if (typeof textOrIndex === 'string' && textOrIndex !== 'random') {
-    option = selector.withText(textOrIndex)
-  } else if (typeof textOrIndex === 'number') {
-    option = selector.nth(textOrIndex)
-  } else {
-    const randomIndex = await ClientFunction(
-      () => {
-        if (!cssSelector) {
-          return 0
-        }
-        return Math.floor(
-          Math.random() * document.querySelectorAll(cssSelector).length
-        )
-      },
-      { dependencies: { cssSelector } }
-    )()
-    option = selector.nth(randomIndex)
-  }
-
-  await t.click(option)
-
-  return option
-}
 
 export function generatePerson({ pncNumber } = {}) {
   return {
@@ -89,65 +47,6 @@ export async function createPersonFixture() {
 }
 
 /**
- * Select random option from autocomplete menu
- *
- * @param {Selector} selector - An autocomplete field
- * @param {string|number} [optionTextOrIndex] - option text or 0-based index or 'random'.
- * @returns {string} - selected value
- */
-export async function selectAutocompleteOption(selector, optionTextOrIndex) {
-  await t.click(selector)
-
-  const optionsSelector = '.autocomplete__menu .autocomplete__option'
-  const autocompleteMenuOptions = await selector.parent().find(optionsSelector)
-
-  return selectOption(
-    autocompleteMenuOptions,
-    optionTextOrIndex,
-    optionsSelector
-  ).then(getInnerText)
-}
-
-/**
- * Get a checked option input element
- *
- * @param legendText
- * @returns {Promise<*>}
- */
-export async function getSelectedFieldsetOption(legendText) {
-  const optionsFieldset = await Selector('.govuk-fieldset__legend')
-    .withText(legendText)
-    .parent('.govuk-fieldset')
-
-  return Selector(optionsFieldset)
-    .find('.govuk-radios__input')
-    .withAttribute('checked')
-}
-
-/**
- * Select option (radio or checkbox)
- *
- * @param {string} legendText - legend text for the fieldset
- * @param {string|number} [optionLabelTextOrIndex] - option text or 0-based index or 'random'.
- * @returns {Selector}
- */
-export async function selectFieldsetOption(
-  legendText,
-  optionLabelTextOrIndex = 'random'
-) {
-  const optionsFieldset = await Selector('.govuk-fieldset__legend')
-    .withText(legendText)
-    .parent('.govuk-fieldset')
-
-  const optionCssSelector = '.govuk-label'
-  const optionSelector = Selector(optionsFieldset)
-    .find(optionCssSelector)
-    .withText(optionLabelTextOrIndex)
-
-  return selectOption(optionSelector, optionLabelTextOrIndex, optionCssSelector)
-}
-
-/**
  * Select option from selector
  *
  * @param {object} [item]
@@ -156,13 +55,13 @@ export async function selectFieldsetOption(
  *
  * @returns {string|array} - value of the selected item or array of items selected
  */
-async function chooseOption({ options, value }) {
+async function selectOption({ options, value }) {
   if (isArray(value)) {
     const filledInValues = []
 
     for (const itemValue of value) {
       filledInValues.push(
-        await chooseOption({
+        await selectOption({
           options,
           value: itemValue,
         })
@@ -202,7 +101,7 @@ export async function fillAutocomplete({ selector, value }) {
   const optionsSelector = '.autocomplete__menu .autocomplete__option'
   const autocompleteMenuOptions = await selector.parent().find(optionsSelector)
 
-  return chooseOption({
+  return selectOption({
     value,
     options: autocompleteMenuOptions,
   })
@@ -221,7 +120,7 @@ export async function fillRadioOrCheckbox({ selector, value }) {
   const options = selector
     .parent('fieldset')
     .find('[type="radio"] ~ label, [type="checkbox"] ~ label')
-  return chooseOption({
+  return selectOption({
     value,
     options,
   })
@@ -289,18 +188,6 @@ export function getCsvDownloadFilePaths() {
 }
 
 /**
- * Click selector if it exists on page
- *
- * @param selector
- * @returns {Promise<void>}
- */
-export async function clickSelectorIfExists(selector) {
-  if (await selector.exists) {
-    await t.click(selector)
-  }
-}
-
-/**
  * Wait for the csvDownload path to exist. Handy when the files are downloading
  * @param t
  * @param delay
@@ -317,23 +204,3 @@ export async function waitForCsvDownloadFilePaths(t, delay) {
     }
   }
 }
-
-/**
- * Form details object
- *
- * @typedef {object} FormDetails
- * @property {TextFields} [text] - text fields values with field IDs as keys
- * @property {SelectFields} [options] - option field values with field IDs as keys
- */
-
-/**
- * Form details text fields
- *
- * @typedef {Object.<string, string>} TextFields
- */
-
-/**
- * Form details select fields
- *
- * @typedef {Object.<string, string>} SelectFields
- */
