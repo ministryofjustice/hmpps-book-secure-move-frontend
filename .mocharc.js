9682ee81f24cf3e556fbafcd63d29fcb7612d1ec
Dom Smith <dom@teneightfive.com>
2020-04-13 15:56:36 +0100

    chore: Update mocha configuration method
    As of v6.0.0 mocha now supports configuration files and will likely
    deprecate the use of mocha.opts in future releases.
    
    This change updates the way we configure mocha to be ready for any
    future deprecations.

---
+++ .mocharc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  ui: 'bdd',
+  recursive: true,
+  require: 'test/unit/common.js',
+  file: [
+    'test/unit/component-helpers.js',
+    'test/unit/global-helpers.js',
+  ],
+}
