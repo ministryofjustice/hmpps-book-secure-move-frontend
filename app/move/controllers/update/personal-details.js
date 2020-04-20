644a0c412e53252bd1aa4437c38f776b0c549e31
Alex Robinson <github@alex.fu2k.org>
2020-04-20 11:54:20 +0100

    fix: Prevent identifier info being lost when updating person

--- app/move/controllers/update/personal-details.js
+++ app/move/controllers/update/personal-details.js
@@ -1,7 +1,40 @@
 const PersonalDetails = require('../create/personal-details')
 const UpdateBase = require('./base')
 
-class UpdatePersonalDetailsController extends UpdateBase {}
+const personService = require('../../../../common/services/person')
+
+class UpdatePersonalDetailsController extends UpdateBase {
+  async saveValues(req, res, next) {
+    const person = req.getPerson()
+
+    const identifiers = person.identifiers.map(
+      identifier => identifier.identifier_type
+    )
+    const identifiersData = personService.unformat(person, identifiers)
+    req.form.values = Object.assign({}, identifiersData, req.form.values)
+
+    PersonalDetails.prototype.saveValues.apply(this, [req, res, next])
+  }
+
+  // async saveValues(req, res, next) {
+  //   try {
+  //     const id = req.getPersonId()
+  //     const person = req.getPerson()
+
+  //     const identifiers = person.identifiers.map(
+  //       identifier => identifier.identifier_type
+  //     )
+  //     const identifiersData = personService.unformat(person, identifiers)
+  //     const data = Object.assign({}, identifiersData, req.form.values)
+
+  //     req.form.values.person = await this.savePerson(id, data)
+
+  //     next()
+  //   } catch (error) {
+  //     next(error)
+  //   }
+  // }
+}
 
 UpdateBase.mixin(UpdatePersonalDetailsController, PersonalDetails)
 
