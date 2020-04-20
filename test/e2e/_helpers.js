dbc6bc7943a37c33c7aabf03715e2ac00e0967c5
Alex Robinson <github@alex.fu2k.org>
2020-04-20 18:03:42 +0100

    chore: Add e2e tests for editability

--- test/e2e/_helpers.js
+++ test/e2e/_helpers.js
@@ -5,21 +5,46 @@ import { join } from 'path'
 import { format } from 'date-fns'
 import faker from 'faker'
 import glob from 'glob'
-import { find, isArray, isNil } from 'lodash'
-import { ClientFunction, t } from 'testcafe'
+import { find, get, isArray, isNil } from 'lodash'
+import { ClientFunction, RequestLogger, t } from 'testcafe'
 
+import moveService from '../../common/services/move'
 import personService from '../../common/services/person'
+import referenceDataService from '../../common/services/reference-data'
+import { formatDate } from '../../config/nunjucks/filters'
+
+let referenceData
+let genders
+let ethnicities
+let locations
+
+const getReferenceData = async () => {
+  genders = await referenceDataService.getGenders()
+  genders = genders.filter(gender => gender.title.match(/^(Male|Female)$/i))
+  ethnicities = await referenceDataService.getEthnicities()
+  locations = await referenceDataService.getLocations()
+  referenceData = true
+}
 
 export const scrollToTop = ClientFunction(() => {
   window.scrollTo(0, 0)
 })
 
-export function generatePerson(overrides = {}) {
+export async function generatePerson(overrides = {}) {
+  if (!referenceData) {
+    await getReferenceData()
+  }
   const firstNames = faker.name.firstName()
   const lastName = faker.name.lastName()
+  const gender = faker.random.arrayElement(genders.map(({ title }) => title))
+  const ethnicity = faker.random.arrayElement(
+    ethnicities.map(({ title }) => title)
+  )
   return {
     lastName,
     firstNames,
+    gender,
+    ethnicity,
     fullname: `${lastName}, ${firstNames}`.toUpperCase(),
     policeNationalComputer: faker
       .fake('{{random.alphaNumeric(6)}}/{{random.alphaNumeric(2)}}')
@@ -38,8 +63,13 @@ export function generatePerson(overrides = {}) {
   }
 }
 
-export async function createPersonFixture() {
-  const fixture = generatePerson()
+export async function createPersonFixture(overrides = {}) {
+  const fixture = await generatePerson(overrides)
+
+  const gender = genders.filter(gen => gen.title === fixture.gender)[0].id
+  const ethnicity = ethnicities.filter(
+    eth => eth.title === fixture.ethnicity
+  )[0].id
   const person = await personService.create({
     police_national_computer: fixture.policeNationalComputer,
     prison_number: fixture.prisonNumber,
@@ -49,6 +79,8 @@ export async function createPersonFixture() {
     last_name: fixture.lastName,
     first_names: fixture.firstNames,
     date_of_birth: fixture.dateOfBirth,
+    ethnicity,
+    gender,
   })
 
   return {
@@ -57,12 +89,17 @@ export async function createPersonFixture() {
     lastName: person.last_name,
     firstNames: person.first_names,
     dateOfBirth: person.date_of_birth,
+    gender: fixture.gender,
+    ethnicity: fixture.ethnicity,
     prisonNumber: find(person.identifiers, {
       identifier_type: 'prison_number',
     }).value,
-    policeNationalComputer: find(person.identifiers, {
-      identifier_type: 'police_national_computer',
-    }).value,
+    policeNationalComputer: get(
+      find(person.identifiers, {
+        identifier_type: 'police_national_computer',
+      }),
+      'value'
+    ),
     criminalRecordsOffice: find(person.identifiers, {
       identifier_type: 'criminal_records_office',
     }).value,
@@ -73,6 +110,69 @@ export async function createPersonFixture() {
       identifier_type: 'athena_reference',
     }).value,
   }
+}
+
+/**
+ * Get a random location
+ *
+ * @param {string} [locationType=police] - type of location
+ *
+ * @returns {string} - location id
+ */
+const getRandomLocation = (locationType = 'police') => {
+  return faker.random.arrayElement(
+    locations
+      .filter(loc => loc.location_type === locationType)
+      .map(({ id }) => id)
+  )
+}
+
+/**
+ * Generate move data
+ *
+ * @param {object} [moveOverrides] - explicit values to use for move
+ *
+ * @param {object} [moveOptions] - config options for move creation
+ *
+ * @returns {object} - move data
+ */
+export function generateMove(personId, options = {}, overrides = {}) {
+  const fromLocationType = options.from_location_type || 'police'
+  const toLocationType = options.to_location_type || 'court'
+  getRandomLocation()
+  return {
+    person: {
+      id: personId,
+    },
+    from_location: getRandomLocation(fromLocationType),
+    to_location: getRandomLocation(toLocationType),
+    move_type: 'court_appearance',
+    date: formatDate(new Date(), 'yyyy-MM-dd'),
+    ...overrides,
+  }
+}
+
+/**
+ * Create a move object
+ *
+ * @param {object} [personOverrides] - explicit values to use for person
+ *
+ * @param {object} [moveOverrides] - explicit values to use for move
+ *
+ * @param {object} [moveOptions] - config options for move creation
+ *
+ * @returns {object} - created move object containing person data retunred from createPersonFixture
+ */
+export async function createMoveFixture({
+  personOverrides = {},
+  moveOverrides,
+  moveOptions = {},
+} = {}) {
+  const person = await createPersonFixture(personOverrides)
+  const moveFixture = generateMove(person.id, moveOptions, moveOverrides)
+  const move = await moveService.create(moveFixture)
+  move.person = person
+  return move
 }
 
 /**
@@ -246,4 +346,85 @@ export function deleteCsvDownloads() {
       throw new Error(`Failed to delete CSV download file: ${err.message}`)
     }
   }
+}
+
+/**
+ * Create TestCafe RequestLogger
+ *
+ * @param {string} baseUrl - Base url to capture requests
+ *
+ * @returns {object} - logger
+ */
+export function createLogger(baseUrl) {
+  return RequestLogger(request => {
+    if (request.url.match(/\.(js|css|woff|woff2|gif|svg|jpg|png)$/)) {
+      return false
+    }
+    if (request.url.startsWith(`${baseUrl}/connect`)) {
+      return false
+    }
+    if (request.url.startsWith(`${baseUrl}/browser-sync`)) {
+      return false
+    }
+    return request.url.startsWith(baseUrl)
+  })
+}
+
+/**
+ * Get logged response values for a url
+ *
+ * @param {string} url - URL to match
+ *
+ * @returns {object} - logger response object
+ */
+export function getLoggerResponse(logger, url) {
+  const response =
+    logger.requests
+      .filter(req => req.request.url === url)
+      .map(req => req.response)[0] || {}
+  return response
+}
+
+/**
+ * Get status code for a url
+ *
+ * @param {string} url - URL to match
+ *
+ * @returns {number} - status code
+ */
+export function getResponseStatus(logger, url) {
+  return getLoggerResponse(logger, url).statusCode
+}
+
+/**
+ * Check status code for a url
+ *
+ * @param {string} url - URL to check
+ *
+ * @param {number} statusCode - Expected status code
+ *
+ * @param {boolean} [negated] - whether to negate the assertion
+ *
+ * @returns {Promise<boolean>}
+ */
+export async function expectStatusCode(url, statusCode, negated = true) {
+  const logger = createLogger(url)
+  await t.addRequestHooks(logger)
+  await t.navigateTo(url)
+  const expectMethod = negated === false ? 'notOk' : 'ok'
+  await t.expect(getResponseStatus(logger, url) === statusCode)[expectMethod]()
+  await t.removeRequestHooks(logger)
+}
+
+/**
+ * Check whether a url is protected
+ *
+ * @param {string} url - URL to check
+ *
+ * @param {boolean} [negated] - whether to negate the assertion
+ *
+ * @returns {Promise<boolean>}
+ */
+export async function expectForbidden(url, negated) {
+  await expectStatusCode(url, 403, negated)
 }
--- test/e2e/_routes.js
+++ test/e2e/_routes.js
@@ -1,5 +1,19 @@
 import { E2E } from '../../config'
 
+const getUpdatePageStub = page => {
+  const updateMap = {
+    court: 'court-information',
+    health: 'health-information',
+    move_details: 'move-details',
+    personal_details: 'personal-details',
+    risk: 'risk-information',
+  }
+  return updateMap[page]
+}
+
 export const home = E2E.BASE_URL
 export const movesByDay = `${E2E.BASE_URL}/moves`
 export const newMove = `${E2E.BASE_URL}/move/new`
+export const getMove = id => `${E2E.BASE_URL}/move/${id}`
+export const getUpdateMove = (id, page) =>
+  `${E2E.BASE_URL}/move/${id}/edit/${getUpdatePageStub(page)}`
