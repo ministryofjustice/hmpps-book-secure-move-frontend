5f484af51c49d279d78485cc4094f9007bfaee1e
Dom Smith <dom@teneightfive.com>
2020-04-29 09:41:49 +0100

    fix(single requests): Ensure page titles are present and consistent
    This change makes sure a page title exists on the list/dashboard
    pages and makes the language between screens a little more consistent.

--- app/moves/controllers/dashboard.js
+++ app/moves/controllers/dashboard.js
@@ -1,3 +1,5 @@
 module.exports = function dashboard(req, res) {
-  res.render('moves/views/dashboard')
+  res.render('moves/views/dashboard', {
+    pageTitle: 'moves::dashboard.dashboard',
+  })
 }
