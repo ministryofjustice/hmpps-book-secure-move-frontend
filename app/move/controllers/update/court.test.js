604204050a7ae0bf4b0b84f9e1e9cda2303af3c3
Alex Robinson <github@alex.fu2k.org>
2020-05-07 22:28:09 +0100

    chore: Prevent access to update court info route when no court

---
+++ app/move/controllers/update/court.test.js
@@ -0,0 +1,79 @@
+const AssessmentController = require('./assessment')
+const CourtController = require('./court')
+
+const controller = new CourtController({ route: '/' })
+const ownProto = Object.getPrototypeOf(controller)
+
+describe('Move controllers', function() {
+  const req = {}
+  let res
+  let nextSpy
+  beforeEach(function() {
+    res = {}
+    nextSpy = sinon.spy()
+    sinon.stub(AssessmentController.prototype, 'configure')
+  })
+  describe('Update court info controller', function() {
+    it('should extend AssessmentController', function() {
+      expect(Object.getPrototypeOf(ownProto)).to.equal(
+        AssessmentController.prototype
+      )
+    })
+
+    context('#configure', function() {
+      context('when the move is not to a court', function() {
+        let args
+        let error
+        beforeEach(function() {
+          args = []
+          error = {}
+          controller.configure(req, res, nextSpy)
+          try {
+            args = nextSpy.getCall(0).args
+            error = args[0]
+          } catch (e) {}
+        })
+
+        it('should call next once with an error', function() {
+          expect(nextSpy).to.be.calledOnce
+          expect(args.length).to.equal(1)
+        })
+
+        it('should set the correct error message', function() {
+          expect(error.message).to.equal(
+            'Updating court information is not possible for this move'
+          )
+        })
+
+        it('should set the correct error statusCode', function() {
+          expect(error.statusCode).to.equal(404)
+        })
+      })
+
+      context('when the move is to a court', function() {
+        beforeEach(function() {
+          res = {
+            locals: {
+              move: {
+                to_location: {
+                  location_type: 'court',
+                },
+              },
+            },
+          }
+          controller.configure(req, res, nextSpy)
+        })
+
+        it('should not call next', function() {
+          expect(nextSpy).to.not.be.called
+        })
+
+        it('should invoke super’s configure', function() {
+          expect(
+            AssessmentController.prototype.configure
+          ).to.be.calledOnceWithExactly(req, res, nextSpy)
+        })
+      })
+    })
+  })
+})
--- app/move/controllers/update/index.js
+++ app/move/controllers/update/index.js
@@ -1,10 +1,12 @@
 const Assessment = require('./assessment')
+const Court = require('./court')
 const MoveDate = require('./move-date')
 const MoveDetails = require('./move-details')
 const PersonalDetails = require('./personal-details')
 
 module.exports = {
   Assessment,
+  Court,
   MoveDate,
   MoveDetails,
   PersonalDetails,
