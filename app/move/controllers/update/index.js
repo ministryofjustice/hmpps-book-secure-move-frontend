7f8be1e1d4c64290917f0d91aba5f120ffd09a9f
Alex Robinson <github@alex.fu2k.org>
2020-05-12 16:26:36 +0100

    chore: Plumb in update move documents route

--- app/move/controllers/update/index.js
+++ app/move/controllers/update/index.js
@@ -1,5 +1,6 @@
 const Assessment = require('./assessment')
 const Court = require('./court')
+const Document = require('./document')
 const MoveDate = require('./move-date')
 const MoveDetails = require('./move-details')
 const PersonalDetails = require('./personal-details')
@@ -7,6 +8,7 @@ const PersonalDetails = require('./personal-details')
 module.exports = {
   Assessment,
   Court,
+  Document,
   MoveDate,
   MoveDetails,
   PersonalDetails,
