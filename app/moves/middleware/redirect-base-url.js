3fdb49953aa7459a3ffb80656496405f9468882c
barbaracassani <barbara@kitsch-en.net>
2020-05-12 16:28:50 +0100

    feat(allocations): Redirect PMU user to allocations dashboard

--- app/moves/middleware/redirect-base-url.js
+++ app/moves/middleware/redirect-base-url.js
@@ -8,19 +8,26 @@ function redirectBaseUrl(req, res) {
   const today = format(new Date(), dateFormat)
   const currentLocation = get(req.session, 'currentLocation')
 
+  const userPermissions = get(req.session, 'user.permissions')
+
+  const canViewAllocations = permissions.check(
+    'allocations:view',
+    userPermissions
+  )
+  if (canViewAllocations) {
+    return res.redirect(`${req.baseUrl}/allocations`)
+  }
+  const canViewProposedMoves = permissions.check(
+    'moves:view:proposed',
+    userPermissions
+  )
   if (currentLocation) {
-    const userPermissions = get(req.session, 'user.permissions')
-    const canViewProposedMoves = permissions.check(
-      'moves:view:proposed',
-      userPermissions
-    )
     const url =
       canViewProposedMoves && currentLocation.location_type === 'prison'
         ? `${req.baseUrl}/week/${today}/${currentLocation.id}/`
         : `${req.baseUrl}/day/${today}/${currentLocation.id}/outgoing`
     return res.redirect(url)
   }
-
   return res.redirect(`${req.baseUrl}/day/${today}/outgoing`)
 }
 
