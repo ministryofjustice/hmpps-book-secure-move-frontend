33827e3c9c373456dda42546488492b16e106521
Alex Robinson <github@alex.fu2k.org>
2020-05-21 16:42:57 +0100

    feat: Assign person to move
    Copies controllers and templates from create move journey to enable a person to be added to an allocation move
    
    - person search
    - agreement status
    - risk assessment
    - health assessment / special vehicle status
    
    Confrmation page links back to add another person if slots remain unfilled
    
    Error page displayed if person has already been added to a move
    
    P4-1330 - OCA user adds a returned person record to an allocation
    P4-1334 - Present risk information and 'Not for release' prompt to OCA user when adding a person to an allocation

---
+++ app/allocation/steps/assign.js
@@ -0,0 +1,64 @@
+const { create } = require('../../move/steps')
+const controllers = require('../controllers/assign')
+
+const assignSteps = {
+  '/': {
+    entryPoint: true,
+    reset: true,
+    resetJourney: true,
+    skip: true,
+    next: 'person-lookup-prison-number',
+  },
+  '/person-lookup-prison-number': {
+    ...create['/person-lookup-prison-number'],
+    controller: controllers.PersonSearch,
+    next: 'person-lookup-results',
+  },
+  '/person-lookup-results': {
+    ...create['/person-lookup-results'],
+    controller: controllers.PersonSearchResults,
+    next: 'agreement-status',
+  },
+  '/agreement-status': {
+    ...create['/agreement-status'],
+    next: 'release-status',
+  },
+  '/release-status': {
+    ...create['/release-status'],
+    controller: controllers.Assessment,
+    next: 'special-vehicle',
+  },
+  '/special-vehicle': {
+    ...create['/special-vehicle'],
+    controller: controllers.Assessment,
+    pageTitle: 'moves::steps.health_information.heading',
+    fields: ['special_vehicle_check'],
+    next: [
+      {
+        field: 'special_vehicle_check',
+        value: 'true',
+        next: 'no-special-vehicle',
+      },
+      'save',
+    ],
+  },
+  '/no-special-vehicle': {
+    templatePath: 'allocation/views/assign/',
+    template: 'no-special-vehicle',
+    pageTitle: 'moves::steps.person_search.heading',
+  },
+  '/save': {
+    ...create['/save'],
+    controller: controllers.Save,
+    next: 'confirmation',
+  },
+  '/confirmation': {
+    checkJourney: false,
+    checkSession: false,
+    controller: controllers.Confirmation,
+    templatePath: 'allocation/views/assign/',
+    template: 'confirmation',
+  },
+}
+
+module.exports = assignSteps
