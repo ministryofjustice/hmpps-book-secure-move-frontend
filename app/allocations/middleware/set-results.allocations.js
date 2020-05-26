bdea963e3903f728597fc0b865d08f0acb295c12
Dom Smith <dom@teneightfive.com>
2020-05-26 16:06:19 +0100

    feat(allocations): Add progress column to table display
    This adds a new column to the allocations table with the progress of
    the allocation.
    
    The display is different based on the current scenario:
    
    - If a user is viewing one location, it will not show the "From location" column
    - If a user is viewing all locations, it will show the "From location" column
    
    - If a user has permission to assign, it will show number of remaining
    - If a user does not have permission to assign, it will show number of added

--- app/allocations/middleware/set-results.allocations.js
+++ app/allocations/middleware/set-results.allocations.js
@@ -1,7 +1,20 @@
+const { get } = require('lodash')
+
+const permissions = require('../../../common/middleware/permissions')
 const presenters = require('../../../common/presenters')
 const allocationService = require('../../../common/services/allocation')
 
 async function setResultsAllocations(req, res, next) {
+  const currentLocationId = get(req.session, 'currentLocation.id')
+  const userPermissions = get(req.session, 'user.permissions')
+  const displayConfig = {
+    showRemaining: permissions.check(
+      'allocation:person:assign',
+      userPermissions
+    ),
+    showFromLocation: !currentLocationId,
+  }
+
   try {
     const [activeAllocations, cancelledAllocations] = (
       await Promise.all([
@@ -15,8 +28,12 @@ async function setResultsAllocations(req, res, next) {
       cancelled: cancelledAllocations,
     }
     req.resultsAsTable = {
-      active: presenters.allocationsToTable(activeAllocations),
-      cancelled: presenters.allocationsToTable(cancelledAllocations),
+      active: presenters.allocationsToTableComponent(displayConfig)(
+        activeAllocations
+      ),
+      cancelled: presenters.allocationsToTableComponent(displayConfig)(
+        cancelledAllocations
+      ),
     }
 
     next()
