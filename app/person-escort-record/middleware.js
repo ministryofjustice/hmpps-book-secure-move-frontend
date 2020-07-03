c88307e73bdcdee27555a3ccae1989a11e36e972
Dom Smith <dom@teneightfive.com>
2020-07-03 15:52:00 +0100

    chore: Set Person Escort Record on request object
    This uses express param middleware to set the Person Escort Record
    on the request object so that it can be used elsewhere in the request
    cycle.

--- app/person-escort-record/middleware.js
+++ app/person-escort-record/middleware.js
@@ -1,3 +1,5 @@
+const personEscortRecordService = require('../../common/services/person-escort-record')
+
 function setFramework(framework) {
   return (req, res, next) => {
     req.framework = framework
@@ -12,7 +14,21 @@ function setFrameworkSection(frameworkSection) {
   }
 }
 
+async function setPersonEscortRecord(req, res, next, recordId) {
+  if (!recordId) {
+    return next()
+  }
+
+  try {
+    req.personEscortRecord = await personEscortRecordService.getById(recordId)
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
 module.exports = {
   setFramework,
   setFrameworkSection,
+  setPersonEscortRecord,
 }
--- app/person-escort-record/middleware.test.js
+++ app/person-escort-record/middleware.test.js
@@ -1,4 +1,8 @@
+const personEscortRecordService = require('../../common/services/person-escort-record')
+
 const middleware = require('./middleware')
+
+const personEscortRecordStub = { foo: 'bar' }
 
 describe('Person escort record middleware', function () {
   describe('#setFramework()', function () {
@@ -73,6 +77,88 @@ describe('Person escort record middleware', function () {
 
       it('should set framework on request', function () {
         expect(mockReq.frameworkSection).to.deep.equal(mockFramework)
+      })
+    })
+  })
+
+  describe('#setPersonEscortRecord()', function () {
+    const mockRecordId = '12345'
+    const errorStub = new Error('Problem')
+    let mockReq, nextSpy
+
+    beforeEach(function () {
+      mockReq = {}
+      nextSpy = sinon.spy()
+
+      sinon
+        .stub(personEscortRecordService, 'getById')
+        .resolves(personEscortRecordStub)
+    })
+
+    context('when no record ID exists', function () {
+      beforeEach(async function () {
+        await middleware.setPersonEscortRecord(mockReq, {}, nextSpy)
+      })
+
+      it('should call next with no argument', function () {
+        expect(nextSpy).to.be.calledOnceWithExactly()
+      })
+
+      it('should not call API with record ID', function () {
+        expect(personEscortRecordService.getById).not.to.be.called
+      })
+
+      it('should not set response data to locals object', function () {
+        expect(mockReq).not.to.have.property('personEscortRecord')
+      })
+    })
+
+    context('when record ID exists', function () {
+      context('when API call returns succesfully', function () {
+        beforeEach(async function () {
+          await middleware.setPersonEscortRecord(
+            mockReq,
+            {},
+            nextSpy,
+            mockRecordId
+          )
+        })
+
+        it('should call API with record ID', function () {
+          expect(personEscortRecordService.getById).to.be.calledWith(
+            mockRecordId
+          )
+        })
+
+        it('should set response data to locals object', function () {
+          expect(mockReq).to.have.property('personEscortRecord')
+          expect(mockReq.personEscortRecord).to.equal(personEscortRecordStub)
+        })
+
+        it('should call next with no argument', function () {
+          expect(nextSpy).to.be.calledOnceWithExactly()
+        })
+      })
+
+      context('when API call returns an error', function () {
+        beforeEach(async function () {
+          personEscortRecordService.getById.throws(errorStub)
+
+          await middleware.setPersonEscortRecord(
+            mockReq,
+            {},
+            nextSpy,
+            mockRecordId
+          )
+        })
+
+        it('should not set a value on the locals object', function () {
+          expect(mockReq).not.to.have.property('personEscortRecord')
+        })
+
+        it('should send error to next function', function () {
+          expect(nextSpy).to.be.calledOnceWithExactly(errorStub)
+        })
       })
     })
   })
