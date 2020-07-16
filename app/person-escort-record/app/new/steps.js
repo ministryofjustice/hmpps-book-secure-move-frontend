960df2b07d2e82a647429376ec7ee72015d660fd
Dom Smith <dom@teneightfive.com>
2020-07-16 12:10:42 +0100

    fix(per): Remove before you start page when creating a new PER
    This removes the conditional logic we had for before you start pages
    being dependent on location type and needing to occur before creating
    a new Person Escort Record.
    
    The requirement is to have them come in before each section of
    the journey so these have been removed and the creation happens
    immediately. After creation, the user is redirect back to the move
    detail page.

--- app/person-escort-record/app/new/steps.js
+++ app/person-escort-record/app/new/steps.js
@@ -2,17 +2,10 @@ const { NewPersonEscortRecordController } = require('./controllers')
 
 module.exports = {
   '/': {
+    controller: NewPersonEscortRecordController,
     entryPoint: true,
     reset: true,
     resetJourney: true,
     skip: true,
-    next: 'before-you-start',
-  },
-  '/before-you-start': {
-    backLink: null,
-    controller: NewPersonEscortRecordController,
-    pageCaption: 'person-escort-record::heading',
-    pageTitle: 'person-escort-record::create.steps.before_you_start.heading',
-    template: 'start-page',
   },
 }
