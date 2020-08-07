d62766aee9d46af93971330927bbe337a0c3c7ad
Dom Smith <dom@teneightfive.com>
2020-08-07 11:10:22 +0200

    fix: Remove circular dependency when loading routers
    The file that loads the routers for the sub-apps was trying to
    load itself which was causing a circular dependency warning.
    
    This change ensures we only run the map to load routers for directories.
    
    This will remove the warning that appears each time the app is started.

--- app/router.js
+++ app/router.js
@@ -1,21 +1,29 @@
 const fs = require('fs')
 
+const debug = require('debug')('app:router')
 const router = require('express').Router()
 
-const subApps = fs.readdirSync(__dirname)
+const subApps = fs.readdirSync(__dirname, { withFileTypes: true })
 
-const appRouters = subApps.map(subAppDir => {
-  const subApp = require(`./${subAppDir}`)
+const appRouters = subApps
+  .filter(dirent => dirent.isDirectory())
+  .map(({ name }) => {
+    const subApp = require(`./${name}`)
+    debug('Loading app:', name)
 
-  if (subApp.router && !subApp.skip) {
-    if (subApp.mountpath) {
-      return router.use(subApp.mountpath, subApp.router)
+    if (subApp.router && !subApp.skip) {
+      debug('Loading router:', name)
+
+      if (subApp.mountpath) {
+        return router.use(subApp.mountpath, subApp.router)
+      }
+
+      return router.use(subApp.router)
     }
 
-    return router.use(subApp.router)
-  }
+    debug('Skipping router:', name)
 
-  return (req, res, next) => next()
-})
+    return (req, res, next) => next()
+  })
 
 module.exports = appRouters
