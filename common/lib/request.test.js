e16cd60c6e4ce880b8116f31a82a01660563f1a4
Alex Robinson <github@alex.fu2k.org>
2020-08-19 11:15:10 +0100

    chore: Add getUrl helper method to request module

--- common/lib/request.test.js
+++ common/lib/request.test.js
@@ -1,7 +1,18 @@
-const { getQueryString } = require('./request')
+const { getQueryString, getUrl } = require('./request')
 
 describe('Request library', function () {
   describe('#getQueryString()', function () {
+    context('when both queries are empty', function () {
+      it('should return a merged object as an empty string', function () {
+        const requestQuery = {}
+        const newQuery = {}
+
+        const querystring = getQueryString(requestQuery, newQuery)
+
+        expect(querystring).to.equal('')
+      })
+    })
+
     context('when existing query is empty', function () {
       it('should return a merged object as a query string', function () {
         const requestQuery = {}
@@ -45,6 +56,22 @@ describe('Request library', function () {
 
           expect(querystring).to.equal('?foo=world&hello=world')
         })
+      })
+    })
+  })
+
+  describe('#getUrl()', function () {
+    context('when called without query params', function () {
+      it('should output expected url', function () {
+        const url = getUrl('/foo', {})
+        expect(url).to.equal('/foo')
+      })
+    })
+
+    context('when called with query params', function () {
+      it('should output expected url', function () {
+        const url = getUrl('/foo', { bar: 'baz' })
+        expect(url).to.equal('/foo?bar=baz')
       })
     })
   })
