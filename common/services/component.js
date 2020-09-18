88c5aee2fc571e0ae2883896b3cd6b4ab367919f
Dom Smith <dom@teneightfive.com>
2020-09-18 15:05:10 +0100

    chore: Attempt to gain more information from circular Type error
    The production instance is currently throwing an error around the
    `getComponent` with a circular structure. This error cannot be replicated
    locally or any other environment.
    
    This change attempts to provide Sentry with more information of this
    error and to fail more silently, hopefully unblocking users who are
    seeing this in our production environment.

--- common/services/component.js
+++ common/services/component.js
@@ -1,3 +1,4 @@
+const Sentry = require('@sentry/node')
 const { kebabCase } = require('lodash')
 
 const config = require('../../config')
@@ -17,16 +18,30 @@ function _macroNameToFilepath(macroName) {
   return kebabCase(macroName.replace(/^\b(app)/, ''))
 }
 
-function getComponent(macroName, params) {
-  const nunjucksEnv = nunjucksConfig(null, config, configPaths)
-  const macroParams = JSON.stringify(params, null, 2)
-  const filename = _macroNameToFilepath(macroName)
-  const macroString = `
+function getComponent(macroName, params = {}) {
+  try {
+    const nunjucksEnv = nunjucksConfig(null, config, configPaths)
+    const macroParams = JSON.stringify(params, null, 2)
+    const filename = _macroNameToFilepath(macroName)
+    const macroString = `
     {%- from "${filename}/macro.njk" import ${macroName} -%}
     {{- ${macroName}(${macroParams}) -}}
   `
 
-  return nunjucksEnv.renderString(macroString)
+    return nunjucksEnv.renderString(macroString)
+  } catch (error) {
+    Sentry.withScope(scope => {
+      scope.setExtra('macroName', macroName)
+      scope.setExtra('keys', Object.keys(params))
+      scope.setExtra('id', params.id)
+      scope.setExtra('type', params.type)
+      scope.setExtra('version', params.version)
+      scope.setExtra('name', params.name)
+      Sentry.captureException(error)
+    })
+
+    return ''
+  }
 }
 
 module.exports = {
