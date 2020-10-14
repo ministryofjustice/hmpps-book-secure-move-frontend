a43e4f1c472bea82c09323adb0dbbdba810fd21a
Dom Smith <dom@teneightfive.com>
2020-10-14 15:20:14 +0100

    chore: Add new feedback prompt component
    This creates a new component to render a feedback prompt at the
    bottom (usually) of the content area.
    
    This pattern is taken from examples on GOV.UK.

---
+++ common/components/feedback-prompt/template.test.js
@@ -0,0 +1,94 @@
+const {
+  renderComponentHtmlToCheerio,
+  getExamples,
+} = require('../../../test/unit/component-helpers')
+
+const examples = getExamples('feedback-prompt')
+
+describe('Feedback prompt component', function () {
+  context('by default', function () {
+    let $component
+
+    beforeEach(function () {
+      const $ = renderComponentHtmlToCheerio(
+        'feedback-prompt',
+        examples.default
+      )
+      $component = $('.app-feedback-prompt')
+    })
+
+    it('should render component', function () {
+      expect($component.get(0).tagName).to.equal('div')
+    })
+
+    it('should render content', function () {
+      expect(
+        $component.find('.app-feedback-prompt__content').text().trim()
+      ).to.equal(
+        'This is a new feature — give us your feedback to help us improve it'
+      )
+    })
+  })
+
+  context('with classes', function () {
+    it('should render classes', function () {
+      const $ = renderComponentHtmlToCheerio('feedback-prompt', {
+        classes: 'custom-class',
+        content: {
+          text: 'This is a new feature',
+        },
+      })
+      const $component = $('.app-feedback-prompt')
+
+      expect($component.hasClass('custom-class')).to.be.true
+    })
+  })
+
+  context('with content', function () {
+    context('when html is passed to text', function () {
+      it('should render escaped html', function () {
+        const $ = renderComponentHtmlToCheerio('feedback-prompt', {
+          content: {
+            text: 'A message with <strong>bold text</strong>',
+          },
+        })
+
+        const $component = $('.app-feedback-prompt')
+        expect($component.html()).to.contain(
+          'A message with &lt;strong&gt;bold text&lt;/strong&gt;'
+        )
+      })
+    })
+
+    context('when html is passed to html', function () {
+      it('should render unescaped html', function () {
+        const $ = renderComponentHtmlToCheerio('feedback-prompt', {
+          content: {
+            html: 'A message with <strong>bold text</strong>',
+          },
+        })
+
+        const $component = $('.app-feedback-prompt')
+        expect($component.html()).to.contain(
+          'A message with <strong>bold text</strong>'
+        )
+      })
+    })
+
+    context('when both html and text params are used', function () {
+      it('should render unescaped html', function () {
+        const $ = renderComponentHtmlToCheerio('feedback-prompt', {
+          content: {
+            html: 'A message with <strong>bold text</strong>',
+            text: 'A message with <strong>bold text</strong>',
+          },
+        })
+
+        const $component = $('.app-feedback-prompt')
+        expect($component.html()).to.contain(
+          'A message with <strong>bold text</strong>'
+        )
+      })
+    })
+  })
+})
