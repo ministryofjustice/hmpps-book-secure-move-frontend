a0a78764c85fa3b6f28be1f9d29972b7e79cf299
Dom Smith <dom@teneightfive.com>
2020-10-26 13:06:55 +0000

    chore: Create new presenter to render previous framework answer
    This introduces a new presenter that is used to render an answer from
    a previous Person Escort Record.
    
    This will be used within the framework step controller in a later commit.

--- common/helpers/frameworks/index.js
+++ common/helpers/frameworks/index.js
@@ -2,14 +2,16 @@ const appendResponseToField = require('./append-response-to-field')
 const mapFieldFromName = require('./map-field-from-name')
 const reduceResponsesToFormValues = require('./reduce-responses-to-form-values')
 const renderNomisMappingsToField = require('./render-nomis-mappings-to-field')
+const renderPreviousAnswerToField = require('./render-previous-answer-to-field')
 const responsesToSaveReducer = require('./responses-to-save-reducer')
 const setValidationRules = require('./set-validation-rules')
 
 module.exports = {
-  mapFieldFromName,
   appendResponseToField,
+  mapFieldFromName,
   reduceResponsesToFormValues,
   renderNomisMappingsToField,
+  renderPreviousAnswerToField,
   responsesToSaveReducer,
   setValidationRules,
 }
