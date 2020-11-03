72dbc5b54c1c66f7f0db0b95ae1fb6c0b2130bf5
Robin Harrison <merlinc@courts.io>
2020-11-03 17:54:36 +0000

    chore: Cleanup unused constants

--- app/population/constants.js
+++ app/population/constants.js
@@ -4,22 +4,7 @@ const MOUNTPATH = '/population'
 
 const BASE_PATH = `/:period(week|day)/:date(${dateRegex})`
 
-const DEFAULTS = {
-  QUERY: {
-    population: { status: '', sortBy: 'date', sortDirection: 'asc' },
-  },
-  TIME_PERIOD: {
-    population: 'week',
-  },
-}
-
-const FILTERS = {
-  filterTypething: 'prison',
-}
-
 module.exports = {
   BASE_PATH,
-  DEFAULTS,
   MOUNTPATH,
-  FILTERS,
 }
