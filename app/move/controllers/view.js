7028ff7f2bf13854715ee365ddf5dd1ddc98e572
Alex Robinson <github@alex.fu2k.org>
2020-11-23 17:23:17 +0000

    chore: Copy rejected single request rebook prop from event to move
    Rebook info is not currently returned as part of the move.
    
    Therefore we have to get the events for the move (necessitating a second call)
    and fish the MoveRehject event out which does have it.
    
    When the BE returns the rebook property either on the move directly or as
    part of the meta data, this can be removed.

--- app/move/controllers/view.js
+++ app/move/controllers/view.js
@@ -1,6 +1,19 @@
+const moveService = require('../../../common/services/move')
+
 const getViewLocals = require('./view/view.locals')
 
-module.exports = function view(req, res) {
+module.exports = async function view(req, res) {
+  const { id, status, cancellation_reason: cancellationReason } = req.move
+
+  // TODO: remove this second call when backend returns rebook info as part of move
+  if (status === 'cancelled' && cancellationReason === 'rejected') {
+    const moveWithEvents = await moveService.getByIdWithEvents(id)
+    const moveRejectEvent = moveWithEvents.timeline_events.filter(
+      event => event.event_type === 'MoveReject'
+    )[0]
+    req.move.rebook = moveRejectEvent.details.rebook
+  }
+
   const locals = getViewLocals(req)
 
   res.render('move/views/view', locals)
