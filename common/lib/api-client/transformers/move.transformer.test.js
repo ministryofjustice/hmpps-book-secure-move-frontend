6b3327df4effea43d8f08c7041b22efe3804bd03
Alex Robinson <github@alex.fu2k.org>
2020-12-07 15:58:21 +0000

    chore: Update move transformer to deal with important_events
    - create important_events from timeline_events (if present) as BE cannot send both at the same time
    - insert _eventCount property on events to anble them to be moe clearly labelled

--- common/lib/api-client/transformers/move.transformer.test.js
+++ common/lib/api-client/transformers/move.transformer.test.js
@@ -1,3 +1,5 @@
+const { expect } = require('chai')
+
 const transformer = require('./move.transformer')
 
 describe('API Client', function () {
@@ -74,6 +76,87 @@ describe('API Client', function () {
 
         it('should set youth to false', function () {
           expect(item._is_youth_move).to.be.false
+        })
+      })
+
+      describe('when move has timeline events', function () {
+        let move
+        beforeEach(function () {
+          move = {
+            timeline_events: [
+              {
+                id: 'none',
+              },
+              {
+                id: 'default',
+                classification: 'default',
+              },
+              {
+                id: 'medical',
+                classification: 'medical',
+              },
+              {
+                id: 'incident',
+                classification: 'incident',
+              },
+            ],
+          }
+          transformer(move)
+        })
+
+        it('should copy and filter those events by classification to important events', function () {
+          expect(move.important_events).to.deep.equal([
+            {
+              id: 'medical',
+              classification: 'medical',
+            },
+            {
+              id: 'incident',
+              classification: 'incident',
+            },
+          ])
+        })
+      })
+
+      describe('when move has important events', function () {
+        let move
+        beforeEach(function () {
+          move = {
+            important_events: [
+              {
+                id: 'foo1',
+                event_type: 'foo',
+              },
+              {
+                id: 'foo2',
+                event_type: 'foo',
+              },
+              {
+                id: 'bar',
+                event_type: 'bar',
+              },
+            ],
+          }
+          transformer(move)
+        })
+
+        it('should add a count to important events', function () {
+          expect(move.important_events).to.deep.equal([
+            {
+              id: 'foo1',
+              event_type: 'foo',
+              _eventCount: 1,
+            },
+            {
+              id: 'foo2',
+              event_type: 'foo',
+              _eventCount: 2,
+            },
+            {
+              id: 'bar',
+              event_type: 'bar',
+            },
+          ])
         })
       })
     })
