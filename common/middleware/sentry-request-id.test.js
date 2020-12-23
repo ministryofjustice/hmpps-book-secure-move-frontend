c99611ad475cd5fc83ca195b6982707b48d3fd0b
Dom Smith <dom@teneightfive.com>
2020-12-23 23:12:08 +0100

    refactor: Separate request ID and transaction ID in Sentry tags
    Before we were always using the transaction ID for Sentry tags.
    
    But in the case where the request ID doesn't exist, we don't want
    to set it in Sentry, we'd rather leave it unset which shows there
    was no request ID available.
    
    This change separates the two variables and allows them to be set
    independently.

--- common/middleware/sentry-request-id.test.js
+++ common/middleware/sentry-request-id.test.js
@@ -8,11 +8,13 @@ describe('#sentryRequestId', function () {
   beforeEach(function () {
     sinon.stub(Sentry, 'setTag')
     nextSpy = sinon.spy()
-    req = {}
+    req = {
+      get: sinon.stub(),
+    }
     res = {}
   })
 
-  context('without transaction id', function () {
+  context('without any ids', function () {
     beforeEach(function () {
       sentryRequestId(req, res, nextSpy)
     })
@@ -36,8 +38,45 @@ describe('#sentryRequestId', function () {
       expect(nextSpy).to.be.calledOnceWithExactly()
     })
 
+    it('should set transaction ID tag in Sentry', function () {
+      expect(Sentry.setTag).to.be.calledOnceWithExactly(
+        'transaction_id',
+        '12345-aaaaa'
+      )
+    })
+  })
+
+  context('with request id', function () {
+    beforeEach(function () {
+      req.get.withArgs('x-request-id').returns('67890-bbbbb')
+      sentryRequestId(req, res, nextSpy)
+    })
+
+    it('should call next', function () {
+      expect(nextSpy).to.be.calledOnceWithExactly()
+    })
+
     it('should set request ID tag in Sentry', function () {
-      expect(Sentry.setTag).to.be.calledWithExactly('request_id', '12345-aaaaa')
+      expect(Sentry.setTag).to.be.calledOnceWithExactly(
+        'request_id',
+        '67890-bbbbb'
+      )
+    })
+  })
+
+  context('with both ids', function () {
+    beforeEach(function () {
+      req.transactionId = '12345-aaaaa'
+      req.get.withArgs('x-request-id').returns('67890-bbbbb')
+      sentryRequestId(req, res, nextSpy)
+    })
+
+    it('should call next', function () {
+      expect(nextSpy).to.be.calledOnceWithExactly()
+    })
+
+    it('should set request ID tag in Sentry', function () {
+      expect(Sentry.setTag).to.be.calledWithExactly('request_id', '67890-bbbbb')
     })
 
     it('should set transaction ID tag in Sentry', function () {
@@ -45,6 +84,10 @@ describe('#sentryRequestId', function () {
         'transaction_id',
         '12345-aaaaa'
       )
+    })
+
+    it('should set correct number of tags', function () {
+      expect(Sentry.setTag).to.be.calledTwice
     })
   })
 })
