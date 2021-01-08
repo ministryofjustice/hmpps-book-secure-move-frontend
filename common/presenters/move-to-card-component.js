ba27bf2e0dbe82ac6d9698a6294500b872e4d86d
Dom Smith <dom@teneightfive.com>
2021-01-08 15:34:49 +0100

    chore: Add support to show from or to location in the card component
    This adds the ability to show the from location (destination) or
    to location (pickup) in the card component when the move is displayed
    on a dashboard or within an allocation.
    
    This change is required to be able to change the grouping of moves
    on the dashboard from location to vehicle.

--- common/presenters/move-to-card-component.js
+++ common/presenters/move-to-card-component.js
@@ -9,14 +9,39 @@ function moveToCardComponent({
   showMeta = true,
   showTags = true,
   showStatus = true,
+  showToLocation = false,
+  showFromLocation = false,
   hrefSuffix = '',
 } = {}) {
   return function item(move) {
-    const { id, reference, profile, status } = move
+    const {
+      id,
+      reference,
+      profile,
+      status,
+      to_location: toLocation,
+      from_location: fromLocation,
+    } = move
     const href = profile ? `/move/${id}${hrefSuffix}` : ''
     const excludedBadgeStatuses = ['cancelled']
 
     showTags = isCompact ? false : showTags
+
+    const moveMetaItems = []
+
+    if (showFromLocation) {
+      moveMetaItems.push({
+        label: i18n.t('fields::from_location.label'),
+        text: fromLocation?.title,
+      })
+    }
+
+    if (showToLocation) {
+      moveMetaItems.push({
+        label: i18n.t('fields::to_location.label'),
+        text: toLocation?.title,
+      })
+    }
 
     const showStatusBadge =
       showStatus && !excludedBadgeStatuses.includes(status) && !isCompact
@@ -24,6 +49,7 @@ function moveToCardComponent({
       ? { text: i18n.t(`statuses::${status}`) }
       : undefined
     const personCardComponent = profileToCardComponent({
+      meta: moveMetaItems,
       showImage: isCompact ? false : showImage,
       showMeta: isCompact ? false : showMeta,
       showTags,
