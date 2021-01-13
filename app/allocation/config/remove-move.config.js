3c5866f5466cb8d8f876813565c8e546671daefa
Dom Smith <dom@teneightfive.com>
2021-01-13 12:51:56 +0100

    chore: Remove controllers from existing config
    This is now covered by the unique form wizard so can be removed
    from individual configs.

--- app/allocation/config/remove-move.config.js
+++ app/allocation/config/remove-move.config.js
@@ -1,8 +1,5 @@
-const FormWizardController = require('../../../common/controllers/form-wizard')
-
 module.exports = function config(id) {
   return {
-    controller: FormWizardController,
     name: `remove-move-from-allocation-${id}`,
     templatePath: 'allocation/views/cancel/',
     template: '../../../form-wizard',
