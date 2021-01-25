d8bb80131467e926c3e18b9ea48044ad8540afcf
Dom Smith <dom@teneightfive.com>
2021-01-25 11:10:27 +0000

    test: Extend STC tests to cater for all scenarios
    The previous tests for Secure Training Centres (STC) only created a move
    from that location type to court.
    
    However, there are a number of other scenarios that weren't tested and
    that created different banners on the detail page.
    
    This change extends the tests to cater for the other scenarios.

--- test/e2e/move.new.stc.test.js
+++ test/e2e/move.new.stc.test.js
@@ -35,7 +35,6 @@ test('With a new person', async t => {
   await page.submitForm()
 
   // Confirmation page
-
   await createMovePage.checkConfirmationStep({
     fullname: personalDetails.fullname,
     location: moveDetails.courtLocation,
@@ -50,4 +49,174 @@ test('With a new person', async t => {
 
   // Check assessment
   await moveDetailPage.checkCourtInformation(courtInformation)
+
+  // Check banner
+  await t
+    .expect(moveDetailPage.nodes.instructionBanner.innerText)
+    .contains(
+      'Start the Youth Risk Assessment',
+      'Should contain start assessment banner'
+    )
+})
+
+fixture('New move from Secure Training Centre (STC) to Prison').beforeEach(
+  async t => {
+    await t.useRole(stcUser).navigateTo(newMove)
+  }
+)
+
+test('With a new person', async t => {
+  // PNC lookup
+  await t
+    .expect(page.getCurrentUrl())
+    .contains('/move/new/person-lookup-pnc')
+    .click(createMovePage.steps.personLookup.nodes.noIdentifierLink)
+    .click(createMovePage.steps.personLookup.nodes.moveSomeoneNew)
+
+  // Personal details
+  const personalDetails = await createMovePage.fillInPersonalDetails()
+  await page.submitForm()
+
+  // Move details
+  const moveDetails = await createMovePage.fillInMoveDetails('Prison')
+  await page.submitForm()
+
+  // Move date
+  await createMovePage.fillInDateRange()
+  await page.submitForm()
+
+  // Transfer reason
+  await createMovePage.fillInPrisonTransferReasons()
+  await page.submitForm()
+
+  // Agreement status
+  await createMovePage.fillInAgreementStatus()
+  await page.submitForm()
+
+  // Confirmation page
+  await createMovePage.checkConfirmationStep({
+    fullname: personalDetails.fullname,
+    location: moveDetails.prisonLocation,
+  })
+  await t.click(Selector('a').withExactText(personalDetails.fullname))
+
+  // Move detail assertions
+  await moveDetailPage.checkHeader({ fullname: personalDetails.fullname })
+
+  // Personal details assertions
+  await moveDetailPage.checkPersonalDetails(personalDetails)
+
+  // Check banner
+  await t
+    .expect(moveDetailPage.nodes.instructionBanner.innerText)
+    .contains('Pending review', 'Should show that the move is pending a review')
+    .expect(moveDetailPage.nodes.instructionBanner.innerText)
+    .contains('Placement team', 'Should include the correct review team')
+})
+
+fixture('New move from Secure Training Centre (STC) to Hospital').beforeEach(
+  async t => {
+    await t.useRole(stcUser).navigateTo(newMove)
+  }
+)
+
+test('With a new person', async t => {
+  // PNC lookup
+  await t
+    .expect(page.getCurrentUrl())
+    .contains('/move/new/person-lookup-pnc')
+    .click(createMovePage.steps.personLookup.nodes.noIdentifierLink)
+    .click(createMovePage.steps.personLookup.nodes.moveSomeoneNew)
+
+  // Personal details
+  const personalDetails = await createMovePage.fillInPersonalDetails()
+  await page.submitForm()
+
+  // Move details
+  const moveDetails = await createMovePage.fillInMoveDetails('Hospital')
+  await page.submitForm()
+
+  // Move date
+  await createMovePage.fillInDate()
+  await page.submitForm()
+
+  // Transfer reason
+  await createMovePage.fillInHospitalDetails()
+  await page.submitForm()
+
+  // Confirmation page
+  await createMovePage.checkConfirmationStep({
+    fullname: personalDetails.fullname,
+    location: moveDetails.hospitalLocation,
+  })
+  await t.click(Selector('a').withExactText(personalDetails.fullname))
+
+  // Move detail assertions
+  await moveDetailPage.checkHeader({ fullname: personalDetails.fullname })
+
+  // Personal details assertions
+  await moveDetailPage.checkPersonalDetails(personalDetails)
+
+  // Check banner
+  await t
+    .expect(moveDetailPage.nodes.instructionBanner.innerText)
+    .contains(
+      'Start the Youth Risk Assessment',
+      'Should contain start assessment banner'
+    )
+})
+
+fixture(
+  'New move from Secure Training Centre (STC) to Secure Childrens Home (SCH)'
+).beforeEach(async t => {
+  await t.useRole(stcUser).navigateTo(newMove)
+})
+
+test('With a new person', async t => {
+  // PNC lookup
+  await t
+    .expect(page.getCurrentUrl())
+    .contains('/move/new/person-lookup-pnc')
+    .click(createMovePage.steps.personLookup.nodes.noIdentifierLink)
+    .click(createMovePage.steps.personLookup.nodes.moveSomeoneNew)
+
+  // Personal details
+  const personalDetails = await createMovePage.fillInPersonalDetails()
+  await page.submitForm()
+
+  // Move details
+  const moveDetails = await createMovePage.fillInMoveDetails('SCH')
+  await page.submitForm()
+
+  // Move date
+  await createMovePage.fillInDateRange()
+  await page.submitForm()
+
+  // Transfer reason
+  await createMovePage.fillInPrisonTransferReasons()
+  await page.submitForm()
+
+  // Agreement status
+  await createMovePage.fillInAgreementStatus()
+  await page.submitForm()
+
+  // Confirmation page
+  await createMovePage.checkConfirmationStep({
+    fullname: personalDetails.fullname,
+    location: moveDetails.secureChildrensHomeLocation,
+  })
+  await t.click(Selector('a').withExactText(personalDetails.fullname))
+
+  // Move detail assertions
+  await moveDetailPage.checkHeader({ fullname: personalDetails.fullname })
+
+  // Personal details assertions
+  await moveDetailPage.checkPersonalDetails(personalDetails)
+
+  // Check banner
+  await t
+    .expect(moveDetailPage.nodes.instructionBanner.innerText)
+    .contains('Pending review', 'Should show that the move is pending a review')
+    .expect(moveDetailPage.nodes.instructionBanner.innerText)
+    .contains('Placement team', 'Should include the correct review team')
 })
