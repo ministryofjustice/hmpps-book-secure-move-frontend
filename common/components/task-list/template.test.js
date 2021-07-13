bf35a15c032fc9093864b8b4a7fce5ae9a563633
Dom Smith <dom@teneightfive.com>
2021-07-13 13:21:21 +0100

    feat: Extend task list component to support sections
    This change adds the ability to group tasks within sections for the
    Task List Component.
    
    Previously it only supported showing tasks rather than the ability to
    group them.
    
    This change follows a similar pattern to the MOJ Design System but
    maintains the flexibility to apply different tags and labels.
    https://moj-design-system.herokuapp.com/components/task-list

--- common/components/task-list/template.test.js
+++ common/components/task-list/template.test.js
@@ -11,7 +11,7 @@ describe('Task list component', function () {
 
     beforeEach(function () {
       $ = renderComponentHtmlToCheerio('task-list', examples.default)
-      $component = $('.app-task-list')
+      $component = $('.app-task-list__items')
       items = $component.find('.app-task-list__item')
     })
 
@@ -57,7 +57,7 @@ describe('Task list component', function () {
 
     beforeEach(function () {
       $ = renderComponentHtmlToCheerio('task-list', examples['with tags'])
-      $component = $('.app-task-list')
+      $component = $('.app-task-list__items')
     })
 
     it('should render component', function () {
@@ -155,7 +155,7 @@ describe('Task list component', function () {
         ],
       })
 
-      const $component = $('.app-task-list')
+      const $component = $('.app-task-list__items')
       expect($component.html()).to.contain(
         'A title with &lt;strong&gt;bold text&lt;/strong&gt;'
       )
@@ -172,7 +172,7 @@ describe('Task list component', function () {
         ],
       })
 
-      const $component = $('.app-task-list')
+      const $component = $('.app-task-list__items')
       expect($component.html()).to.contain(
         'A title with <strong>bold text</strong>'
       )
@@ -190,10 +190,208 @@ describe('Task list component', function () {
         ],
       })
 
-      const $component = $('.app-task-list')
+      const $component = $('.app-task-list__items')
       expect($component.html()).to.contain(
         'A title with <strong>bold text</strong>'
       )
+    })
+  })
+
+  context('with sections', function () {
+    let $, $component, $sections
+
+    beforeEach(function () {
+      $ = renderComponentHtmlToCheerio('task-list', examples['with sections'])
+      $component = $('.app-task-list')
+      $sections = $component.find('.app-task-list__section')
+    })
+
+    it('should render component', function () {
+      expect($component.get(0).tagName).to.equal('ol')
+    })
+
+    describe('sections', function () {
+      let sectionHeadings
+
+      beforeEach(function () {
+        sectionHeadings = $component.find('.app-task-list__section-heading')
+      })
+
+      it('should render correct number of sections', function () {
+        expect($sections.length).to.equal(2)
+      })
+
+      it('should render sections headings correctly', function () {
+        expect($(sectionHeadings[0]).text().trim()).to.equal(
+          '1. Request a move'
+        )
+        expect($(sectionHeadings[1]).text().trim()).to.equal(
+          '2. Complete assessment'
+        )
+      })
+    })
+
+    describe('items', function () {
+      let items
+
+      beforeEach(function () {
+        items = $component.find(
+          '.app-task-list__section .app-task-list__task-name'
+        )
+      })
+
+      it('should render correct number of items', function () {
+        expect(items.length).to.equal(5)
+      })
+
+      it('should render items correctly', function () {
+        expect($(items[0]).text().trim()).to.equal('Personal details')
+        expect($(items[1]).text().trim()).to.equal(
+          'Provide booking information'
+        )
+        expect($(items[2]).text().trim()).to.equal('Health information')
+        expect($(items[3]).text().trim()).to.equal('Risk information')
+        expect($(items[4]).text().trim()).to.equal('Confirm answers')
+      })
+    })
+
+    describe('links', function () {
+      let links
+
+      beforeEach(function () {
+        links = $component.find(
+          '.app-task-list__section .app-task-list__task-name a'
+        )
+      })
+
+      it('should contain hrefs for items with href attribute', function () {
+        expect(links.length).to.equal(4)
+      })
+
+      it('should not contain hrefs for items without href attribute', function () {
+        const items = $component.find('.app-task-list__task-name')
+        expect($(items[4]).find('a').length).to.equal(0)
+      })
+    })
+
+    describe('tags', function () {
+      let tags
+
+      beforeEach(function () {
+        tags = $component.find(
+          '.app-task-list__section .app-task-list__item .govuk-tag'
+        )
+      })
+
+      it('should contain correct number of tags', function () {
+        expect(tags.length).to.equal(5)
+      })
+
+      it('should render tag text', function () {
+        expect($(tags[0]).text().trim()).to.equal('Completed')
+        expect($(tags[1]).text().trim()).to.equal('Completed')
+        expect($(tags[2]).text().trim()).to.equal('Incomplete')
+        expect($(tags[3]).text().trim()).to.equal('Not started')
+        expect($(tags[4]).text().trim()).to.equal('Cannot start yet')
+      })
+
+      it('should render tag classes', function () {
+        expect($(tags[0]).attr('class')).to.equal(
+          'govuk-tag  app-task-list__tag'
+        )
+        expect($(tags[1]).attr('class')).to.equal(
+          'govuk-tag  app-task-list__tag'
+        )
+        expect($(tags[2]).attr('class')).to.equal(
+          'govuk-tag govuk-tag--blue app-task-list__tag'
+        )
+        expect($(tags[3]).attr('class')).to.equal(
+          'govuk-tag govuk-tag--grey app-task-list__tag'
+        )
+        expect($(tags[4]).attr('class')).to.equal(
+          'govuk-tag govuk-tag--grey app-task-list__tag'
+        )
+      })
+
+      it('should render tag ids', function () {
+        expect($(tags[0]).attr('id')).to.equal('Personal%20details-status')
+        expect($(tags[1]).attr('id')).to.equal(
+          'Provide%20booking%20information-status'
+        )
+        expect($(tags[2]).attr('id')).to.equal('Health%20information-status')
+        expect($(tags[3]).attr('id')).to.equal('Risk%20information-status')
+        expect($(tags[4]).attr('id')).to.equal('Confirm%20answers-status')
+      })
+    })
+  })
+
+  context('with sections and items', function () {
+    let $, $component, $sections, $itemLists, $items
+
+    beforeEach(function () {
+      $ = renderComponentHtmlToCheerio('task-list', {
+        sections: [
+          {
+            heading: {
+              text: 'A section heading',
+            },
+            items: [
+              {
+                text: 'A section item title',
+              },
+              {
+                text: 'Another section item title',
+              },
+            ],
+          },
+          {
+            heading: {
+              text: 'Another section heading',
+            },
+            items: [
+              {
+                text: 'Second section item title',
+              },
+              {
+                text: 'Another second section item title',
+              },
+              {
+                text: 'A third, second section item title',
+              },
+            ],
+          },
+        ],
+        items: [
+          {
+            text: 'An item title',
+          },
+          {
+            text: 'Another item title',
+          },
+        ],
+      })
+      $component = $('.app-task-list')
+      $sections = $component.find('.app-task-list__section')
+      $itemLists = $component.find(
+        '.app-task-list__section .app-task-list__items'
+      )
+      $items = $component.find('.app-task-list__section .app-task-list__item')
+    })
+
+    it('should render task list component', function () {
+      expect($component.get(0).tagName).to.equal('ol')
+    })
+
+    it('should render correct number of sections', function () {
+      expect($sections.length).to.equal(2)
+    })
+
+    it('should render correct number of item lists', function () {
+      expect($itemLists.length).to.equal(2)
+    })
+
+    it('should render correct number of items', function () {
+      expect($items.length).to.equal(5)
     })
   })
 })
