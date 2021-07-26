48fc85be157f820c49abd92a4a932e266c55463c
Dom Smith <dom@teneightfive.com>
2021-07-26 20:12:44 +0100

    fix: Ensure cache is cleared on errors during deserialization
    Currently devour, the JSON:API client will build a cached of
    deserialized resources. We have added a transformation layer to help
    with certain things the frontend requires for these resources.
    
    However, if an error occurs, the cache remains in tact which will
    mean that in some cases, on a refresh of the page the app will use
    the cached version.
    
    This can lead to some unexpected renders so this fix calls the `clear()`
    method on the cache to ensure on the next request it will begin again
    from the start.

--- common/lib/api-client/transformers/transform-resource.js
+++ common/lib/api-client/transformers/transform-resource.js
@@ -2,23 +2,28 @@ const { cloneDeep } = require('lodash')
 
 module.exports = function transformResource(transformer) {
   return function deserializer(item, included) {
-    const _this = cloneDeep(this)
-    const modelName = _this.pluralize.singular(item.type)
+    try {
+      const _this = cloneDeep(this)
+      const modelName = _this.pluralize.singular(item.type)
 
-    // Ensure we remove this deserializer to avoid infinite loop
-    delete _this.models[modelName].options.deserializer
+      // Ensure we remove this deserializer to avoid infinite loop
+      delete _this.models[modelName].options.deserializer
 
-    // call devour deserializer
-    const deserializedData = _this.deserialize.resource.call(
-      _this,
-      item,
-      included
-    )
+      // call devour deserializer
+      const deserializedData = _this.deserialize.resource.call(
+        _this,
+        item,
+        included
+      )
 
-    if (transformer) {
-      transformer(deserializedData)
+      if (transformer) {
+        transformer(deserializedData)
+      }
+
+      return deserializedData
+    } catch (error) {
+      this.deserialize.cache.clear()
+      throw error
     }
-
-    return deserializedData
   }
 }
--- common/lib/api-client/transformers/transform-resource.test.js
+++ common/lib/api-client/transformers/transform-resource.test.js
@@ -24,6 +24,7 @@ describe('API Client', function () {
                 },
               ],
               set: sinon.stub().returnsArg(1),
+              clear: sinon.stub().returns(true),
             },
             resource: sinon.stub().returnsArg(0),
           },
@@ -111,6 +112,34 @@ describe('API Client', function () {
         it('should delete model deserializer', function () {
           const _that = _this.deserialize.resource.firstCall.thisValue
           expect(_that.models.book.options.deserializer).to.be.undefined
+        })
+      })
+
+      context('when transformer throws an error', function () {
+        const errorMock = new Error('Transformer error')
+        let transformer
+
+        beforeEach(function () {
+          transformer = sinon.stub().throws(errorMock)
+
+          _this.models.book.options.deserializer = transformer
+        })
+
+        it('should throw error', function () {
+          expect(() =>
+            transformResource(transformer).call(_this, item, included)
+          ).to.throw(errorMock)
+        })
+
+        it('should clear the cache', function (done) {
+          try {
+            transformResource(transformer).call(_this, item, included)
+          } catch {
+            expect(
+              _this.deserialize.cache.clear
+            ).to.have.been.calledOnceWithExactly()
+            done()
+          }
         })
       })
     })
