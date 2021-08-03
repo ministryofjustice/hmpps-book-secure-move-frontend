089dcf59c7d7f4adf8e0e780fccc83f74ce5a352
Thomas Leese <thomas@leese.io>
2021-08-03 08:17:45 +0100

    refactor: Include personId in mountpath
    This avoids needing to specify this prefix for each of the routes. It
    copies a pattern we're using on the new move pages to avoid duplication
    when defining the routes. For this to work I had to set
    `mergeParams: true` on the router.

--- app/person/index.js
+++ app/person/index.js
@@ -1,5 +1,6 @@
-const router = require('express').Router()
+const router = require('express').Router({ mergeParams: true })
 
+const { uuidRegex } = require('../../common/helpers/url')
 const breadcrumbs = require('../../common/middleware/breadcrumbs')
 const { PLACEHOLDER_IMAGES } = require('../../config')
 
@@ -8,10 +9,10 @@ const { setPerson } = require('./middleware')
 
 router.use(breadcrumbs.setHome())
 
-router.get('/:personId', setPerson, renderPerson)
-router.get('/:personId/image', image(PLACEHOLDER_IMAGES.PERSON))
+router.get('/', setPerson, renderPerson)
+router.get('/image', image(PLACEHOLDER_IMAGES.PERSON))
 
 module.exports = {
   router,
-  mountpath: '/person',
+  mountpath: `/person/:personId(${uuidRegex})`,
 }
