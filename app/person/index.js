46c1de96226e18f6a18502cf452b65c3946dcc26
Thomas Leese <thomas@leese.io>
2021-08-03 08:16:20 +0100

    refactor: Remove unnecessary comments
    I'm not sure what they add as the code itself should be clear enough to
    read without them.

--- app/person/index.js
+++ app/person/index.js
@@ -1,7 +1,5 @@
-// NPM dependencies
 const router = require('express').Router()
 
-// Local dependencies
 const breadcrumbs = require('../../common/middleware/breadcrumbs')
 const { PLACEHOLDER_IMAGES } = require('../../config')
 
@@ -10,11 +8,9 @@ const { setPerson } = require('./middleware')
 
 router.use(breadcrumbs.setHome())
 
-// Define routes
 router.get('/:personId', setPerson, renderPerson)
 router.get('/:personId/image', image(PLACEHOLDER_IMAGES.PERSON))
 
-// Export
 module.exports = {
   router,
   mountpath: '/person',
