19c94c7244ee4078a7b429b37e570fcd4bbd37c2
Thomas Leese <thomas@leese.io>
2021-08-04 10:35:37 +0100

    chore: Don't only record performance data in production
    Having the data in the other environments allows us to be aware of any
    potential problems if, for example, a new release in staging suddenly
    causes requests to take longer to respond.
    
    This shouldn't increase the number of traces we record and therefore
    shouldn't affect rate limits. It also reduces differences in the app in
    different environments which makes debugging easier.

--- server.js
+++ server.js
@@ -70,17 +70,12 @@ if (config.SENTRY.DSN) {
     dsn: config.SENTRY.DSN,
     environment: config.SENTRY.ENVIRONMENT,
     release: config.SENTRY.RELEASE,
-    integrations:
-      config.SENTRY.ENVIRONMENT === 'production'
-        ? [
-            // enable HTTP calls tracing
-            new Sentry.Integrations.Http({ tracing: true }),
-            // enable Express.js middleware tracing
-            new Tracing.Integrations.Express({
-              app,
-            }),
-          ]
-        : [],
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Tracing.Integrations.Express({ app }),
+    ],
     // Quarter of all requests will be used for performance sampling
     tracesSampler: samplingContext => {
       const transactionName =
@@ -88,9 +83,7 @@ if (config.SENTRY.DSN) {
         samplingContext.transactionContext &&
         samplingContext.transactionContext.name
 
-      if (config.SENTRY.ENVIRONMENT !== 'production') {
-        return 0
-      } else if (transactionName && transactionName.includes('ping')) {
+      if (transactionName && transactionName.includes('ping')) {
         return 0
       } else {
         // Default sample rate
@@ -108,9 +101,7 @@ if (config.SENTRY.DSN) {
   )
   app.use(sentryRequestId)
 
-  if (config.SENTRY.ENVIRONMENT === 'production') {
-    app.use(Sentry.Handlers.tracingHandler())
-  }
+  app.use(Sentry.Handlers.tracingHandler())
 }
 
 // Configure prometheus to handle metrics
