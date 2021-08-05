6060fc9f42c252f1211ac711102b92389b1ef530
Thomas Leese <thomas@leese.io>
2021-08-05 09:57:04 +0100

    feat: Add a middleware for person moves
    This adds a middleware which fetches the moves for a person from the API
    ready to display in a table. At the moment it doesn't do any sort of
    pagination or anything like that.

--- app/person/middleware/index.js
+++ app/person/middleware/index.js
@@ -1,6 +1,13 @@
 const localsIdentityBar = require('./locals.identity-bar')
 const localsTabs = require('./locals.tabs')
 const setBreadcrumb = require('./set-breadcrumb')
+const setMoveResults = require('./set-move-results')
 const setPerson = require('./set-person')
 
-module.exports = { localsIdentityBar, localsTabs, setBreadcrumb, setPerson }
+module.exports = {
+  localsIdentityBar,
+  localsTabs,
+  setBreadcrumb,
+  setMoveResults,
+  setPerson,
+}
