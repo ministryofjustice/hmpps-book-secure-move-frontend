4e19acbc017023f12c8ddcdf3773a60a17f83c77
Dom Smith <dom@teneightfive.com>
2021-08-06 09:39:43 +0200

    chore: Add the ability to allow a user to opt out of preview move design
    This creates the option for a user to still opt out of the preview
    design after opting in, in case they have changed their mind.

--- app/move/app/view/middleware/locals.actions.js
+++ app/move/app/view/middleware/locals.actions.js
@@ -1,23 +1,29 @@
-function setActions(req, res, next) {
-  const { canAccess, move = {} } = req
+function setActions({ previewPrefix = '' } = {}) {
+  return (req, res, next) => {
+    const { canAccess, move = {} } = req
 
-  const actions = [
-    {
-      text: 'actions::view_journeys',
-      url: canAccess('move:view:journeys')
-        ? `/move/${move.id}/journeys`
-        : undefined,
-    },
-    {
-      text: 'actions::cancel_move',
-      classes: 'app-link--destructive',
-      url: move._canCancel ? `/move/${move.id}/cancel` : undefined,
-    },
-  ]
+    const actions = [
+      {
+        text: 'actions::view_journeys',
+        url: canAccess('move:view:journeys')
+          ? `/move/${move.id}/journeys`
+          : undefined,
+      },
+      {
+        text: 'actions::cancel_move',
+        classes: 'app-link--destructive',
+        url: move._canCancel ? `/move/${move.id}/cancel` : undefined,
+      },
+      {
+        text: 'actions::view_old_move_design',
+        url: `/move${previewPrefix}/opt-out?move_id=${move.id}`,
+      },
+    ]
 
-  res.locals.actions = actions.filter(item => item.url)
+    res.locals.actions = actions.filter(item => item.url)
 
-  next()
+    next()
+  }
 }
 
 module.exports = setActions
