19241c5ab5a8e00ebc059bfd00996e1dbfc3335a
Thomas Leese <thomas@leese.io>
2021-08-25 13:48:04 +0100

    chore: Add sendEvent function

--- common/lib/analytics.test.js
+++ common/lib/analytics.test.js
@@ -1,6 +1,66 @@
 const proxyquire = require('proxyquire').noCallThru()
 
 describe('Analytics', function () {
+  describe('#sendEvent', function () {
+    let response, analytics
+
+    context('without GA_ID', function () {
+      beforeEach(async function () {
+        analytics = proxyquire('./analytics', {
+          '../../config': {},
+        })
+
+        response = await analytics.sendEvent({})
+      })
+
+      it('should resolve with empty promise', function () {
+        expect(response).to.equal()
+      })
+    })
+
+    context('with GA_ID', function () {
+      const mockUUID = '3333'
+      const mockGaID = '11111'
+      const mockResponse = {
+        success: true,
+      }
+
+      beforeEach(async function () {
+        analytics = proxyquire('./analytics', {
+          uuid: {
+            v4: () => mockUUID,
+          },
+          '../../config': {
+            ANALYTICS: {
+              GA_ID: mockGaID,
+            },
+          },
+        })
+
+        nock('https://www.google-analytics.com')
+          .post(
+            `/collect?v=1&t=event&ec=category&ea=action&el=label&ev=value&cid=${mockUUID}&tid=${mockGaID}`
+          )
+          .reply(200, mockResponse)
+
+        response = await analytics.sendEvent({
+          category: 'category',
+          action: 'action',
+          label: 'label',
+          value: 'value',
+        })
+      })
+
+      it('should send data to GA', function () {
+        expect(response).to.deep.equal(mockResponse)
+      })
+
+      it('should post hit to GA', function () {
+        expect(nock.isDone()).to.be.true
+      })
+    })
+  })
+
   describe('#sendJourneyTime()', function () {
     let response, analytics
 
