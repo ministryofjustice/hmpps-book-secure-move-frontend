4b0855034422c4ecb70a43452675f8fc28e56ebc
Thomas Leese <thomas@leese.io>
2021-08-25 14:27:01 +0100

    feat: Record events when opting in or out
    When a user opts in or out of the new move page design, we will record
    an event in Google Analytics so we can do analysis on the data.

--- app/move/controllers/preview-opt-in.js
+++ app/move/controllers/preview-opt-in.js
@@ -1,13 +1,26 @@
+const Sentry = require('@sentry/node')
+
+const { sendEvent } = require('../../../common/lib/analytics')
 const { COOKIES } = require('../../../config')
 const { PREVIEW_PREFIX } = require('../app/view/constants')
 
-function previewOptIn(req, res) {
+async function previewOptIn(req, res) {
   const moveId = req.query.move_id
   const cookieName = COOKIES.MOVE_DESIGN_PREVIEW.name(req.user.userId)
 
   res.cookie(cookieName, 1, {
     maxAge: COOKIES.MOVE_DESIGN_PREVIEW.maxAge,
   })
+
+  try {
+    await sendEvent({
+      category: 'Move Design Preview',
+      action: 'Opt in',
+      label: req.currentLocation?.location_type,
+    })
+  } catch (e) {
+    Sentry.captureException(e)
+  }
 
   if (moveId) {
     return res.redirect(`/move${PREVIEW_PREFIX}/${moveId}`)
