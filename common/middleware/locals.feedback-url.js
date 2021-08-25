0f7bb1e50d9a0f5b2bd26c78c9d0785f2a838603
Thomas Leese <thomas@leese.io>
2021-08-25 12:15:19 +0100

    feat: Set feedback URL depending on user design choice
    If the user has opted in to the new move page design we change the
    global feedback URL to a different link. This allows us to tailor the
    questionnaire based on what users will actually be seeing.

--- common/middleware/locals.feedback-url.js
+++ common/middleware/locals.feedback-url.js
@@ -1,6 +1,21 @@
-const { FEEDBACK_URL } = require('../../config')
+const {
+  COOKIES,
+  FEEDBACK_URL,
+  MOVE_DESIGN_FEEDBACK_URL,
+} = require('../../config')
 
 module.exports = function (req, res, next) {
-  res.locals.FEEDBACK_URL = FEEDBACK_URL
+  let feedbackUrl = FEEDBACK_URL
+
+  if (MOVE_DESIGN_FEEDBACK_URL) {
+    const cookieName = COOKIES.MOVE_DESIGN_PREVIEW.name(req.user.userId)
+
+    if (req.cookies[cookieName] === '1') {
+      feedbackUrl = MOVE_DESIGN_FEEDBACK_URL
+    }
+  }
+
+  res.locals.FEEDBACK_URL = feedbackUrl
+
   next()
 }
--- common/middleware/locals.feedback-url.test.js
+++ common/middleware/locals.feedback-url.test.js
@@ -2,32 +2,105 @@ const proxyquire = require('proxyquire')
 
 const config = {
   FEEDBACK_URL: 'https://feedback.url',
+  COOKIES: {
+    MOVE_DESIGN_PREVIEW: {
+      name: userId => `cookie__${userId}`,
+    },
+  },
 }
-
-const middleware = proxyquire('./locals.feedback-url', {
-  '../../config': config,
-})
 
 describe('Middleware', function () {
   describe('#localsFeedbackUrl', function () {
     let req
     let res
     let next
+    let middleware
 
     beforeEach(function () {
-      req = {}
+      req = { user: { userId: 10 } }
       res = { locals: {} }
       next = sinon.spy()
+    })
 
+    function optIn() {
+      req.cookies = { cookie__10: '1' }
       middleware(req, res, next)
+    }
+
+    function optOut() {
+      req.cookies = { cookie__10: '0' }
+      middleware(req, res, next)
+    }
+
+    context('without a move preview feedback URL set', function () {
+      beforeEach(function () {
+        middleware = proxyquire('./locals.feedback-url', {
+          '../../config': config,
+        })
+      })
+
+      context("when the user isn't opted in to the new design", function () {
+        beforeEach(optOut)
+
+        it('should set FEEDBACK_URL', function () {
+          expect(res.locals.FEEDBACK_URL).to.equal('https://feedback.url')
+        })
+
+        it('should call next', function () {
+          expect(next).to.be.calledOnceWithExactly()
+        })
+      })
+
+      context('when the user is opted in to the new design', function () {
+        beforeEach(optIn)
+
+        it('should set FEEDBACK_URL', function () {
+          expect(res.locals.FEEDBACK_URL).to.equal('https://feedback.url')
+        })
+
+        it('should call next', function () {
+          expect(next).to.be.calledOnceWithExactly()
+        })
+      })
     })
 
-    it('should call next', function () {
-      expect(next).to.be.calledOnceWithExactly()
-    })
+    context('with a move preview feedback URL set', function () {
+      beforeEach(function () {
+        const configWithFeedbackUrl = {
+          ...config,
+          MOVE_DESIGN_FEEDBACK_URL: 'https://move.preview.feedback.url',
+        }
 
-    it('should set FEEDBACK_URL', function () {
-      expect(res.locals.FEEDBACK_URL).to.equal('https://feedback.url')
+        middleware = proxyquire('./locals.feedback-url', {
+          '../../config': configWithFeedbackUrl,
+        })
+      })
+
+      context("when the user isn't opted in to the new design", function () {
+        beforeEach(optOut)
+
+        it('should set FEEDBACK_URL', function () {
+          expect(res.locals.FEEDBACK_URL).to.equal('https://feedback.url')
+        })
+
+        it('should call next', function () {
+          expect(next).to.be.calledOnceWithExactly()
+        })
+      })
+
+      context('when the user is opted in to the new design', function () {
+        beforeEach(optIn)
+
+        it('should set MOVE_PREVIEW_FEEDBACK_URL', function () {
+          expect(res.locals.FEEDBACK_URL).to.equal(
+            'https://move.preview.feedback.url'
+          )
+        })
+
+        it('should call next', function () {
+          expect(next).to.be.calledOnceWithExactly()
+        })
+      })
     })
   })
 })
