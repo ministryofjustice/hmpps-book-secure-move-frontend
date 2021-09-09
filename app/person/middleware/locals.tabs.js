33a6b49282d93b462155b571dab8f37b1312566c
Thomas Leese <thomas@leese.io>
2021-09-09 13:50:10 +0100

    fix: Hide moves tab for non-CDM users
    This hides the move tab for non-CDM users as the page can currently only
    be viewed by CDM users anyway.

--- app/person/middleware/locals.tabs.js
+++ app/person/middleware/locals.tabs.js
@@ -1,14 +1,23 @@
+const { get } = require('lodash')
+
+const { check } = require('../../../common/middleware/permissions')
+
 module.exports = (req, res, next) => {
   const tabs = [
     {
       text: 'person::tabs.personal_details',
       url: `${req.baseUrl}/personal-details`,
     },
-    {
+  ]
+
+  const userPermissions = get(req.session, 'user.permissions')
+
+  if (check('locations:contract_delivery_manager', userPermissions)) {
+    tabs.push({
       text: 'person::tabs.moves',
       url: `${req.baseUrl}/moves`,
-    },
-  ]
+    })
+  }
 
   res.locals.tabs = tabs.map(item => ({
     ...item,
