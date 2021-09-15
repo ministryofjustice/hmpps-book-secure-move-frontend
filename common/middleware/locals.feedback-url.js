740150ad1113663d7370ac3345e8d37fb88b9e3d
Thomas Leese <thomas@leese.io>
2021-09-15 11:32:50 +0100

    fix: Support user not being logged in
    This fixes two places where we try and read a cookie before the user is
    available.

--- common/middleware/locals.feedback-url.js
+++ common/middleware/locals.feedback-url.js
@@ -8,7 +8,7 @@ module.exports = function (req, res, next) {
   let feedbackUrl = FEEDBACK_URL
 
   if (MOVE_DESIGN_FEEDBACK_URL) {
-    const cookieName = COOKIES.MOVE_DESIGN_PREVIEW.name(req.user.userId)
+    const cookieName = COOKIES.MOVE_DESIGN_PREVIEW.name(req.user?.userId)
 
     if (req.cookies[cookieName] === '1') {
       feedbackUrl = MOVE_DESIGN_FEEDBACK_URL
