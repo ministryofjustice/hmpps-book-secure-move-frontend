c2b8aecdf354dd0b85d9d5698819c68a513d77b7
Thomas Leese <thomas@leese.io>
2021-09-16 13:48:38 +0100

    refactor: Remove "Return to the old version" from sidebar
    We're going to move this in to the feedback banner at the bottom of the
    page.

--- app/move/app/view/middleware/locals.actions.test.js
+++ app/move/app/view/middleware/locals.actions.test.js
@@ -27,7 +27,7 @@ describe('Move view app', function () {
         beforeEach(function () {
           req.move._canCancel = true
 
-          middleware()(req, res, nextSpy)
+          middleware(req, res, nextSpy)
         })
 
         it('should add cancel action to locals', function () {
@@ -35,17 +35,6 @@ describe('Move view app', function () {
             text: 'actions::cancel_move',
             classes: 'app-link--destructive',
             url: '/move/12345/cancel',
-          })
-        })
-
-        it('should add opt out action to locals', function () {
-          expect(res.locals.actions).to.deep.include({
-            text: 'messages::preview_new_feature.actions.return',
-            itemClasses:
-              'app-border-top-1 govuk-!-padding-top-4 govuk-!-margin-top-4',
-            classes:
-              'govuk-button govuk-button--secondary govuk-!-margin-top-4',
-            url: '/move/opt-out?move_id=12345',
           })
         })
       })
@@ -54,7 +43,7 @@ describe('Move view app', function () {
         beforeEach(function () {
           req.move._canCancel = false
 
-          middleware()(req, res, nextSpy)
+          middleware(req, res, nextSpy)
         })
 
         it('should not add cancel actions', function () {
@@ -64,23 +53,13 @@ describe('Move view app', function () {
             url: '/move/12345/cancel',
           })
         })
-
-        it('should add opt out action to locals', function () {
-          expect(res.locals.actions).to.deep.include({
-            text: 'messages::preview_new_feature.actions.return',
-            itemClasses: undefined,
-            classes:
-              'govuk-button govuk-button--secondary govuk-!-margin-top-4',
-            url: '/move/opt-out?move_id=12345',
-          })
-        })
       })
 
       context('when user can view journeys', function () {
         beforeEach(function () {
           req.canAccess.withArgs('move:view:journeys').returns(true)
 
-          middleware()(req, res, nextSpy)
+          middleware(req, res, nextSpy)
         })
 
         it('should add cancel action to locals', function () {
@@ -89,24 +68,13 @@ describe('Move view app', function () {
             url: '/move/12345/journeys',
           })
         })
-
-        it('should add opt out action to locals', function () {
-          expect(res.locals.actions).to.deep.include({
-            text: 'messages::preview_new_feature.actions.return',
-            itemClasses:
-              'app-border-top-1 govuk-!-padding-top-4 govuk-!-margin-top-4',
-            classes:
-              'govuk-button govuk-button--secondary govuk-!-margin-top-4',
-            url: '/move/opt-out?move_id=12345',
-          })
-        })
       })
 
       context('when user cannot view journeys', function () {
         beforeEach(function () {
           req.canAccess.withArgs('move:view:journeys').returns(false)
 
-          middleware()(req, res, nextSpy)
+          middleware(req, res, nextSpy)
         })
 
         it('should not add cancel actions', function () {
@@ -115,38 +83,6 @@ describe('Move view app', function () {
             url: '/move/12345/journeys',
           })
         })
-
-        it('should add opt out action to locals', function () {
-          expect(res.locals.actions).to.deep.include({
-            text: 'messages::preview_new_feature.actions.return',
-            itemClasses: undefined,
-            classes:
-              'govuk-button govuk-button--secondary govuk-!-margin-top-4',
-            url: '/move/opt-out?move_id=12345',
-          })
-        })
-      })
-
-      context('with preview prefix argument', function () {
-        beforeEach(function () {
-          middleware({ previewPrefix: '/preview-path' })(req, res, nextSpy)
-        })
-
-        it('should add opt out action to locals with prefix', function () {
-          expect(res.locals.actions).to.deep.include({
-            text: 'messages::preview_new_feature.actions.return',
-            itemClasses: undefined,
-            classes:
-              'govuk-button govuk-button--secondary govuk-!-margin-top-4',
-            url: '/move/preview-path/opt-out?move_id=12345',
-          })
-        })
-      })
-
-      it('should call next', function () {
-        middleware()(req, res, nextSpy)
-
-        expect(nextSpy).to.be.calledOnceWithExactly()
       })
     })
   })
