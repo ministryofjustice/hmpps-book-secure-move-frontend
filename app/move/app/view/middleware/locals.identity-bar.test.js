949f74c97bf2c948bf40e56f25fa791de38df36a
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2021-11-24 11:46:53 +0000

    fix: Fixing wording in identity bar for proposed moves

--- app/move/app/view/middleware/locals.identity-bar.test.js
+++ app/move/app/view/middleware/locals.identity-bar.test.js
@@ -30,6 +30,7 @@ describe('Move view app', function () {
             id: '12345',
             reference: 'AB1234XY',
             date: '2020-10-07',
+            status: 'requested',
             profile: {
               person: {
                 _fullname: 'DOE, JOHN',
@@ -67,6 +68,7 @@ describe('Move view app', function () {
             expect(req.t).to.be.calledWithExactly(
               'moves::detail.page_heading_summary',
               {
+                context: 'requested',
                 fromLocation: 'Guildford Custody Suite',
                 toLocation: 'HMP Brixton',
                 date: '2020-10-07',
@@ -112,6 +114,7 @@ describe('Move view app', function () {
             id: '12345',
             reference: 'AB1234XY',
             date: '2020-10-07',
+            status: 'requested',
             profile: null,
             from_location: {
               title: 'Guildford Custody Suite',
@@ -145,9 +148,64 @@ describe('Move view app', function () {
             expect(req.t).to.be.calledWithExactly(
               'moves::detail.page_heading_summary',
               {
+                context: 'requested',
                 fromLocation: 'Guildford Custody Suite',
                 toLocation: 'HMP Brixton',
                 date: '2020-10-07',
+              }
+            )
+          })
+        })
+
+        it('should call next', function () {
+          expect(nextSpy).to.be.calledOnceWithExactly()
+        })
+      })
+
+      context('without profile', function () {
+        beforeEach(function () {
+          req.move = {
+            id: '12345',
+            reference: 'AB1234XY',
+            date: null,
+            status: 'proposed',
+            profile: null,
+            from_location: {
+              title: 'Guildford Custody Suite',
+            },
+            to_location: {
+              title: 'HMP Brixton',
+            },
+            foo: 'bar',
+          }
+
+          middleware(req, res, nextSpy)
+        })
+
+        describe('translations', function () {
+          it('should translate caption', function () {
+            expect(req.t).to.be.calledWithExactly('moves::detail.page_caption')
+          })
+
+          it('should translate heading', function () {
+            expect(req.t).to.be.calledWithExactly(
+              'moves::detail.page_heading',
+              {
+                name: 'awaiting_person',
+                reference: 'AB1234XY',
+              }
+            )
+            expect(req.t).to.be.calledWithExactly('awaiting_person')
+          })
+
+          it('should translate summary proposed', function () {
+            expect(req.t).to.be.calledWithExactly(
+              'moves::detail.page_heading_summary',
+              {
+                context: 'proposed',
+                fromLocation: 'Guildford Custody Suite',
+                toLocation: 'HMP Brixton',
+                date: null,
               }
             )
           })
