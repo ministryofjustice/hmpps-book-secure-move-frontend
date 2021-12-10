c7450b84da205518b718c32e45f18abb17e6df19
Thomas Leese <thomas@leese.io>
2021-12-10 13:18:26 +0000

    refactor: Remove setMoveDesignPreview middleware
    It's not used anywhere anymore so we don't need it.

--- common/middleware/set-move-design-preview.js
+++
@@ -1,7 +0,0 @@
-const { COOKIES } = require('../../config')
-
-module.exports = function (req, res, next) {
-  const cookieName = COOKIES.MOVE_DESIGN_PREVIEW.name(req.user?.userId)
-  req.moveDesignPreview = req.cookies[cookieName] === '1'
-  next()
-}
