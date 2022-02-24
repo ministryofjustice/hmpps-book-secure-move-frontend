bc7dce96f2643accdc8a0a7c10bba31961d7a5a1
georgeschena <georgeschena@hotmail.com>
2022-02-24 10:48:42 +0000

    fix: Fix failing tests

--- common/services/contentful.js
+++ common/services/contentful.js
@@ -5,33 +5,31 @@ const {
   CONTENTFUL_DELIVERY_ACCESS_TOKEN,
 } = require('../../config')
 
-const formatEntries = async () => {
-  const entries = await fetchEntries()
+const service = {
+  formatEntries: async () => {
+    const entries = await service.fetchEntries()
 
-  if (!entries.items?.length) {
-    return null
-  }
+    if (!entries.items?.length) {
+      return null
+    }
 
-  const latestNotification = entries.items[0]
+    const latestNotification = entries.items[0]
 
-  const latestNotificationTitle = latestNotification.fields.title
-  const latestNotificationBody = latestNotification.fields.body.content
+    const latestNotificationTitle = latestNotification.fields.title
+    const latestNotificationBody = latestNotification.fields.body.content
 
-  return { title: latestNotificationTitle, body: latestNotificationBody }
+    return { title: latestNotificationTitle, body: latestNotificationBody }
+  },
+  fetchEntries: async () => {
+    const client = contentful.createClient({
+      space: CONTENTFUL_SPACE_ID,
+      accessToken: CONTENTFUL_DELIVERY_ACCESS_TOKEN,
+    })
+
+    const entries = await client.getEntries()
+
+    return entries
+  },
 }
 
-const fetchEntries = async () => {
-  const client = contentful.createClient({
-    space: CONTENTFUL_SPACE_ID,
-    accessToken: CONTENTFUL_DELIVERY_ACCESS_TOKEN,
-  })
-
-  const entries = await client.getEntries()
-
-  return entries
-}
-
-module.exports = {
-  formatEntries: formatEntries,
-  fetchEntries: fetchEntries,
-}
+module.exports = service
--- common/services/contentful.test.js
+++ common/services/contentful.test.js
@@ -3,52 +3,56 @@ const sinon = require('sinon')
 const contentfulService = require('./contentful')
 
 const mockedResponse = {
-  metadata: { tags: [] },
-  sys: {
-    space: { sys: [] },
-    id: '3z9IGEbskrD3I8G0ZWRFkh',
-    type: 'Entry',
-    createdAt: '2022-02-09T09:50:50.672Z',
-    updatedAt: '2022-02-10T09:22:26.455Z',
-    environment: { sys: [] },
-    revision: 5,
-    contentType: { sys: [] },
-    locale: 'en-US',
-  },
-  fields: {
-    title: "What's new today!",
-    body: {
-      nodeType: 'document',
-      data: {},
-      content: [
-        {
-          nodeType: 'paragraph',
+  items: [
+    {
+      metadata: { tags: [] },
+      sys: {
+        space: { sys: [] },
+        id: '3z9IGEbskrD3I8G0ZWRFkh',
+        type: 'Entry',
+        createdAt: '2022-02-09T09:50:50.672Z',
+        updatedAt: '2022-02-10T09:22:26.455Z',
+        environment: { sys: [] },
+        revision: 5,
+        contentType: { sys: [] },
+        locale: 'en-US',
+      },
+      fields: {
+        title: 'Whats new today!',
+        body: {
+          nodeType: 'document',
+          data: {},
           content: [
             {
-              nodeType: 'text',
-              value:
-                'The latest updates and improvements to Book a secure move.',
-              marks: [],
+              nodeType: 'paragraph',
+              content: [
+                {
+                  nodeType: 'text',
+                  value:
+                    'The latest updates and improvements to Book a secure move.',
+                  marks: [],
+                  data: {},
+                },
+              ],
+              data: {},
+            },
+            {
+              nodeType: 'heading-4',
+              content: [
+                {
+                  nodeType: 'text',
+                  value: 'We love a heading 4.',
+                  marks: [],
+                  data: {},
+                },
+              ],
               data: {},
             },
           ],
-          data: {},
         },
-        {
-          nodeType: 'heading-4',
-          content: [
-            {
-              nodeType: 'text',
-              value: 'We love a heading 4.',
-              marks: [],
-              data: {},
-            },
-          ],
-          data: {},
-        },
-      ],
+      },
     },
-  },
+  ],
 }
 
 describe('Contentful Service', function () {
@@ -58,7 +62,14 @@ describe('Contentful Service', function () {
 
   it('should return the content title', async function () {
     const formattedEntries = await contentfulService.formatEntries()
+    expect(formattedEntries.title).to.equal('Whats new today!')
+  })
 
-    expect(formattedEntries.title).to.equal('test')
+  it('should return the body', async function () {
+    const formattedEntries = await contentfulService.formatEntries()
+    expect(formattedEntries.body[0].nodeType).to.equal('paragraph')
+    expect(formattedEntries.body[0].content[0].value).to.equal(
+      'The latest updates and improvements to Book a secure move.'
+    )
   })
 })
