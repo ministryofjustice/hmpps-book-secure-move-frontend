a2af48efb2a59eff7fdbba42ac6d1c2f218797b5
georgeschena <georgeschena@hotmail.com>
2022-03-22 17:20:34 +0000

    feat: Render 500 page when contenful servers are down on the whats-new page

--- app/whats-new/index.js
+++ app/whats-new/index.js
@@ -4,11 +4,18 @@ const router = require('express').Router()
 const whatsNewContentService = require('../../common/services/whats-new-content')
 
 // Define routes
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
   const content = await whatsNewContentService.fetch()
-  res.render('whats-new/whats-new', {
-    posts: content.posts,
-  })
+
+  if (!content.error === 'ServerError') {
+    const error = new Error('Contentful servers are down')
+    error.statusCode = 500
+    return next(error)
+  } else {
+    res.render('whats-new/whats-new', {
+      posts: content.posts,
+    })
+  }
 })
 
 // Export
