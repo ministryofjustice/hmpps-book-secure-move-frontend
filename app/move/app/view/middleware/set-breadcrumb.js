9fe19c6d1749b6626631bce3acd4566695633ac5
Thomas Leese <thomas@leese.io>
2022-03-30 14:12:58 +0100

    feat: Add breadcrumb to dashboard for move
    This adds a new breadcrumb item when looking at a move that allows the
    user to be taken back to approriate dashboard they were looking at
    before selecting the move.

--- app/move/app/view/middleware/set-breadcrumb.js
+++ app/move/app/view/middleware/set-breadcrumb.js
@@ -1,6 +1,27 @@
+const i18n = require('../../../../../config/i18n')
+const movesApp = require('../../../../moves')
+
+function dashboardBreadcrumbTextContext(movesUrl) {
+  if (movesUrl.includes('/outgoing')) {
+    return 'outgoing_moves'
+  } else if (movesUrl.includes('/incoming')) {
+    return 'incoming_moves'
+  } else if (movesUrl.includes('/requested')) {
+    return 'single_requests'
+  }
+}
+
 function setBreadcrumb(req, res, next) {
   const { profile, reference } = req.move || {}
   const name = profile?.person?._fullname
+  const movesUrl = req.session?.movesUrl || movesApp.mountpath
+
+  res.breadcrumb({
+    text: i18n.t('collections::page_title', {
+      context: dashboardBreadcrumbTextContext(movesUrl),
+    }),
+    href: movesUrl,
+  })
 
   if (reference) {
     res.breadcrumb({
