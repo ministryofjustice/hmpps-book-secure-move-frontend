9999c4483370bf72e611de4a5e568ed0df549af5
Thomas Leese <thomas@leese.io>
2022-03-31 09:37:37 +0100

    feat: Order important events most recent first
    We'd like users to see the most recent event at the top of the list.

--- common/presenters/move-to-in-transit-events-panel-list.js
+++ common/presenters/move-to-in-transit-events-panel-list.js
@@ -16,10 +16,14 @@ function toPanelList(events, move) {
 module.exports = function (move = {}) {
   const { timeline_events: timelineEvents = [] } = move
 
-  const importantEvents = timelineEvents.filter(
-    ({ event_type: eventType, classification }) =>
-      eventType === 'PerPropertyChange' || classification === 'incident'
-  )
+  const importantEvents = timelineEvents
+    .filter(
+      ({ event_type: eventType, classification }) =>
+        eventType === 'PerPropertyChange' || classification === 'incident'
+    )
+    .sort(({ occurred_at: occurredAtA }, { occurred_at: occurredAtB }) =>
+      occurredAtA > occurredAtB ? -1 : 1
+    )
 
   return toPanelList(importantEvents, move)
 }
