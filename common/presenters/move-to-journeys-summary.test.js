0b91411e72ec050b3168be701f576abc1eeb8fcc
Thomas Leese <thomas@leese.io>
2022-04-04 18:56:37 +0100

    fix: Ensure only active journeys are considered
    This removes any journeys which are cancelled or rejected and doesn't
    include them when displaying journeys in the identity bar and move
    details tab.

--- common/presenters/move-to-journeys-summary.test.js
+++ common/presenters/move-to-journeys-summary.test.js
@@ -42,6 +42,46 @@ describe('Presenters', function () {
     )
 
     context(
+      "when the move is not locked out and there is a journey on a different date but it's cancelled",
+      function () {
+        it('will present the journeys correctly', function () {
+          const move = {
+            date: '2020-10-07',
+            is_lockout: false,
+            from_location: {
+              title: 'Guildford Custody Suite',
+            },
+            to_location: {
+              title: 'HMP Brixton',
+            },
+          }
+
+          const journeys = [
+            {
+              date: '2021-01-13',
+              state: 'cancelled',
+              from_location: {
+                title: 'WETHERBY (HMPYOI)',
+              },
+              to_location: {
+                title: 'Exeter Probation Office',
+              },
+            },
+          ]
+
+          expect(moveToJourneysSummary(move, journeys)).to.deep.equal([
+            {
+              context: undefined,
+              date: '7 Oct 2020',
+              fromLocation: 'Guildford Custody Suite',
+              toLocation: 'HMP Brixton',
+            },
+          ])
+        })
+      }
+    )
+
+    context(
       'when the move is locked out and there is one journey',
       function () {
         it('will present the journeys correctly', function () {
