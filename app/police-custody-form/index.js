21903adb3c69317e6f6f1b0abb7f1fda98b4f842
georgeschena <georgeschena@hotmail.com>
2022-04-08 12:50:55 +0100

    chore: Allow for form submission

--- app/police-custody-form/index.js
+++ app/police-custody-form/index.js
@@ -6,6 +6,12 @@ router.get('/', (req, res) => {
   res.render('police-custody-form/police-custody-form')
 })
 
+router.post('/', function (req, res) {
+  const moveId = req.body.moveId
+
+  res.redirect(`move/${moveId}/warnings`)
+})
+
 // Export
 module.exports = {
   router,
