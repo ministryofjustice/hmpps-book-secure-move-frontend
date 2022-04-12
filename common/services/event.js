77d09c8dab0f1ca9665b22d6344e0575ba294322
georgeschena <georgeschena@hotmail.com>
2022-04-12 12:33:02 +0100

    chore: Allow a user to process multiple events at once

--- common/services/event.js
+++ common/services/event.js
@@ -3,41 +3,87 @@ const restClient = require('../lib/api-client/rest-client')
 const BaseService = require('./base')
 
 class EventService extends BaseService {
-  async postEvents(lockoutEvents, move, user) {
+  isPerEvent(event) {
+    const perEvents = [
+      'PerViolentDangerous',
+      'PerWeapons',
+      'PerConcealed',
+      'PerSelfHarm',
+      'PerEscape',
+      'PerMedicalAid',
+      'PerMedicalDrugsAlcohol',
+      'PerMedicalMentalHealth',
+      'PerPropertyChange',
+      'PerGeneric',
+    ]
+    return perEvents.includes(event)
+  }
+
+  isPersonMoveEvent(event) {
+    const personMoveEvents = ['PersonMoveUsedForce', 'PersonMoveDeathInCustody']
+    return personMoveEvents.includes(event)
+  }
+
+  postEvents(lockoutEvents, move, user) {
     const todaysDate = new Date()
+    const events = []
 
-    const payload = {
-      data: {
-        type: 'events',
-        attributes: {
-          occurred_at: todaysDate.toISOString(),
-          recorded_at: todaysDate.toISOString(),
-          notes: 'Additional information about this event',
-          details: {
-            police_personnel_number: user.username,
+    events.push(lockoutEvents.events)
+
+    events.flat().map(async event => {
+      let type
+      let relationshipsId
+      let supplierPersonnelNumber
+
+      const eventDescription = lockoutEvents[event]
+
+      if (this.isPerEvent(event)) {
+        type = 'person_escort_records'
+        relationshipsId = move.profile.person_escort_record.id
+        supplierPersonnelNumber = 'supplier_personnel_number'
+      } else if (this.isPersonMoveEvent(event)) {
+        type = 'moves'
+        relationshipsId = move.id
+        supplierPersonnelNumber = 'supplier_personnel_numbers'
+      }
+
+      const payload = {
+        data: {
+          type: 'events',
+          attributes: {
+            occurred_at: todaysDate.toISOString(),
+            recorded_at: todaysDate.toISOString(),
+            notes: eventDescription,
+            details: {
+              [supplierPersonnelNumber]: [user.username],
+              fault_classification: 'investigation',
+              reported_at: todaysDate.toISOString(),
+              advised_at: todaysDate.toISOString(),
+              advised_by: user.username,
+            },
+            event_type: event,
           },
-          event_type: lockoutEvents.events,
-        },
-        relationships: {
-          eventable: {
-            data: {
-              type: 'person_escort_records',
-              id: `${move.profile.person_escort_record.id}`,
+          relationships: {
+            eventable: {
+              data: {
+                type: type,
+                id: relationshipsId,
+              },
+            },
+            location: {
+              data: {
+                id: `${move.from_location.id}`,
+                type: 'locations',
+              },
             },
           },
-          location: {
-            data: {
-              id: `${move.from_location.id}`,
-              type: 'locations',
-            },
-          },
         },
-      },
-    }
+      }
 
-    const response = await restClient.post('/events', payload)
+      const response = await restClient.post('/events', payload)
 
-    return response
+      return response
+    })
   }
 }
 
