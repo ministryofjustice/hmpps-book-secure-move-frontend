8eee4bf66bac746127d18b2a9d0c024507f42729
georgeschena <georgeschena@hotmail.com>
2022-04-28 11:47:04 +0100

    chore: Show events in lockout section
    chore: Use event.supplier
    
    chore: Update tests
    
    chore: Update events.json
    
    chore: Check eventType and supplier for getDescription
    
    chore: Refactor checking if supplier is null

--- common/helpers/events/get-heading.js
+++ common/helpers/events/get-heading.js
@@ -1,8 +1,14 @@
 const i18n = require('../../../config/i18n')
 
 const getHeading = event => {
-  const { event_type: eventType, _index, details } = event
-  let heading = i18n.t(`events::${eventType}.heading`, details)
+  const { event_type: eventType, _index, details, supplier } = event
+  let heading
+
+  if (supplier === null) {
+    details.context = 'without_supplier'
+  }
+
+  heading = i18n.t(`events::${eventType}.heading`, details)
 
   if (_index) {
     heading += ` (${_index})`
