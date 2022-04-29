f052ea9d69f8a5c510698160ef200692c567a8c4
georgeschena <georgeschena@hotmail.com>
2022-04-29 08:55:38 +0100

    chore: PR comment

--- common/helpers/events/get-heading.js
+++ common/helpers/events/get-heading.js
@@ -2,13 +2,12 @@ const i18n = require('../../../config/i18n')
 
 const getHeading = event => {
   const { event_type: eventType, _index, details, supplier } = event
-  let heading
 
   if (supplier === null) {
     details.context = 'without_supplier'
   }
 
-  heading = i18n.t(`events::${eventType}.heading`, details)
+  let heading = i18n.t(`events::${eventType}.heading`, details)
 
   if (_index) {
     heading += ` (${_index})`
