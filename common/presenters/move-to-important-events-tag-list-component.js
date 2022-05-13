63ad625066ed7895b0cd3ce3565c35ed79c8f78b
Thomas Leese <thomas@leese.io>
2022-05-13 11:00:44 +0100

    fix: Exclude certain events from tags list
    These were added accidentally when we started including these events in
    db2990edad77481b2e34e4274be7ebac6d40c42b and
    5532e88971848e0d77ed60174c4624068d851d7a.

--- common/presenters/move-to-important-events-tag-list-component.js
+++ common/presenters/move-to-important-events-tag-list-component.js
@@ -1,13 +1,16 @@
 const eventToTagComponent = require('./event-to-tag-component')
 
+const EXCLUDED_EVENTS = ['PerHandover', 'MoveLodgingStart', 'MoveLodgingEnd']
+
 const moveToImportantEventsTagListComponent = (
   move = {},
   linkToLocal = false
 ) => {
   const { important_events: importantEvents = [] } = move
-  return importantEvents.map(event =>
-    eventToTagComponent(event, move.id, linkToLocal)
-  )
+
+  return importantEvents
+    .filter(({ event_type: eventType }) => !EXCLUDED_EVENTS.includes(eventType))
+    .map(event => eventToTagComponent(event, move.id, linkToLocal))
 }
 
 module.exports = moveToImportantEventsTagListComponent
