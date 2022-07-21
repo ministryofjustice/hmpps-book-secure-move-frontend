51285827c2567cad9e3b5a2b32668d06b9d35581
georgeschena <georgeschena@hotmail.com>
2022-07-21 10:49:54 +0100

    chore: Move dedicated content into whats-new url path

--- app/whats-new/index.ts
+++ app/whats-new/index.ts
@@ -4,7 +4,7 @@ const router = require('express').Router()
 const contentfulService = require('../../common/services/contentful')
 
 // Define routes
-router.get('/', async (req, res, next) => {
+router.get('/',async (req: any, res: any, next: any) => {
   let content
 
   try {
@@ -15,6 +15,20 @@ router.get('/', async (req, res, next) => {
 
   res.render('whats-new/whats-new', {
     posts: content.posts,
+  })
+})
+
+router.get('/:id', async (req: any, res: any, next: any) => {
+  let content
+
+  try {
+    content = await contentfulService.fetchEntryBySlugId(req.params.id)
+  } catch (e) {
+    return next(e)
+  }
+
+  res.render('whats-new/content', {
+    post: content
   })
 })
 
