4b0cac7d207303009868d3b83d826256c56e8466
Adam Sharp <adam.sharp@digital.justice.gov.uk>
2022-12-20 09:31:47 +0000

    fix: Fix error thrown by content which doesn't have a date

--- common/services/contentful/dedicated-content.ts
+++ common/services/contentful/dedicated-content.ts
@@ -1,9 +1,24 @@
-import { ContentfulService } from './contentful'
+import * as contentful from 'contentful'
+
+import { ContentfulFields, ContentfulService } from './contentful'
 
 export class DedicatedContentService extends ContentfulService {
   public constructor() {
     super()
 
     this.contentType = 'dedicatedContent'
+  }
+
+  async fetchEntryBySlugId(slugId: any) {
+    const entry = (await this.client.getEntries({
+      content_type: this.contentType,
+      'fields.slug[in]': slugId,
+    })) as contentful.EntryCollection<ContentfulFields>
+
+    if (!entry.items?.length) {
+      return undefined
+    }
+
+    return this.createContent(entry.items[0].fields).getPostData()
   }
 }
