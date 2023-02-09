32d3953e06cc04592050c050f27acff3b45b1f15
Adam Sharp <adam.sharp@digital.justice.gov.uk>
2023-02-09 14:20:03 +0000

    feat: Add lodgings banner for moves with MoveLodgingStart

--- common/types/event.ts
+++ common/types/event.ts
@@ -1,17 +1,17 @@
 import { EventDetails } from './event-details'
-import { PersonEscortRecord } from './person-escort-record'
-import { Move } from './move'
 import { Journey } from './journey'
+import { Move } from './move'
 import { Person } from './person'
+import { PersonEscortRecord } from './person-escort-record'
 
 export interface Event {
   id: string
   event_type?: string
   classification?: string
-  occurred_at?: string
+  occurred_at: string
   recorded_at?: string
-  notes?: string
-  created_by?: string
+  notes?: string | null
+  created_by?: string | null
   eventable?: Journey | Move | PersonEscortRecord | Person
   supplier?: string
   details: EventDetails
--- common/types/move.ts
+++ common/types/move.ts
@@ -1,3 +1,5 @@
+import { Event } from './event'
+import { Location } from './location'
 import { Profile } from './profile'
 
 export interface Move {
@@ -9,4 +11,31 @@ export interface Move {
     | 'in-transit'
     | 'completed'
     | 'cancelled'
+  from_location: Location
+  to_location?: Location
+  move_type:
+    | 'court_appearance'
+    | 'court_other'
+    | 'hospital'
+    | 'police_transfer'
+    | 'prison_recall'
+    | 'prison_remand'
+    | 'prison_transfer'
+    | 'video_remand'
+  id: string
+  _vehicleRegistration?: string
+  _expectedCollectionTime?: string
+  _expectedArrivalTime?: string
+  _hasLeftCustody?: boolean
+  _hasArrived?: boolean
+  important_events?: Event[]
+  timeline_events?: Event[]
+  meta?: {
+    vehicle_registration?: string | null
+    expected_collection_time?: string | null
+    expected_time_of_arrival?: string | null
+  }
+  is_lockout?: boolean
+  _canEdit?: boolean
+  _isPerLocked?: boolean
 }
