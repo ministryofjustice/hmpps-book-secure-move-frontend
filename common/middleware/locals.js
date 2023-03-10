5aaba270fd8b0362701107d4037daaf995dd77c3
Adam Sharp <adam.sharp@digital.justice.gov.uk>
2023-03-10 15:04:54 +0000

    chore: WIP add lodge form

--- common/middleware/locals.js
+++ common/middleware/locals.js
@@ -3,6 +3,18 @@ const { startOfTomorrow, startOfYesterday } = require('date-fns')
 const movesApp = require('../../app/moves')
 const SERVICE_NAME = 'Book a secure move'
 
+function startOfDayAfterTomorrow() {
+  const now = new Date()
+  const year = now.getFullYear()
+  const month = now.getMonth()
+  const day = now.getDate()
+
+  const date = new Date(0)
+  date.setFullYear(year, month, day + 2)
+  date.setHours(0, 0, 0, 0)
+  return date
+}
+
 module.exports = function setLocals(req, res, next) {
   const protocol = req.encrypted ? 'https' : req.protocol
   const baseUrl = `${protocol}://${req.get('host')}`
@@ -10,6 +22,7 @@ module.exports = function setLocals(req, res, next) {
     CANONICAL_URL: baseUrl + req.path,
     TODAY: new Date(),
     TOMORROW: startOfTomorrow(),
+    DAY_AFTER_TOMORROW: startOfDayAfterTomorrow(),
     YESTERDAY: startOfYesterday(),
     REQUEST_PATH: req.path,
     USER: req.user,
--- common/types/basm_request.ts
+++ common/types/basm_request.ts
@@ -1,6 +1,8 @@
 import I18n from '../../config/i18n'
 
 import { Allocation } from './allocation'
+import I18n from '../../config/i18n'
+
 import { Journey } from './journey'
 import { Move } from './move'
 import { Service } from './service'
@@ -9,13 +11,7 @@ import { SessionModel } from './session_model'
 export interface BasmRequest extends Express.Request {
   allocation?: Allocation
   canAccess: (permission: string) => boolean
-  form?: {
-    options: {
-      fields: object
-      next: string
-    }
-    values: any
-  }
+
   initialStep?: any
   journeys: Journey[]
   move: Move
@@ -24,4 +20,11 @@ export interface BasmRequest extends Express.Request {
   }
   sessionModel?: SessionModel
   t: typeof I18n.t
+  form?: {
+    options: {
+      fields: { [key: string]: any }
+      next: string
+    }
+    values: { [key: string]: any }
+  }
 }
