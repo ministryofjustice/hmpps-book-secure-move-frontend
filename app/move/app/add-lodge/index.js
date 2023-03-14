9751dee89cbc00bef0f42ff6ae6a3049061b36ea
Daniel Benton <daniel.benton@digital.justice.gov.uk>
2023-03-14 15:16:58 +0000

    feat: P4-3891 Fix recursive breadcrumbs on newlodge page

--- app/move/app/add-lodge/index.js
+++ app/move/app/add-lodge/index.js
@@ -4,15 +4,17 @@ const router = require('express').Router({ mergeParams: true })
 const { protectRoute } = require('../../../../common/middleware/permissions')
 const wizard = require('../../../../common/middleware/unique-form-wizard')
 
+
 const config = require('./config')
 const steps = require('./steps')
+const fields = require('./fields')
 
 
 // Define shared middleware
 router.use(protectRoute(['move:create']))
 
 // Define routes
-router.use(wizard(steps, null, config, 'params.moveId'))
+router.use(wizard(steps, fields, config, 'params.moveId'))
 
 // Export
 module.exports = {
