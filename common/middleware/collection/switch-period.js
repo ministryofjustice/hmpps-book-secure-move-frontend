9dbdc43f8a541daa8e6467e97d181783a80c0c7f
Daniel Benton <daniel.benton@digital.justice.gov.uk>
2023-03-29 15:05:36 +0100

    feat: P4-4090 Add day/week switcher. Collections remember selected date

---
+++ common/middleware/collection/switch-period.js
@@ -0,0 +1,19 @@
+function switchPeriod(defaults = {}) {
+  return function handleSwitchPeriod(req, res) {
+    const { view } = req.params
+    let currentPeriod = req.session.period
+
+    if (!currentPeriod) {
+      currentPeriod = defaults[view]
+    }
+
+    const switchedPeriod = currentPeriod === 'day' ? 'week' : 'day'
+    req.session.period = switchedPeriod
+    const redir = req.originalUrl
+      .replace('/switch-view', '')
+      .replace(currentPeriod, switchedPeriod)
+    res.redirect(`${redir}`)
+  }
+}
+
+module.exports = switchPeriod
