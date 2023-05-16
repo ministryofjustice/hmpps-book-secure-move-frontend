e8c2c4f2366be3822acb3f76b290064c230a4e05
Jim Bailey <jim.bailey@digital.justice.gov.uk>
2023-05-16 14:14:39 +0100

    feat: Add AppInsights tracking by user and location
    [P4-4144]

---
+++ common/lib/azure-appinsights.test.ts
@@ -0,0 +1,99 @@
+import {
+  DataTelemetry,
+  EnvelopeTelemetry,
+} from 'applicationinsights/out/Declarations/Contracts'
+import { expect } from 'chai'
+
+import { addUserDataToRequests, ContextObject } from './azure-appinsights'
+
+const user = {
+  activeCaseLoadId: 'LII',
+  username: 'test-user',
+}
+
+const createEnvelope = (
+  properties?: Record<string, string | boolean>,
+  baseType = 'RequestData'
+) =>
+  ({
+    data: {
+      baseType,
+      baseData: { properties },
+    } as DataTelemetry,
+  } as EnvelopeTelemetry)
+
+const createContext = (username?: string, activeCaseLoadId?: string) =>
+  ({
+    'http.ServerRequest': {
+      res: {
+        req: {
+          session: {
+            currentLocation: {
+              nomis_agency_id: activeCaseLoadId,
+            },
+            user: {
+              username,
+            },
+          },
+        },
+      },
+    },
+  } as ContextObject)
+
+const context = createContext(user.username, user.activeCaseLoadId)
+
+describe('azure-appinsights', function () {
+  describe('addUserDataToRequests', function () {
+    it('adds user data to properties when present', function () {
+      const envelope = createEnvelope({ other: 'things' })
+
+      addUserDataToRequests(envelope, context)
+
+      expect(envelope.data.baseData!.properties).to.deep.equal({
+        ...user,
+        other: 'things',
+      })
+    })
+
+    it('handles absent user data', function () {
+      const envelope = createEnvelope({ other: 'things' })
+
+      addUserDataToRequests(
+        envelope,
+        createContext(undefined, user.activeCaseLoadId)
+      )
+
+      expect(envelope.data.baseData!.properties).to.deep.equal({
+        other: 'things',
+      })
+    })
+
+    it('returns true when not RequestData type', function () {
+      const envelope = createEnvelope({}, 'NOT_REQUEST_DATA')
+
+      const response = addUserDataToRequests(envelope, context)
+
+      expect(response).to.equal(true)
+    })
+
+    it('handles when no properties have been set', function () {
+      const envelope = createEnvelope(undefined)
+
+      addUserDataToRequests(envelope, context)
+
+      expect(envelope.data.baseData!.properties).to.deep.equal(user)
+    })
+
+    it('handles missing user details', function () {
+      const envelope = createEnvelope({ other: 'things' })
+
+      addUserDataToRequests(envelope, {
+        'http.ServerRequest': {},
+      } as ContextObject)
+
+      expect(envelope.data.baseData!.properties).to.deep.equal({
+        other: 'things',
+      })
+    })
+  })
+})
---
+++ common/lib/azure-appinsights.ts
@@ -0,0 +1,42 @@
+import * as appInsights from 'applicationinsights'
+import { EnvelopeTelemetry } from 'applicationinsights/out/Declarations/Contracts'
+import { Contracts } from 'applicationinsights'
+import fs from 'fs'
+
+const packageData = JSON.parse(fs.readFileSync('./package.json', 'utf-8'))
+
+export type ContextObject = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [name: string]: any
+} | undefined
+
+export const initialiseAppInsights = () => {
+  if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
+    // eslint-disable-next-line no-console
+    console.log('Enabling azure application insights')
+    appInsights.setup().setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C).start()
+    appInsights.defaultClient.context.tags['ai.cloud.role'] = packageData.name
+    appInsights.defaultClient.context.tags['ai.application.ver'] = packageData.version
+    appInsights.defaultClient.addTelemetryProcessor(addUserDataToRequests)
+    return appInsights.defaultClient
+  }
+  return null
+}
+
+export const addUserDataToRequests = (envelope: EnvelopeTelemetry, contextObjects: ContextObject): boolean => {
+  const isRequest = envelope.data.baseType === Contracts.TelemetryTypeString.Request
+  if (isRequest) {
+    const session = contextObjects?.['http.ServerRequest']?.res?.req?.session
+    const username = session?.user?.username
+    const activeCaseLoadId = session?.currentLocation?.nomis_agency_id
+    if (username) {
+      // eslint-disable-next-line no-param-reassign
+      envelope.data.baseData!.properties = {
+        username,
+        activeCaseLoadId,
+        ...envelope.data.baseData!.properties,
+      }
+    }
+  }
+  return true
+}
