4697ab59a67ad1bcb38671490b5fa50dcf3937c4
Adam Sharp <adam.sharp@digital.justice.gov.uk>
2023-06-15 16:14:06 +0100

    feat: Add planned lodges to move details

--- common/presenters/move-to-journeys-summary.js
+++ common/presenters/move-to-journeys-summary.js
@@ -25,23 +25,69 @@ module.exports = (move, journeys, { formatDate = filters.formatDate } = {}) => {
     ({ state }) => state !== 'rejected' && state !== 'cancelled'
   )
 
-  // order the journeys by date, then by creation order
-  filteredJourneys.sort(
+  const hasJourneysOnDifferentDay =
+    filteredJourneys.filter(({ date }) => date !== move.date).length !== 0
+
+  const overnightLodges = move.timeline_events
+    ?.filter(e => e.event_type === 'MoveOvernightLodge')
+    .sort((a, b) =>
+      (b.details.end_date || '') > (a.details.end_date || '') ? -1 : 1
+    )
+
+  if (!hasJourneysOnDifferentDay && !overnightLodges?.length) {
+    if (move.is_lockout) {
+      return presentLockout(move, formatDate)
+    }
+
+    return [presentMoveOrJourney(move, formatDate)]
+  }
+
+  const journeysWithLodges = [...filteredJourneys]
+  journeysWithLodges.push(
+    ...overnightLodges
+      .map((lodge, i) => {
+        const from =
+          i > 0 ? overnightLodges[i - 1].location : move.from_location
+
+        return {
+          date: lodge.details.start_date,
+          status: 'proposed',
+          from_location: from,
+          to_location: lodge.location,
+          lodge,
+        }
+      })
+      .filter((lodge, i) => {
+        // filter out any lodges that have journeys, to avoid duplication
+        return !filteredJourneys.find(
+          journey =>
+            journey.from_location.id === lodge.from_location.id &&
+            journey.to_location.id === lodge.to_location.id
+        )
+      })
+  )
+
+  // order by date, then by creation order
+  journeysWithLodges.sort(
     (a, b) =>
       a.date.localeCompare(b.date) ||
       new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime()
   )
 
-  const hasJourneysOnDifferentDay =
-    filteredJourneys.filter(({ date }) => date !== move.date).length !== 0
+  const lastJourney = journeysWithLodges[journeysWithLodges.length - 1]
 
-  if (hasJourneysOnDifferentDay) {
-    return filteredJourneys.map(journey =>
-      presentMoveOrJourney(journey, formatDate)
-    )
-  } else if (move.is_lockout) {
-    return presentLockout(move, formatDate)
-  } else {
-    return [presentMoveOrJourney(move, formatDate)]
+  // if the last journey is a lodge, we need to add a fake journey from that
+  //   lodge to complete the move
+  if (lastJourney.lodge) {
+    journeysWithLodges.push({
+      date: lastJourney.lodge.details.end_date,
+      status: 'proposed',
+      from_location: lastJourney.lodge.location,
+      to_location: move.to_location,
+    })
   }
+
+  return journeysWithLodges.map(journey =>
+    presentMoveOrJourney(journey, formatDate)
+  )
 }
