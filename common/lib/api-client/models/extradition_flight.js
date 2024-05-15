0ba1b204b731e0ade84e264f7ef310eb0291f7dd
Daniel Benton <daniel.benton@digital.justice.gov.uk>
2024-05-15 16:40:05 +0100

    fix: MAP-277 Add ignored js file

---
+++ common/lib/api-client/models/extradition_flight.js
@@ -0,0 +1,11 @@
+module.exports = {
+  fields: {
+    flight_time: '',
+    flight_number: '',
+  },
+  options: {
+    pluralize: false,
+    collectionPath: 'extradition_flight',
+  },
+  pluralize: false,
+}
