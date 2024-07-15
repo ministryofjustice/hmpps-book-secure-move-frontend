459d41efbf3f85dfc02863d69ee3d7aa964aadc8
danbenton-mojdt <113102670+danbenton-mojdt@users.noreply.github.com>
2024-07-15 13:15:09 +0100

    MAP-1393 (#3204)
    * chore: MAP-1393 Add dob validation after 1900

--- app/move/app/new/fields/date-of-birth.js
+++ app/move/app/new/fields/date-of-birth.js
@@ -1,10 +1,21 @@
 const { cloneDeep } = require('lodash')
 
+const { after } = require('../../../validators')
+
 const commonDateField = require('./common.date')
 
 const dateOfBirth = {
   ...cloneDeep(commonDateField),
-  validate: [...commonDateField.validate, 'required', 'before'],
+  validate: [
+    ...commonDateField.validate,
+    { type: 'required' },
+    { type: 'before' },
+    {
+      fn: after,
+      message: 'Date of birth should be after 01/01/1900',
+      arguments: ['1900-01-01'],
+    },
+  ],
   label: {
     text: 'fields::date_of_birth.label',
     classes: 'govuk-label--s',
