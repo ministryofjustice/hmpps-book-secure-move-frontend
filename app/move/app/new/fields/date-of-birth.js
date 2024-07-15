61c0734f4fb81ea47a6fc968ce799c03f619350a
Daniel Benton <daniel.benton@digital.justice.gov.uk>
2024-07-15 11:43:46 +0100

    chore: MAP-1393 Add dob validation

--- app/move/app/new/fields/date-of-birth.js
+++ app/move/app/new/fields/date-of-birth.js
@@ -6,11 +6,15 @@ const commonDateField = require('./common.date')
 
 const dateOfBirth = {
   ...cloneDeep(commonDateField),
-  validate: [...commonDateField.validate, 'required', 'before', after1900],
-  // {
-  //   type: after1900,
-  //   message: 'Date of birth should be after 01/01/1900',
-  // },
+  validate: [
+    ...commonDateField.validate,
+    { type: 'required' },
+    { type: 'before' },
+    {
+      fn: after1900,
+      message: 'Date of birth should be after 01/01/1900',
+    },
+  ],
   label: {
     text: 'fields::date_of_birth.label',
     classes: 'govuk-label--s',
