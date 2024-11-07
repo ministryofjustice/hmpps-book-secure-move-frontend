8d2da7e195839f723bec7d53f4cc6af9b60aa03b
Barry Gee <barry.gee@justice.gov.uk>
2024-11-07 07:54:07 +0000

    fix(MAP-1707): Fix indentation

--- app/police-custody-form/controllers.ts
+++ app/police-custody-form/controllers.ts
@@ -13,46 +13,46 @@ export const addEvents = async (
     req: BasmRequest,
     res: BasmResponse
     ): Promise<void> => {
-    const { user, move, journeys } = req
-    const lockoutEvents = req.body
-    const moveId = move.id
+        const { user, move, journeys } = req
+        const lockoutEvents = req.body
+        const moveId = move.id
 
-    const errors = [lockoutEvents.events || []]
-        .flat()
-        .filter((e: string) => lockoutEvents[e] === '')
+        const errors = [lockoutEvents.events || []]
+            .flat()
+            .filter((e: string) => lockoutEvents[e] === '')
 
-    if (lockoutEvents.events === undefined || errors.length > 0) {
-        const mappedErrors = mapErrorMessages(errors)
-        res.locals.showErrorsSummary = true
+        if (lockoutEvents.events === undefined || errors.length > 0) {
+            const mappedErrors = mapErrorMessages(errors)
+            res.locals.showErrorsSummary = true
 
-        if (errors.length > 0) {
-        res.locals.formErrors = mappedErrors
-        res.locals.formData = req.body
+            if (errors.length > 0) {
+                res.locals.formErrors = mappedErrors
+                res.locals.formData = req.body
+            }
+
+            delete res.breadcrumb
+            return res.render('police-custody-form/police-custody-form')
         }
 
-        delete res.breadcrumb
-        return res.render('police-custody-form/police-custody-form')
-    }
+        await req.services.event?.postLockoutEvents(
+            req,
+            lockoutEvents,
+            move,
+            journeys, 
+            user
+        )
+    
+        const fullName = move.profile?.person._fullname || 'Unknown'
 
-    await req.services.event?.postLockoutEvents(
-        req,
-        lockoutEvents,
-        move,
-        journeys, 
-        user
-    )
-  
-    const fullName = move.profile?.person._fullname || 'Unknown'
-
-    req.flash('success', {
-        title: req.t('messages::events_added.heading', {
-        type: move.is_lockout ? 'Lockout' : 'Overnight lodge'
-        }),
-        content: req.t('messages::events_added.content', {
-        fullName,
-        type: move.is_lockout ? 'lockout' : 'overnight lodge'
+        req.flash('success', {
+            title: req.t('messages::events_added.heading', {
+            type: move.is_lockout ? 'Lockout' : 'Overnight lodge'
+            }),
+            content: req.t('messages::events_added.content', {
+            fullName,
+            type: move.is_lockout ? 'lockout' : 'overnight lodge'
+            })
         })
-    })
 
-    return res.redirect(`/move/${moveId}`)
+        return res.redirect(`/move/${moveId}`)
 }
