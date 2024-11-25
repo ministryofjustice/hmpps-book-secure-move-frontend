407654099bdcb72e961a55b8caf4bf01edc78631
Jim Bailey <jim.bailey@digital.justice.gov.uk>
2024-11-25 15:04:44 +0000

    chore: Add "for bag 1" etc to property bag validation errors
    MAP-879

--- common/helpers/field/add-errorlist-to-errors.test.js
+++ common/helpers/field/add-errorlist-to-errors.test.js
@@ -10,7 +10,7 @@ const addErrorListToErrors = proxyquire('./add-errorlist-to-errors', {
 describe('Field helpers', function () {
   describe('#addErrorListToErrors()', function () {
     let errors
-    const mockFields = {
+    let mockFields = {
       fieldOne: {
         id: 'field-one',
       },
@@ -82,6 +82,78 @@ describe('Field helpers', function () {
 
       it('should get error messages correct number of times', function () {
         expect(getFieldErrorMessageStub.callCount).to.equal(2)
+      })
+
+      it('should transform errors object and add errorList property', function () {
+        expect(errors).to.deep.equal({
+          ...mockErrors,
+          errorList: [
+            {
+              href: `#${mockFields.fieldOne.id}`,
+              html: `${mockErrors.fieldOne.key}.${mockErrors.fieldOne.type}`,
+            },
+            {
+              href: `#${mockFields.fieldTwo.id}`,
+              html: `${mockErrors.fieldTwo.key}.${mockErrors.fieldTwo.type}`,
+            },
+          ],
+        })
+      })
+    })
+
+    context('when there are property bag errors', function () {
+      mockFields = {
+        fieldOne: {
+          id: 'field-one',
+          prefix: 'property-bags[0]',
+        },
+        fieldTwo: {
+          id: 'field-two',
+          prefix: 'property-bags[1]',
+        },
+      }
+
+      const mockErrors = {
+        fieldOne: {
+          key: 'fieldOne',
+          message: 'Enter the seal number',
+          type: 'required',
+          url: '/step-url',
+        },
+        fieldTwo: {
+          key: 'fieldTwo',
+          message: 'Select the types of property',
+          type: 'required',
+          url: '/step-url',
+        },
+      }
+
+      beforeEach(function () {
+        errors = addErrorListToErrors(mockErrors, mockFields)
+      })
+
+      it('should contain correct number of errors', function () {
+        expect(errors.errorList.length).to.equal(2)
+      })
+
+      it('should get error messages for fields that have errors', function () {
+        expect(getFieldErrorMessageStub).to.be.calledWithExactly({
+          ...mockFields.fieldOne,
+          ...mockErrors.fieldOne,
+        })
+        expect(getFieldErrorMessageStub).to.be.calledWithExactly({
+          ...mockFields.fieldTwo,
+          ...mockErrors.fieldTwo,
+        })
+      })
+
+      it('should get error messages with the correct args', function () {
+        expect(getFieldErrorMessageStub.getCall(0).firstArg.message).to.equal(
+          'Enter the seal number for bag 1'
+        )
+        expect(getFieldErrorMessageStub.getCall(1).firstArg.message).to.equal(
+          'Select the types of property for bag 2'
+        )
       })
 
       it('should transform errors object and add errorList property', function () {
