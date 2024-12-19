a476abe5a7bda07b76130d5c2c7473055050dcf6
Barry Gee <barry.gee@justice.gov.uk>
2024-12-19 09:23:59 +0000

    feat(MAP-1861): Add html and initial logic to display SASH data

--- common/presenters/event-to-timeline-panel.js
+++ common/presenters/event-to-timeline-panel.js
@@ -1,24 +1,77 @@
-const filters = require('../../config/nunjucks/filters')
 const eventHelpers = require('../helpers/events')
 
 const eventToTagComponent = require('./event-to-tag-component')
 
+/* eslint-disable-next-line require-await */
 module.exports = async (token, moveEvent, move) => {
   const event = eventHelpers.setEventDetails(moveEvent, move)
-  const { id, occurred_at: timestamp } = event
-  const description = await eventHelpers.getDescription(token, event)
+  const { id, event_type: eventType, details } = event
+  const {
+    nature_of_self_harm: natureOfSelfHarm,
+    history_of_self_harm_recency: historyOfSelfHarmRecency,
+  } = details
+  // const description = await eventHelpers.getDescription(token, event)
+
+  const rows = []
+
+  if (eventType === 'PerSuicideAndSelfHarm') {
+    natureOfSelfHarm &&
+      rows.push([
+        {
+          heading:
+            '<h4 class="govuk-heading-s govuk-!-font-size-16">Concern</h4>',
+        },
+        {
+          html: natureOfSelfHarm
+            .map(
+              obj =>
+                `<p class="govuk-!-font-size-16">${obj.option} - ${obj.details}</p>`
+            )
+            .join(''),
+        },
+      ])
+
+    historyOfSelfHarmRecency &&
+      rows.push([
+        {
+          heading:
+            '<h4 class="govuk-heading-s govuk-!-font-size-16">History</h4>',
+        },
+        {
+          html: `<time class="govuk-!-font-size-16" datetime="${historyOfSelfHarmRecency}">${historyOfSelfHarmRecency}</time>`,
+        },
+      ])
+  }
+
+  let html = `
+    <table class="govuk-table">
+      <tbody class="govuk-table__body">
+  `
+  // Loop through the rows array and add dynamic rows
+  rows.forEach(row => {
+    const heading = row[0].heading
+    const htmlContent = row[1].html
+
+    html += `
+      <tr class="govuk-table__row">
+        <th scope="row" class="govuk-table__header">${heading}</th>
+        <td class="govuk-table__cell">${htmlContent}</td>
+      </tr>
+    `
+  })
+
+  // Close the table tag
+  html += `
+      </tbody>
+    </table>
+  `
+
+  // console.log(html)
 
   const tag = eventToTagComponent(event)
   delete tag.href
 
-  const formattedDate = filters.formatDateWithTimeAndDay(timestamp)
-
-  const html = `
-    <div class="app-timeline__description">${description}</div>
-    <div class="app-timeline__date">
-      <time datetime="${timestamp}">${formattedDate}</time>
-    </div>
-  `
+  // const formattedDate = filters.formatDateWithTimeAndDay(timestamp)
 
   return {
     tag,
