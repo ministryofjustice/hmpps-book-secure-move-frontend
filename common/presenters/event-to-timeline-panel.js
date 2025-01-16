10d039984db94804da3426394042a030f7a939d4
Barry Gee <barry.gee@justice.gov.uk>
2025-01-16 09:45:09 +0000

    fix(MAP-1917): Remove eslint comment

--- common/presenters/event-to-timeline-panel.js
+++ common/presenters/event-to-timeline-panel.js
@@ -3,7 +3,6 @@ const eventHelpers = require('../helpers/events')
 
 const eventToTagComponent = require('./event-to-tag-component')
 
-/* eslint-disable-next-line require-await */
 module.exports = async (
   token,
   moveEvent,
