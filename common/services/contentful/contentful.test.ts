154f4881ad898998c0889e3c1aadefc3ff48a50b
Barry Gee <barry.gee@justice.gov.uk>
2025-03-03 14:28:22 +0000

    feat(MAP-1857): Refactor unit test

--- common/services/contentful/contentful.test.ts
+++ common/services/contentful/contentful.test.ts
@@ -441,11 +441,11 @@ const dedicatedContentMockedResponse = {
 
 const formattedEntriesMockResponse = {
   bannerContent: {
-    body: '',
-    date: ''
+    body: "This is the banner body text",  // Set a mock body
+    date: "2025-03-03",  // Set a mock date
   },
-  posts: {
-    title: 'string',
+  posts: [{
+    title: 'Post',
     body: '<h1 class="govuk-heading-xl govuk-!-margin-top-6">The latest updates and improvements to Book a secure' +
           ' move.</h1><h2 class="govuk-heading-l govuk-!-margin-top-5">Test heading 2.</h2><h3 class="govuk-heading-m' +
           ' govuk-!-margin-top-4"><em>Test heading 3.</em></h3><h4 class="govuk-heading-s govuk-!-margin-top-3">Test' +
@@ -456,8 +456,8 @@ const formattedEntriesMockResponse = {
           '</p></li><li><p class="govuk-template__body">TEST LINE 2</p></li></ol><figure class="govuk-!-margin-top-6 ' +
           'govuk-!-margin-bottom-6"><img src="https://images.ctfassets.net/m5k1kmk3zqwh/4W3q8OwEoyEQxjJtdtCkbg/51b7fc' +
           '14e8d568d5f5314733e1b9aadb/image.png" alt="asset-test" /></figure>',
-    date: ''
-  }
+    date: '2025-03-03'
+  }]
 }
 
 const emptyMockedResponse = { items: [] }
@@ -476,7 +476,7 @@ describe('ContentfulService', function () {
         .resolves(whatsNewMockedResponse)
       
       sinon
-        .stub((contentfulService as any).client, 'fetch')
+        .stub(contentfulService, 'fetch')
         .resolves(formattedEntriesMockResponse)
     })
 
