61f0bbdc4f80f2e6ad3b744088a66d29ad2c7e36
Barry Gee <barry.gee@justice.gov.uk>
2025-03-05 14:41:18 +0000

    fix(MAP-1857): Linting error fixes

--- common/services/contentful/contentful.test.ts
+++ common/services/contentful/contentful.test.ts
@@ -449,20 +449,20 @@ const formattedEntriesMockResponse = {
     body: 'Some text briefly explaining the changes.',
     date: formattedTodaysDate,
   },
-  posts: [{    
-      title: 'Whats new today!',
-      body:
-        '<h1 class="govuk-heading-xl govuk-!-margin-top-6">The latest updates and improvements to Book a secure' +
-        ' move.</h1><h2 class="govuk-heading-l govuk-!-margin-top-5">Test heading 2.</h2><h3 class="govuk-heading-m' +
-        ' govuk-!-margin-top-4"><em>Test heading 3.</em></h3><h4 class="govuk-heading-s govuk-!-margin-top-3">Test' +
-        ' heading 4.</h4><a class="govuk-link" href="https://google.com">Test Link</a><p class="govuk-template__body">' +
-        '<strong>Some random paragraph text.</strong></p><ul class="govuk-list govuk-list--bullet ' +
-        'govuk-list-bullet-bottom-padding"><li><p class="govuk-template__body">TEST LINE 1</p></li><li><p class="govuk-template__body">' +
-        'TEST LINE 2</p></li></ul><ol class="govuk-list govuk-list--number"><li><p class="govuk-template__body">TEST LINE 1' +
-        '</p></li><li><p class="govuk-template__body">TEST LINE 2</p></li></ol><figure class="govuk-!-margin-top-6 ' +
-        'govuk-!-margin-bottom-6"><img src="https://images.ctfassets.net/m5k1kmk3zqwh/4W3q8OwEoyEQxjJtdtCkbg/51b7fc' +
-        '14e8d568d5f5314733e1b9aadb/image.png" alt="asset-test" /></figure>',
-      date: formattedTodaysDate,
+  posts: [{
+    title: 'Whats new today!',
+    body:
+      '<h1 class="govuk-heading-xl govuk-!-margin-top-6">The latest updates and improvements to Book a secure' +
+      ' move.</h1><h2 class="govuk-heading-l govuk-!-margin-top-5">Test heading 2.</h2><h3 class="govuk-heading-m' +
+      ' govuk-!-margin-top-4"><em>Test heading 3.</em></h3><h4 class="govuk-heading-s govuk-!-margin-top-3">Test' +
+      ' heading 4.</h4><a class="govuk-link" href="https://google.com">Test Link</a><p class="govuk-template__body">' +
+      '<strong>Some random paragraph text.</strong></p><ul class="govuk-list govuk-list--bullet ' +
+      'govuk-list-bullet-bottom-padding"><li><p class="govuk-template__body">TEST LINE 1</p></li><li><p class="govuk-template__body">' +
+      'TEST LINE 2</p></li></ul><ol class="govuk-list govuk-list--number"><li><p class="govuk-template__body">TEST LINE 1' +
+      '</p></li><li><p class="govuk-template__body">TEST LINE 2</p></li></ol><figure class="govuk-!-margin-top-6 ' +
+      'govuk-!-margin-bottom-6"><img src="https://images.ctfassets.net/m5k1kmk3zqwh/4W3q8OwEoyEQxjJtdtCkbg/51b7fc' +
+      '14e8d568d5f5314733e1b9aadb/image.png" alt="asset-test" /></figure>',
+    date: formattedTodaysDate,
     },
   ],
 }
@@ -478,7 +478,8 @@ describe('ContentfulService', function () {
 
   context('with a response for whats new content', function () {
     beforeEach(function () {
-        sinon.stub((contentfulService as any).client, 'getEntries')
+        sinon
+          .stub((contentfulService as any).client, 'getEntries')
           .resolves(whatsNewMockedResponse)
 
         sinon
