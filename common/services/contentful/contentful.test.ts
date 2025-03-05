c4e74c0a91a3cdf3c31da7726762b4d621d1d34d
Barry Gee <barry.gee@justice.gov.uk>
2025-03-05 12:07:55 +0000

    fix(MAP-1857): Linting

--- common/services/contentful/contentful.test.ts
+++ common/services/contentful/contentful.test.ts
@@ -11,7 +11,7 @@ const todaysDate = new Date()
 const formattedTodaysDate = todaysDate.toLocaleDateString('en-GB', {
   day: 'numeric',
   month: 'long',
-  year: 'numeric'
+  year: 'numeric',
 })
 
 const whatsNewMockedResponse = {
@@ -446,8 +446,8 @@ const dedicatedContentMockedResponse = {
 
 const formattedEntriesMockResponse = {
   bannerContent: {
-    body: "Some text briefly explaining the changes.",
-    date: formattedTodaysDate
+    body: 'Some text briefly explaining the changes.',
+    date: formattedTodaysDate,
   },
   posts: [{
     title: 'Whats new today!',
@@ -489,16 +489,7 @@ describe('ContentfulService', function () {
       const formattedEntries = await contentfulService.fetch()
       // @ts-ignore
       expect(formattedEntries.posts[0].body).to.equal(
-        '<h1 class="govuk-heading-xl govuk-!-margin-top-6">The latest updates and improvements to Book a secure' +
-          ' move.</h1><h2 class="govuk-heading-l govuk-!-margin-top-5">Test heading 2.</h2><h3 class="govuk-heading-m' +
-          ' govuk-!-margin-top-4"><em>Test heading 3.</em></h3><h4 class="govuk-heading-s govuk-!-margin-top-3">Test' +
-          ' heading 4.</h4><a class="govuk-link" href="https://google.com">Test Link</a><p class="govuk-template__body">' +
-          '<strong>Some random paragraph text.</strong></p><ul class="govuk-list govuk-list--bullet ' +
-          'govuk-list-bullet-bottom-padding"><li><p class="govuk-template__body">TEST LINE 1</p></li><li><p class="govuk-template__body">' +
-          'TEST LINE 2</p></li></ul><ol class="govuk-list govuk-list--number"><li><p class="govuk-template__body">TEST LINE 1' +
-          '</p></li><li><p class="govuk-template__body">TEST LINE 2</p></li></ol><figure class="govuk-!-margin-top-6 ' +
-          'govuk-!-margin-bottom-6"><img src="https://images.ctfassets.net/m5k1kmk3zqwh/4W3q8OwEoyEQxjJtdtCkbg/51b7fc' +
-          '14e8d568d5f5314733e1b9aadb/image.png" alt="asset-test" /></figure>'
+        formattedEntriesMockResponse.posts[0].body
       )
     })
     it('returns the content title', async function () {
