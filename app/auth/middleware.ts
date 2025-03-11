7746c6dc3017f30c866dac72bcab506acc63c0a9
Thource <me@thource.dev>
2025-03-11 16:05:38 +0000

    feat: Make ip blocking code work

--- app/auth/middleware.ts
+++ app/auth/middleware.ts
@@ -1,15 +1,24 @@
-const { get } = require('lodash')
+import { NextFunction } from 'express'
+import { get } from 'lodash'
+
+import { BasmError } from '../../common/types/basm_error'
+import { BasmRequest } from '../../common/types/basm_request'
+import { BasmResponse } from '../../common/types/basm_response'
 
 const { decodeAccessToken } = require('../../common/lib/access-token')
 const { loadUser } = require('../../common/lib/user')
 const { OFF_NETWORK_ALLOWLIST } = require('../../config')
 
 function processAuthResponse() {
-  return async function middleware(req, res, next) {
+  return async function middleware(
+    req: BasmRequest,
+    _res: BasmResponse,
+    next: NextFunction
+  ) {
     const accessToken = get(req.session, 'grant.response.access_token')
 
     if (!accessToken) {
-      const error = new Error('Could not authenticate user')
+      const error = new Error('Could not authenticate user') as BasmError
       error.statusCode = 403
       return next(error)
     }
@@ -20,7 +29,7 @@ function processAuthResponse() {
 
       const user = await loadUser(req, accessToken)
 
-      req.session.regenerate(error => {
+      req.session.regenerate((error: BasmError) => {
         if (error) {
           return next(error)
         }
@@ -29,17 +38,24 @@ function processAuthResponse() {
 
         const authSource = decodedAccessToken.auth_source
 
-        if (authSource !== 'auth') {
+        if (authSource !== 'auth' && OFF_NETWORK_ALLOWLIST !== '*') {
           // user is a staff member
           // check their IP address against OFF_NETWORK_ALLOWLIST
-          const ipAddress = req.headers['x-forwarded-for'] || 
-                           req.connection.remoteAddress || 
-                           req.socket.remoteAddress ||
-                           (req.connection.socket ? req.connection.socket.remoteAddress : null);
+          let ipAddress =
+            (req.headers && req.headers['x-forwarded-for']) ||
+            req.connection?.remoteAddress ||
+            req.socket?.remoteAddress ||
+            req.connection?.socket?.remoteAddress
 
-          // Note: changed include? to includes
-          if (!OFF_NETWORK_ALLOWLIST.includes(ipAddress)) {
-            const error = new Error('Access denied from this network location')
+          // convert ip from ipv6 to ipv4
+          if (ipAddress?.startsWith('::ffff:')) {
+            ipAddress = ipAddress.replace('::ffff:', '')
+          }
+
+          if (ipAddress && !(OFF_NETWORK_ALLOWLIST || '').includes(ipAddress)) {
+            const error = new Error(
+              'Access denied from this network location'
+            ) as BasmError
             error.statusCode = 403
             return next(error)
           }
