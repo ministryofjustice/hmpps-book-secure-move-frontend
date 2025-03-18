e782e6edc35b372495c93704ea8f525a219d3ddf
Barry Gee <barry.gee@justice.gov.uk>
2025-03-18 14:14:58 +0000

    feat(MAP-2223): Update

--- app/move/app/new/fields/common.date-picker.js
+++ app/move/app/new/fields/common.date-picker.js
@@ -3,24 +3,9 @@ const { date: dateFormatter } = require('../../../../../common/formatters')
 const date = {
   validate: ['date'],
   formatter: [dateFormatter],
-  component: 'raw',
-  html: `
-    <div class="moj-datepicker" data-module="moj-date-picker">
-
-    <div class="govuk-form-group">
-      <label class="govuk-label" for="date">
-        Date
-      </label>
-
-      <div id="date-hint" class="govuk-hint">
-        For example, 17/5/2024.
-      </div>
-
-      <input class="govuk-input moj-js-datepicker-input" id="date_picker" name="date_picker" type="text" aria-describedby="date-hint" autocomplete="off">
-
-    </div>
-
-  </div>`,
+  component: 'mojDatePicker',
+  autocomplete: 'off',
+  classes: 'govuk-input--width-10',
   hint: {
     text: 'fields::date.hint',
   },
