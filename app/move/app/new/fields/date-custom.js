8da4810e1998a35503e24364802cf360beaba7c5
Barry Gee <barry.gee@justice.gov.uk>
2025-03-26 12:24:48 +0000

    feat(MAP-2223): Refactor to correct id and name fields

--- app/move/app/new/fields/date-custom.js
+++ app/move/app/new/fields/date-custom.js
@@ -5,8 +5,8 @@ const commonDatePickerField = require('./common.date-picker')
 const datePicker = {
   ...cloneDeep(commonDatePickerField),
   validate: [...commonDatePickerField.validate, 'required', 'after'],
-  id: 'date',
-  name: 'date',
+  id: 'date_custom',
+  name: 'date_custom',
   classes: 'govuk-input__wrapper',
   label: {
     text: 'fields::date_custom.label',
