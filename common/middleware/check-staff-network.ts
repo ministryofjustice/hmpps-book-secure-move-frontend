0ae4c9fc652749a7fc35d67c1cca1c79da7e481a
danbenton-mojdt <113102670+danbenton-mojdt@users.noreply.github.com>
2025-04-15 17:18:28 +0100

    Revert "MAP-2156 Revert commits to 'prevent prison staff from accessing off n…"
    This reverts commit 31787b629fe68c5c1002b3f31bf89a9b59ae4b77.

---
+++ common/middleware/check-staff-network.ts
@@ -0,0 +1,57 @@
+import { NextFunction } from 'express'
+import { get } from 'lodash'
+import { Netmask } from 'netmask'
+
+import { OFF_NETWORK_ALLOWLIST } from '../../config'
+import { BasmError } from '../types/basm_error'
+import { BasmRequest } from '../types/basm_request'
+import { BasmResponse } from '../types/basm_response'
+
+const { decodeAccessToken } = require('../lib/access-token')
+
+export default (req: BasmRequest, _res: BasmResponse, next: NextFunction) => {
+  if (req.url.startsWith('/auth/') || req.url.startsWith('/connect/')) {
+    return next()
+  }
+
+  const accessToken = get(req.session, 'grant.response.access_token')
+
+  if (!accessToken) {
+    return next()
+  }
+
+  const decodedAccessToken = decodeAccessToken(accessToken)
+
+  if (
+    decodedAccessToken.auth_source !== 'auth' &&
+    !OFF_NETWORK_ALLOWLIST.includes('*')
+  ) {
+    // user is a staff member
+    // check their IP address against OFF_NETWORK_ALLOWLIST
+    let ipAddress =
+      (req.headers && req.headers['x-forwarded-for']) ||
+      req.connection?.remoteAddress ||
+      req.socket?.remoteAddress ||
+      req.connection?.socket?.remoteAddress
+
+    // convert ip from ipv6 to ipv4
+    if (ipAddress?.startsWith('::ffff:')) {
+      ipAddress = ipAddress.replace('::ffff:', '')
+    }
+
+    if (
+      ipAddress &&
+      !OFF_NETWORK_ALLOWLIST.find(subnet =>
+        new Netmask(subnet).contains(ipAddress!)
+      )
+    ) {
+      const error = new Error(
+        'Access denied from this network location'
+      ) as BasmError
+      error.statusCode = 403
+      return next(error)
+    }
+  }
+
+  next()
+}
