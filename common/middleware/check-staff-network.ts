4cdfd44c87fa69ef402260850ef79f03f8846eca
danbenton-mojdt <113102670+danbenton-mojdt@users.noreply.github.com>
2025-04-17 16:32:48 +0100

    Map 2156 fix username (#3283)
    * fix: MAP-2156 Use username when logging off-network access, switch off in UAT
    
    * feat: MAP-2156 Stop reporting image access, consolidate access reports
    
    * fix: MAP-2156 Log correct thing
    
    * feat: MAP-2156 Ignore urls that redirect

--- common/middleware/check-staff-network.ts
+++ common/middleware/check-staff-network.ts
@@ -18,7 +18,16 @@ import { BasmResponse } from '../types/basm_response'
 const { decodeAccessToken } = require('../lib/access-token')
 
 export default (req: BasmRequest, _res: BasmResponse, next: NextFunction) => {
-  if (req.url.startsWith('/auth/') || req.url.startsWith('/connect/')) {
+  const { url } = req
+  if (
+    url.startsWith('/auth/') ||
+    url.startsWith('/connect/') ||
+    (url.startsWith('/person/') && url.endsWith('/image')) ||
+    url.startsWith('/moves/outgoing') ||
+    url.startsWith('/moves/incoming') ||
+    url.startsWith('/moves/requested') ||
+    url.startsWith('/allocations/outgoing')
+  ) {
     return next()
   }
 
@@ -29,79 +38,103 @@ export default (req: BasmRequest, _res: BasmResponse, next: NextFunction) => {
   }
 
   const decodedAccessToken = decodeAccessToken(accessToken)
-  let badDevice = false
-
   if (decodedAccessToken.auth_source !== 'auth') {
     // user is a staff member
-    if (DISALLOWED_DEVICES && DISALLOWED_DEVICES.length) {
+    const device = (req.headers && req.headers['user-agent'] || 'Unknown')
+    const badDevice = DISALLOWED_DEVICES && DISALLOWED_DEVICES.length > 0 && DISALLOWED_DEVICES.some(d => device.includes(d))
 
-      let device = (req.headers && req.headers['user-agent'] || 'Unknown')
+    let ipAddress =
+    (req.headers && req.headers['x-forwarded-for']) ||
+      req.connection?.remoteAddress ||
+      req.socket?.remoteAddress ||
+      req.connection?.socket?.remoteAddress
 
-      if (DISALLOWED_DEVICES.some(d => device.includes(d))) {
-        badDevice = true
-        if (DISALLOWED_DEVICES_ACTIONS.includes('WARN')) {
-          console.log(`${req.user.username} has accessed service from a disallowed device: ${device}`)
-        }
-        if (DISALLOWED_DEVICES_ACTIONS.includes('REPORT')) {
-          Sentry.captureException(new Error('User access from disallowed device'))
-        }
+    // convert ip from ipv6 to ipv4
+    if (ipAddress?.startsWith('::ffff:')) {
+      ipAddress = ipAddress.replace('::ffff:', '')
+    }
+    if (ipAddress?.startsWith('::1')) {
+      ipAddress = '127.0.0.1'
+    }
+    const offNetwork = !ipAddressPermitted(ipAddress)
 
-        if (DISALLOWED_DEVICES_ACTIONS.includes('ENFORCE')) {
-          const error = new Error(
-            'Access denied from this device'
-          ) as BasmError
-          error.statusCode = 403
-          return next(error)
-        }
+    //Do any logging we need to
+    if (offNetwork && badDevice && (OFF_NETWORK_ALLOWLIST_ACTIONS.includes('WARN') || DISALLOWED_DEVICES_ACTIONS.includes('WARN'))) {
+      console.log(`${req.user.username} has accessed service from a disallowed device and has an IP address outside of the allowlist: [${ipAddress}]`)
+    } else
+    {
+      if (offNetwork && OFF_NETWORK_ALLOWLIST_ACTIONS.includes('WARN')) {
+        console.log(`${req.user.username} has accessed service from an IP address outside of the allowlist: [${ipAddress}]`)
+      }
+      if (badDevice && DISALLOWED_DEVICES_ACTIONS.includes('WARN')) {
+        console.log(`${req.user.username} has accessed service from a disallowed device: ${device}`)
       }
     }
-    if (!OFF_NETWORK_ALLOWLIST.includes('*')){
-      // check their IP address against OFF_NETWORK_ALLOWLIST
-      let ipAddress =
-        (req.headers && req.headers['x-forwarded-for']) ||
-        req.connection?.remoteAddress ||
-        req.socket?.remoteAddress ||
-        req.connection?.socket?.remoteAddress
 
-      // convert ip from ipv6 to ipv4
-      if (ipAddress?.startsWith('::ffff:')) {
-        ipAddress = ipAddress.replace('::ffff:', '')
-      } if (ipAddress?.startsWith('::1')) {
-        ipAddress = '127.0.0.1'
-      }
-
+    //Do any reporting to Sentry we need to, but only one report
+    if (offNetwork && badDevice) {
       if (
-        ipAddress &&
-        !OFF_NETWORK_ALLOWLIST.find(subnet =>
-          new Netmask(subnet).contains(ipAddress!)
-        )
+        OFF_NETWORK_ALLOWLIST_ACTIONS.includes('REPORT_IF_BAD_DEVICE') ||
+        OFF_NETWORK_ALLOWLIST_ACTIONS.includes('REPORT') ||
+        DISALLOWED_DEVICES_ACTIONS.includes('REPORT_IF_OFF_NETWORK') ||
+        DISALLOWED_DEVICES_ACTIONS.includes('REPORT')
       ) {
-        if (OFF_NETWORK_ALLOWLIST_ACTIONS.includes('WARN')){
-          console.log(`${req.user.username} has accessed service from an IP address outside of the allowlist: ${ipAddress}`)
-        }
-
-        if (OFF_NETWORK_ALLOWLIST_ACTIONS.includes('REPORT')){
-          Sentry.captureException(new Error('User access from outside of allowlist'))
-        }
-
-        if (OFF_NETWORK_ALLOWLIST_ACTIONS.includes('ENFORCE')){
-          const error = new Error(
-            'Access denied from this network location'
-          ) as BasmError
-          error.statusCode = 403
-          return next(error)
-        }
-
-        if (badDevice && OFF_NETWORK_ALLOWLIST_ACTIONS.includes('ENFORCE_IF_BAD_DEVICE')){
-          const error = new Error(
-            'Access denied from this device and network location'
-          ) as BasmError
-          error.statusCode = 403
-          return next(error)
-        }
+        Sentry.captureException(new Error('User access from outside of allowlist on disallowed device'))
       }
+    } else {
+
+      if (offNetwork && OFF_NETWORK_ALLOWLIST_ACTIONS.includes('REPORT')) {
+        Sentry.captureException(new Error('User access from outside of allowlist'))
+      }
+      if (badDevice && DISALLOWED_DEVICES_ACTIONS.includes('REPORT')){
+        Sentry.captureException(new Error('User access from disallowed device'))
+      }
+    }
+
+    if (
+      badDevice &&
+      offNetwork &&
+        (
+          OFF_NETWORK_ALLOWLIST_ACTIONS.includes('ENFORCE_IF_BAD_DEVICE') ||
+          DISALLOWED_DEVICES_ACTIONS.includes('ENFORCE_IF_OFF_NETWORK')
+        )
+      )
+    {
+      const error = new Error(
+        'Access denied from this device and network location'
+      ) as BasmError
+      error.statusCode = 403
+      return next(error)
+    }
+
+    if (offNetwork && OFF_NETWORK_ALLOWLIST_ACTIONS.includes('ENFORCE')) {
+      const error = new Error(
+        'Access denied from this network location'
+      ) as BasmError
+      error.statusCode = 403
+      return next(error)
+    }
+    if (badDevice && DISALLOWED_DEVICES && DISALLOWED_DEVICES_ACTIONS.includes('ENFORCE')) {
+      const error = new Error(
+        'Access denied from this device'
+      ) as BasmError
+      error.statusCode = 403
+      return next(error)
     }
   }
 
   next()
+}
+
+function ipAddressPermitted(ipAddress: string | undefined): boolean {
+  if (OFF_NETWORK_ALLOWLIST.includes('*')) {
+    return true
+  }
+
+  if (!ipAddress) {
+    return false
+  }
+  // check their IP address against OFF_NETWORK_ALLOWLIST
+  return OFF_NETWORK_ALLOWLIST.some(subnet =>
+    new Netmask(subnet).contains(ipAddress!))
 }
