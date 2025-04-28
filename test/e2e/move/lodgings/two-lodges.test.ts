c5801995fd0bd0c1a61fdc8bbdc26aa9756ac236
Dan Benton <daniel.benton@digital.justice.gov.uk>
2025-04-28 14:06:29 +0100

    chore: Lint

--- test/e2e/move/lodgings/two-lodges.test.ts
+++ test/e2e/move/lodgings/two-lodges.test.ts
@@ -1,8 +1,10 @@
-import { testLodges } from './_lodging-helpers'
 import { fixture } from 'testcafe'
+
+import { createMove } from '../../_move'
 import { pmuUser } from '../../_roles'
 import { home } from '../../_routes'
-import { createMove } from '../../_move'
+
+import { testLodges } from './_lodging-helpers'
 
 fixture('A move without any lodgings - adding two').beforeEach(async t => {
   await t.useRole(pmuUser).navigateTo(home)
