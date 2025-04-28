a02e790c79af1afc9c3b297ab50a673eb3ecbe62
Dan Benton <daniel.benton@digital.justice.gov.uk>
2025-04-28 14:03:35 +0100

    test: Rename one-day lodge tests

--- test/e2e/move/lodgings/one-lodge.test.ts
+++ test/e2e/move/lodgings/one-lodge.test.ts
@@ -6,20 +6,19 @@ import { home } from '../../_routes'
 
 import { testLodges } from './_lodging-helpers'
 
-fixture('A move without any lodges - one lodge').beforeEach(async t => {
+fixture('A move without any lodgings - adding one').beforeEach(async t => {
   await t.useRole(pmuUser).navigateTo(home)
   await createMove()
 })
 
-// eslint-disable-next-line no-undef
-test('Without any existing lodges', async function (t) {
+test('One-day lodging', async function (t) {
   await testLodges(t, 1, 1)
 })
 
-test('Without any existing lodges - two days', async function (t) {
+test('Two-day lodging', async function (t) {
   await testLodges(t, 1, 2)
 })
 
-test('Without any existing lodges - seven days', async function (t) {
+test('Seven-day lodging', async function (t) {
   await testLodges(t, 1, 7)
 })
