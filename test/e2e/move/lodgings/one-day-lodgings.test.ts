c0b9a0b5b7f5418d3ecf318cffac7a3584b7632a
danbenton-mojdt <113102670+danbenton-mojdt@users.noreply.github.com>
2025-04-29 10:10:29 +0100

    Update circle orb, enforce bad-device blocking (#3285)
    * ci: Update HMPPS CircleCI Orb
    
    * test: Split up lodgings tests
    
    * test: Rename one-day lodge tests
    
    * chore: Lint
    
    * chore: Import Selector
    
    * chore: Lint
    
    * test: Split lodgings tests over three files
    
    * feat: MAP-2156 Enforce bad device blocking for internal users

---
+++ test/e2e/move/lodgings/one-day-lodgings.test.ts
@@ -0,0 +1,20 @@
+import { fixture } from 'testcafe'
+
+import { createMove } from '../../_move'
+import { pmuUser } from '../../_roles'
+import { home } from '../../_routes'
+
+import { testLodges } from './_lodging-helpers'
+
+fixture('One-day lodgings').beforeEach(async t => {
+  await t.useRole(pmuUser).navigateTo(home)
+  await createMove()
+})
+
+test('Adding one', async function (t) {
+  await testLodges(t, 1, 1)
+})
+
+test('Adding two', async function (t) {
+  await testLodges(t, 2, 1)
+})
