6ce1e31cf5cb0b56805562340e40669d24ec0d6b
Dan Benton <daniel.benton@digital.justice.gov.uk>
2025-06-06 14:43:45 +0100

    fix: MAP-2008 Reinstate cancel reason other in test

--- test/e2e/pages/cancel-move.ts
+++ test/e2e/pages/cancel-move.ts
@@ -7,6 +7,7 @@ import { Page } from './page'
 class CancelMovePage extends Page {
   fields: {
     cancellationReason: Selector
+    cancellationReasonOtherComment: Selector
     cancellationReasonCancelledByPMUComment: Selector
   }
 
@@ -15,6 +16,9 @@ class CancelMovePage extends Page {
 
     this.fields = {
       cancellationReason: Selector('[name="cancellation_reason"]'),
+      cancellationReasonOtherComment: Selector(
+        '#cancellation_reason_other_comment'
+      ),
       cancellationReasonCancelledByPMUComment: Selector(
         '#cancellation_reason_cancelled_by_pmu_comment'
       ),
