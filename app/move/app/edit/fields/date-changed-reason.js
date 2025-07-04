da5f9f029c7fc7147673b60eb3fecfb596714887
Dan Benton <daniel.benton@digital.justice.gov.uk>
2025-07-04 16:46:45 +0100

    fix: MAP-2301 Fix error message when no date date reason is selected

--- app/move/app/edit/fields/date-changed-reason.js
+++ app/move/app/edit/fields/date-changed-reason.js
@@ -1,50 +1,51 @@
 const dateChangedReason = {
+  id: 'date_changed_reason',
   validate: 'required',
   component: 'govukRadios',
   name: 'date_changed_reason',
   fieldset: {
     legend: {
-      text: 'fields::date_change_reason.label',
+      text: 'fields::date_changed_reason.label',
       classes: 'govuk-fieldset__legend--m',
     },
   },
   items: [
     {
-      id: 'date_change_reason',
+      id: 'date_changed_reason',
       value: 'combined_with_another_request',
-      text: 'fields::date_change_reason.items.combined_with_another_request.label',
+      text: 'fields::date_changed_reason.items.combined_with_another_request.label',
     },
     {
       value: 'operational_issues_prison',
-      text: 'fields::date_change_reason.items.operational_issues_prison.label',
+      text: 'fields::date_changed_reason.items.operational_issues_prison.label',
     },
     {
       value: 'tornado_event',
-      text: 'fields::date_change_reason.items.tornado_event.label',
+      text: 'fields::date_changed_reason.items.tornado_event.label',
     },
     {
       value: 'cancelled_by_pmu',
-      text: 'fields::date_change_reason.items.cancelled_by_pmu.label',
+      text: 'fields::date_changed_reason.items.cancelled_by_pmu.label',
     },
     {
       value: 'prisoner_refusal',
-      text: 'fields::date_change_reason.items.prisoner_refusal.label',
+      text: 'fields::date_changed_reason.items.prisoner_refusal.label',
     },
     {
       value: 'receiver_unable_to_accept_prisoner',
-      text: 'fields::date_change_reason.items.receiver_unable_to_accept_prisoner.label',
+      text: 'fields::date_changed_reason.items.receiver_unable_to_accept_prisoner.label',
     },
     {
       value: 'sender_unable_to_fulfil_draft',
-      text: 'fields::date_change_reason.items.sender_unable_to_fulfil_draft.label',
+      text: 'fields::date_changed_reason.items.sender_unable_to_fulfil_draft.label',
     },
     {
       value: 'sender_cancelled_request',
-      text: 'fields::date_change_reason.items.sender_cancelled_request.label',
+      text: 'fields::date_changed_reason.items.sender_cancelled_request.label',
     },
     {
       value: 'supplier_no_resource',
-      text: 'fields::date_change_reason.items.supplier_no_resource.label',
+      text: 'fields::date_changed_reason.items.supplier_no_resource.label',
     },
   ],
 }
