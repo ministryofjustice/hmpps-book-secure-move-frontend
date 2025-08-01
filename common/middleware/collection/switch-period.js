59011211bc0ddf81fcbf9581ac390c2a7e429c12
Dan Benton <daniel.benton@digital.justice.gov.uk>
2025-08-01 16:42:12 +0100

    fix: MAP-2573 Use referer when using collection view switchers

--- common/middleware/collection/switch-period.js
+++ common/middleware/collection/switch-period.js
@@ -2,7 +2,6 @@ function switchPeriod(defaults = {}) {
   return function handleSwitchPeriod(req, res) {
     const { view } = req.params
     let currentPeriod = req.session.period
-    const groupBy = req.session.group_by
 
     if (!currentPeriod) {
       currentPeriod = defaults[view]
@@ -10,10 +9,10 @@ function switchPeriod(defaults = {}) {
 
     const switchedPeriod = currentPeriod === 'day' ? 'week' : 'day'
     req.session.period = switchedPeriod
-    const redir = req.originalUrl
+    const redir = req.headers.referer
       .replace('/switch-view', '')
-      .replace(currentPeriod, switchedPeriod)
-    res.redirect(groupBy ? `${redir}?group_by=${groupBy}` : `${redir}`)
+      .replace(`/${currentPeriod}/`, `/${switchedPeriod}/`)
+    res.redirect(`${redir}`)
   }
 }
 
