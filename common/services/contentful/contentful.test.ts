1bca1a42bf4bf4f4eadb4ddf1def0e63eb510d9e
Dan Benton <daniel.benton@digital.justice.gov.uk>
2025-08-05 16:57:09 +0100

    fix: MAP-2575 Fix test expectation

--- common/services/contentful/contentful.test.ts
+++ common/services/contentful/contentful.test.ts
@@ -463,8 +463,8 @@ const formattedEntriesMockResponse = {
         ' govuk-!-margin-top-4"><em>Test heading 3.</em></h3><h4 class="govuk-heading-s govuk-!-margin-top-3">Test' +
         ' heading 4.</h4><a class="govuk-link" href="https://google.com">Test Link</a><p class="govuk-template__body govuk-!-margin-bottom-4">' +
         '<strong>Some random paragraph text.</strong></p><ul class="govuk-list govuk-list--bullet ' +
-        'govuk-list-bullet-bottom-padding"><li><p class="govuk-template__body govuk-!-margin-bottom-4">TEST LINE 1</p></li><li><p class="govuk-template__body">' +
-        'TEST LINE 2</p></li></ul><ol class="govuk-list govuk-list--number govuk-list-number-bottom-padding"><li><p class="govuk-template__body">TEST LINE 1' +
+        'govuk-list-bullet-bottom-padding"><li><p class="govuk-template__body govuk-!-margin-bottom-4">TEST LINE 1</p></li><li><p class="govuk-template__body govuk-!-margin-bottom-4">' +
+        'TEST LINE 2</p></li></ul><ol class="govuk-list govuk-list--number govuk-list-number-bottom-padding"><li><p class="govuk-template__body govuk-!-margin-bottom-4">TEST LINE 1' +
         '</p></li><li><p class="govuk-template__body govuk-!-margin-bottom-4">TEST LINE 2</p></li></ol><figure class="govuk-!-margin-top-6 ' +
         'govuk-!-margin-bottom-6"><img src="https://images.ctfassets.net/m5k1kmk3zqwh/4W3q8OwEoyEQxjJtdtCkbg/51b7fc' +
         '14e8d568d5f5314733e1b9aadb/image.png" alt="asset-test" /></figure>',
