6be89f6eac81c8069f43bdf3c303d563293787df
Dan Benton <daniel.benton@digital.justice.gov.uk>
2025-10-07 08:55:11 +0100

    chore: MAP-2376 Lint

--- common/types/event-details.ts
+++ common/types/event-details.ts
@@ -71,8 +71,8 @@ export interface EventDetails {
   end_date?: string
   old_end_date?: string
   location?: Location
-  old_location?: Location,
-  stakeholder?: string,
-  summary?: string,
+  old_location?: Location
+  stakeholder?: string
+  summary?: string
   further_details?: string
 }
