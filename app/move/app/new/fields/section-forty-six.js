5bb56ff41b831abdf2830b296824753b594165bf
Dan Benton <daniel.benton@justice.gov.uk>
2026-01-29 10:29:51 +0000

    test: MAPB-89 Add s46 fieldset

--- app/move/app/new/fields/section-forty-six.js
+++ app/move/app/new/fields/section-forty-six.js
@@ -2,6 +2,12 @@ const sectionFortySix = {
   validate: 'required',
   component: 'govukRadios',
   name: 'section_forty_six',
+  fieldset: {
+    legend: {
+      text: 'fields::section_forty_six.label',
+      classes: 'govuk-visually-hidden govuk-fieldset__legend--m',
+    },
+  },
   hint: {
     text: 'fields::section_forty_six.hint',
   },
