fdcf63643c13efc1d40c04a1a5b03dd05a96762c
Dan Benton <daniel.benton@justice.gov.uk>
2026-03-17 11:20:09 +0000

    fix: MAPB-318 Ignore linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
 import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
 
 const config = hmppsConfig({
-  extraPathsAllowingDevDependencies: ['.allowed-scripts.mjs']
+  extraPathsAllowingDevDependencies: ['.allowed-scripts.mjs'],
+  extraIgnorePaths: ['.build', 'node_modules', '**/*.js', '**/*.ts', '.allowed-scripts.mjs'],
 })
 
 export default config
