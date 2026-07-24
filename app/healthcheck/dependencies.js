5576004ebd62a85060b9499d805e4d9f1026803f
David Lonsdale <david.lonsdale@justice.gov.uk>
2026-07-24 11:20:51 +0100

    [MAPB-656] Redis health check fix (#3539)
    * fix: Redis health check fix
    
    * Improved fix + tests
    
    ---------
    
    Co-authored-by: Dan Benton <113102670+danbenton-mojdt@users.noreply.github.com>

--- app/healthcheck/dependencies.js
+++ app/healthcheck/dependencies.js
@@ -50,7 +50,13 @@ module.exports = [
     name: 'redis',
     healthcheck: async () => {
       if (redisStore) {
-        const result = (await redisStore()).client.v4.ping()
+        const store = await redisStore()
+        const result = await Promise.race([
+          store.client.ping(),
+          new Promise((resolve, reject) =>
+            setTimeout(() => reject(new Error('Timed out')), timeout)
+          ),
+        ])
 
         if (!result) {
           throw new Error('No connection')
