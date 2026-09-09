58d25bae5d60b1729c89cc74a99579227c413d3d
Dan Freemantle <dan.freemantle@justice.gov.uk>
2026-09-09 12:10:08 +0100

    Checking if removing fsevents from allowed scripts will bypass the check. fsevents is only installed on darwin os, so the Github runner on linux does not install it.

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -8,8 +8,6 @@ export default configureAllowedScripts({
     'node_modules/@ffmpeg-installer/linux-arm@4.1.3': 'FORBID',
     'node_modules/@ffmpeg-installer/linux-arm64@4.1.4': 'FORBID',
     'node_modules/@ffmpeg-installer/linux-ia32@4.1.0': 'FORBID',
-    // Needed by jest for running tests in watch mode
-    'node_modules/fsevents@2.3.2': 'ALLOW',
     // Used if webpack config
     'node_modules/gifsicle@5.3.0': 'ALLOW',
     // Used if webpack config
